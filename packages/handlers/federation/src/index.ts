57635485bf934377aa81cc52eb6a7299a91a48b1
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-03-23 18:10:49 +0300

    Simplify

--- packages/handlers/federation/src/index.ts
+++ packages/handlers/federation/src/index.ts
@@ -1,9 +1,8 @@
 import { MeshHandlerLibrary, YamlConfig } from '@graphql-mesh/types';
 import { ApolloGateway } from '@apollo/gateway';
 import { GraphQLResolveInfo, print } from 'graphql';
-import { delegateToSchema, IDelegateToSchemaOptions, makeRemoteExecutableSchema } from 'graphql-tools-fork';
+import { delegateToSchema, IDelegateToSchemaOptions, makeRemoteExecutableSchema, Fetcher } from 'graphql-tools-fork';
 import { fetchache, Request } from 'fetchache';
-import { parse } from 'querystring';
 
 const handler: MeshHandlerLibrary<YamlConfig.FederationHandler> = {
     async getMeshSource({ config, hooks, cache }) {
@@ -12,26 +11,28 @@ const handler: MeshHandlerLibrary<YamlConfig.FederationHandler> = {
             ...config,
         });
         const { schema, executor } = await gateway.load();
+        const fetcher: Fetcher = ({ query, operationName, variables, context }) => executor({
+            document: query,
+            request: {
+                query: print(query),
+                variables,
+            },
+            operationName,
+            cache,
+            context,
+            queryHash: print(query) + '_' + JSON.stringify(variables),
+        });
+        const remoteSchemaOptions = {
+            schema,
+            fetcher,
+        };
+        const remoteSchema = makeRemoteExecutableSchema(remoteSchemaOptions);
         hooks.on('buildSdkFn', ({ fieldName, replaceFn, schema }) => {
             replaceFn((args: any, context: any, info: GraphQLResolveInfo) => {
                 const delegationOptions: IDelegateToSchemaOptions = {
-                    schema: {
-                        schema,
-                        executor: ({ document, variables }) => executor({
-                            document,
-                            request: {
-                                query: print(document),
-                                operationName: info.operation.name?.value,
-                                variables,
-                            },
-                            cache,
-                            context,
-                            queryHash: print(document) + '_' + JSON.stringify(variables),
-                            operationName: info.operation.name?.value,
-                            operation: info.operation,
-                        }),
-                    },
+                    operation: info.operation.operation,
                     fieldName,
+                    schema,
                     args,
                     info,
                     context
@@ -40,21 +41,9 @@ const handler: MeshHandlerLibrary<YamlConfig.FederationHandler> = {
                 return delegateToSchema(delegationOptions);
             });
         });
+        hooks.on('destroy', () => gateway.stop());
         return { 
-            schema: makeRemoteExecutableSchema({
-                schema,
-                fetcher: ({ query, operationName, variables, context }) => executor({
-                    document: query,
-                    request: {
-                        query: print(query),
-                        variables,
-                    },
-                    operationName,
-                    cache,
-                    context,
-                    queryHash: print(query) + '_' + JSON.stringify(variables),
-                })
-            })
+            schema: remoteSchema,
         };
     }
 }
