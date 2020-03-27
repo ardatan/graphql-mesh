8cf1ca435a13b200c80be27bc734382294ec080b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-03-27 15:34:45 +0300

    Support Certificates in SOAP handler

--- packages/handlers/soap/src/index.ts
+++ packages/handlers/soap/src/index.ts
@@ -1,9 +1,20 @@
 import { MeshHandlerLibrary, YamlConfig } from '@graphql-mesh/types';
-import { soapGraphqlSchema } from 'soap-graphql';
+import { soapGraphqlSchema, createSoapClient } from 'soap-graphql';
+import { WSSecurityCert } from 'soap';
 
 const handler: MeshHandlerLibrary<YamlConfig.SoapHandler> = {
   async getMeshSource({ config }) {
-    const schema = await soapGraphqlSchema(config.wsdl);
+    const soapClient = await createSoapClient(config.wsdl)
+    const schema = await soapGraphqlSchema({
+      soapClient,
+    });
+    if (config.securityCert) {
+      const { publicKey: publicKeyPath, privateKey: privateKeyPath, password } = config.securityCert;
+      const { readFileSync } = await import('fs');
+      const publicKey = readFileSync(publicKeyPath, 'utf8');
+      const privateKey = readFileSync(privateKeyPath, 'utf8');
+      soapClient.setSecurity(new WSSecurityCert(privateKey, publicKey, password))
+    }
 
     return {
       schema
