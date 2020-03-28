ef8e5b5d92ed94a8b2b8d5a71e0f408cb092967c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-03-28 16:53:53 +0300

    Support cluster

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -22,15 +22,15 @@ const logger = createLogger({
 
 export async function graphqlMesh() {
   yargs
-    .command<{}>(
+    .command<{ fork: string | number }>(
       'serve',
       'Serves a GraphiQLApolloServer interface to test your Mesh API',
       () => null,
-      async () => {
+      async ({ fork }) => {
         try {
           const meshConfig = await parseConfig();
           const { schema, contextBuilder } = await getMesh(meshConfig);
-          await serveMesh(logger, schema, contextBuilder, meshConfig.cache);
+          await serveMesh(logger, schema, contextBuilder, meshConfig.cache, fork);
         } catch (e) {
           logger.error('Unable to serve mesh: ', e);
         }
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -2,19 +2,29 @@ import { GraphQLSchema } from 'graphql';
 import { ApolloServer } from 'apollo-server';
 import { Logger } from 'winston';
 import { KeyValueCache } from '@graphql-mesh/types';
+import { fork as clusterFork, isMaster } from 'cluster';
+import { cpus } from 'os';
 
 export async function serveMesh(
   logger: Logger,
   schema: GraphQLSchema,
   contextBuilder: (initialContextValue?: any) => Record<string, any>,
   cache?: KeyValueCache,
+  fork?: string | number,
 ): Promise<void> {
-  const server = new ApolloServer({
-    schema,
-    context: contextBuilder,
-    cache,
-  });
 
-  const { url } = await server.listen();
-  logger.info(`🕸️ => Serving GraphQL Mesh GraphiQL: ${url}`);
+  if (isMaster && fork) {
+    for (let i = 0; i < (Number.isInteger(fork as number) ? parseInt(fork as string) : cpus().length); i++) {
+      clusterFork();
+    }
+    logger.info(`🕸️ => Serving GraphQL Mesh GraphiQL: http://localhost:4000/`);
+  } else {
+    const server = new ApolloServer({
+      schema,
+      context: contextBuilder,
+      cache,
+    });
+
+    await server.listen();
+  }
 }
