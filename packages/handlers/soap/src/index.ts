71690365e3dde9100696c656299b14e784b97d42
Arda TANRIKULU <20847995+ardatan@users.noreply.github.com>
2020-03-29 17:40:53 +0300

    Support Certificates in SOAP handler (#149)
    * Support Certificates in SOAP handler
    
    * Add non-fs version

--- packages/handlers/soap/src/index.ts
+++ packages/handlers/soap/src/index.ts
@@ -1,9 +1,27 @@
 import { MeshHandlerLibrary, YamlConfig } from '@graphql-mesh/types';
-import { soapGraphqlSchema } from 'soap-graphql';
+import { soapGraphqlSchema, createSoapClient } from 'soap-graphql';
+import { WSSecurityCert } from 'soap';
+
+async function openFile(path?: string) {
+  if (path) {
+    const { promises: { readFile } } = await import('fs');
+    return readFile(path, 'utf-8');
+  }
+}
 
 const handler: MeshHandlerLibrary<YamlConfig.SoapHandler> = {
   async getMeshSource({ config }) {
-    const schema = await soapGraphqlSchema(config.wsdl);
+    const soapClient = await createSoapClient(config.wsdl)
+    const schema = await soapGraphqlSchema({
+      soapClient,
+    });
+    if (config.securityCert) {
+      const securityCertConfig = config.securityCert;
+      const privateKey = securityCertConfig?.privateKey || await openFile(securityCertConfig?.privateKeyFilePath);
+      const publicKey = securityCertConfig?.publicKey || await openFile(securityCertConfig?.publicKeyFilePath);
+      const password = securityCertConfig?.password || await openFile(securityCertConfig?.passwordFilePath);
+      soapClient.setSecurity(new WSSecurityCert(privateKey, publicKey, password))
+    }
 
     return {
       schema
