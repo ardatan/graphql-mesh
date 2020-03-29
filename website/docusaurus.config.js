b4ae3e268dbc3fbdcd6b0834297a8d64e863349e
Dotan Simha <dotansimha@gmail.com>
2020-03-29 20:13:26 +0300

    website wip

---
+++ website/docusaurus.config.js
@@ -0,0 +1,96 @@
+module.exports = {
+  title: 'GraphQL Mesh',
+  tagline: 'Query anything, run anywhere',
+  url: 'https://graphql-mesh.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.ico',
+  organizationName: 'urigo', // Usually your GitHub org/user name.
+  projectName: 'graphql-mesh', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'GraphQL Mesh',
+      logo: {
+        alt: 'GraphQL Mesh',
+        src: 'img/logo.svg',
+      },
+      links: [
+        {
+          to: 'docs/doc1',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/doc1',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ website/sidebars.js
@@ -0,0 +1,8 @@
+module.exports = {
+  sidebar: {
+    'Getting Started': ['doc1', 'doc2', 'doc3'],
+    'Available Handlers': [],
+    'Transforms': [],
+    Recipes: ['mdx']
+  }
+};
