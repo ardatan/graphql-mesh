486d59f1b48c8668049472e084401efb621986c5
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-03-29 17:40:24 +0300

    Add non-fs version

--- packages/handlers/soap/src/index.ts
+++ packages/handlers/soap/src/index.ts
@@ -2,6 +2,13 @@ import { MeshHandlerLibrary, YamlConfig } from '@graphql-mesh/types';
 import { soapGraphqlSchema, createSoapClient } from 'soap-graphql';
 import { WSSecurityCert } from 'soap';
 
+async function openFile(path?: string) {
+  if (path) {
+    const { promises: { readFile } } = await import('fs');
+    return readFile(path, 'utf-8');
+  }
+}
+
 const handler: MeshHandlerLibrary<YamlConfig.SoapHandler> = {
   async getMeshSource({ config }) {
     const soapClient = await createSoapClient(config.wsdl)
@@ -9,10 +16,10 @@ const handler: MeshHandlerLibrary<YamlConfig.SoapHandler> = {
       soapClient,
     });
     if (config.securityCert) {
-      const { publicKey: publicKeyPath, privateKey: privateKeyPath, password } = config.securityCert;
-      const { readFileSync } = await import('fs');
-      const publicKey = readFileSync(publicKeyPath, 'utf8');
-      const privateKey = readFileSync(privateKeyPath, 'utf8');
+      const securityCertConfig = config.securityCert;
+      const privateKey = securityCertConfig?.privateKey || await openFile(securityCertConfig?.privateKeyFilePath);
+      const publicKey = securityCertConfig?.publicKey || await openFile(securityCertConfig?.publicKeyFilePath);
+      const password = securityCertConfig?.password || await openFile(securityCertConfig?.passwordFilePath);
       soapClient.setSecurity(new WSSecurityCert(privateKey, publicKey, password))
     }
 
