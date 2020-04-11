1c62720ac9aaf5eb12f135437b12fd683e192cca
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-04-11 21:38:30 +0300

    Use fetchache in SOAP Handler

--- packages/handlers/soap/src/index.ts
+++ packages/handlers/soap/src/index.ts
@@ -2,29 +2,61 @@ import { MeshHandlerLibrary, YamlConfig } from '@graphql-mesh/types';
 import { soapGraphqlSchema, createSoapClient } from 'soap-graphql';
 import { WSSecurityCert } from 'soap';
 import { readFileOrUrlWithCache } from '@graphql-mesh/utils';
+import { Request, fetchache } from 'fetchache';
 
 const handler: MeshHandlerLibrary<YamlConfig.SoapHandler> = {
   async getMeshSource({ config, cache }) {
     const soapClient = await createSoapClient(config.wsdl, {
       basicAuth: config.basicAuth,
+      options: {
+        request: ((requestObj: any, callback: Function) => {
+          let _request: any = null;
+          const sendRequest = async () => {
+            const req = new Request(requestObj.uri.href, {
+              headers: requestObj.headers,
+              method: requestObj.method,
+              body: requestObj.body,
+            });
+            const res = await fetchache(req, cache);
+            // eslint-disable-next-line dot-notation
+            _request = res.body;
+            const body = await res.text();
+            return { res, body };
+          };
+          sendRequest()
+            .then(({ res, body }) =>
+              callback(
+                null,
+                {
+                  ...res,
+                  statusCode: res.status,
+                },
+                body
+              )
+            )
+            .catch(err => callback(err));
+          // eslint-disable-next-line dot-notation
+          return _request;
+        }) as any,
+      },
     });
 
     if (config.securityCert) {
       const securityCertConfig = config.securityCert;
       const [privateKey, publicKey, password] = await Promise.all([
         securityCertConfig.privateKey ||
-          (securityCertConfig.privateKeyFilePath &&
-            readFileOrUrlWithCache<string>(securityCertConfig.privateKeyFilePath, cache, {
+          (securityCertConfig.privateKeyPath &&
+            readFileOrUrlWithCache<string>(securityCertConfig.privateKeyPath, cache, {
               allowUnknownExtensions: true,
             })),
         securityCertConfig.publicKey ||
-          (securityCertConfig.publicKeyFilePath &&
-            readFileOrUrlWithCache<string>(securityCertConfig.publicKeyFilePath, cache, {
+          (securityCertConfig.publicKeyPath &&
+            readFileOrUrlWithCache<string>(securityCertConfig.publicKeyPath, cache, {
               allowUnknownExtensions: true,
             })),
         securityCertConfig.password ||
-          (securityCertConfig.passwordFilePath &&
-            readFileOrUrlWithCache<string>(securityCertConfig.passwordFilePath, cache, {
+          (securityCertConfig.passwordPath &&
+            readFileOrUrlWithCache<string>(securityCertConfig.passwordPath, cache, {
               allowUnknownExtensions: true,
             })),
       ]);
