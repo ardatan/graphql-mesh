cbcdcff7e39c054f494090c4433bb0a209ff1b51
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-04-12 16:10:46 +0300

    Add Example Query option to serve

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,14 @@
 import { GraphQLSchema } from 'graphql';
 import { ApolloServer } from 'apollo-server';
+import { ApolloServerExpressConfig } from 'apollo-server-express';
 import { Logger } from 'winston';
 import { KeyValueCache } from '@graphql-mesh/types';
 import { fork as clusterFork, isMaster } from 'cluster';
 import { cpus } from 'os';
+import { loadDocuments } from '@graphql-toolkit/core';
+import { CodeFileLoader } from '@graphql-toolkit/code-file-loader';
+import { GraphQLFileLoader } from '@graphql-toolkit/graphql-file-loader';
+import { basename } from 'path';
 
 export async function serveMesh(
   logger: Logger,
@@ -11,25 +16,41 @@ export async function serveMesh(
   contextBuilder: (initialContextValue?: any) => Record<string, any>,
   cache?: KeyValueCache,
   fork?: string | number,
+  port: string | number = 4000,
+  exampleQuery?: string
 ): Promise<void> {
-
   if (isMaster && fork) {
     fork = fork > 1 ? fork : cpus().length;
     for (let i = 0; i < fork; i++) {
       clusterFork();
     }
-    logger.info(`🕸️ => Serving GraphQL Mesh GraphiQL: http://localhost:4000 in ${fork} forks`);
+    logger.info(`🕸️ => Serving GraphQL Mesh GraphiQL: http://localhost:${port} in ${fork} forks`);
   } else {
-    const server = new ApolloServer({
+    const apolloConfig: ApolloServerExpressConfig = {
       schema,
       context: contextBuilder,
       cache,
-    });
+    };
 
-    const { url } = await server.listen();
+    if (exampleQuery) {
+      const documents = await loadDocuments(exampleQuery, {
+        loaders: [new CodeFileLoader(), new GraphQLFileLoader()],
+        cwd: process.cwd(),
+      });
+      apolloConfig.playground = {
+        tabs: documents.map(doc => ({
+          name: doc.location && basename(doc.location),
+          endpoint: `http://localhost:${port}`,
+          query: doc.rawSDL,
+        })),
+      };
+    }
+
+    const server = new ApolloServer(apolloConfig);
+
+    const { url } = await server.listen(port);
     if (!fork) {
       logger.info(`🕸️ => Serving GraphQL Mesh GraphiQL: ${url}`);
     }
   }
 }
-
