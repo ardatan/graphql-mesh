b8d13a99ecb91dc7fdfdf69fef0a702fc99feeef
Arda TANRIKULU <20847995+ardatan@users.noreply.github.com>
2020-04-18 14:11:18 +0300

    Reimplement gRPC (#251)
    * Improvements on MySQL
    
    * Re implement gRPC
    
    * Docs

---
+++ packages/handlers/grpc/src/grpc-graphql-schema-factory.ts
@@ -0,0 +1,246 @@
+import {
+  GraphQLInputType,
+  GraphQLOutputType,
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLScalarType,
+  GraphQLFieldConfigMap,
+  GraphQLEnumType,
+  GraphQLEnumTypeConfig,
+  GraphQLInputObjectTypeConfig,
+  GraphQLObjectTypeConfig,
+  GraphQLList,
+  GraphQLInputObjectType,
+  GraphQLObjectType,
+  GraphQLFieldConfig,
+  GraphQLFieldConfigArgumentMap,
+  GraphQLSchema,
+} from 'graphql';
+import { BigIntResolver as GraphQLBigInt } from 'graphql-scalars';
+import { AnyNestedObject, load } from 'protobufjs';
+import { isAbsolute, join } from 'path';
+import { Readable } from 'stream';
+import { asyncMap } from 'iter-tools';
+import { withAsyncIteratorCancel } from './with-async-iterator-cancel';
+import grpcCaller from 'grpc-caller';
+import { camelCase } from 'camel-case';
+import { pascalCase } from 'pascal-case';
+
+const SCALARS: [string, GraphQLScalarType][] = [
+  ['int32', GraphQLInt],
+  ['int64', GraphQLBigInt],
+  ['float', GraphQLFloat],
+  ['double', GraphQLFloat],
+  ['string', GraphQLString],
+  ['bool', GraphQLBoolean],
+];
+
+export interface GrpcGraphQLSchemaConfig {
+  protoFilePath: string;
+  endpoint: string;
+  serviceName?: string;
+  packageName?: string;
+}
+
+export class GrpcGraphQLSchemaFactory {
+  private inputTypeMap: Map<string, GraphQLInputType>;
+  private outputTypeMap: Map<string, GraphQLOutputType>;
+  private nonSubscriptionFields: GraphQLFieldConfigMap<any, any> = {};
+  private subscriptionFields: GraphQLFieldConfigMap<any, any> = {};
+  private futureJobs: Function[] = [];
+  constructor(private config: GrpcGraphQLSchemaConfig) {
+    this.inputTypeMap = new Map(SCALARS);
+    this.outputTypeMap = new Map(SCALARS);
+    this.outputTypeMap.set(
+      'ServerStatus',
+      new GraphQLObjectType({
+        name: 'ServerStatus',
+        description: 'status of the server',
+        fields: () => ({
+          status: {
+            type: GraphQLString,
+            descripton: 'status string',
+          },
+        }),
+      })
+    );
+  }
+
+  async init() {
+    const absoluteProtoFilePath = isAbsolute(this.config.protoFilePath)
+      ? this.config.protoFilePath
+      : join(process.cwd(), this.config.protoFilePath);
+    const root = await load(absoluteProtoFilePath);
+    const nested = root.toJSON({
+      keepComments: true,
+    });
+    this.visit(nested, '', '');
+    this.futureJobs.forEach(futureJob => futureJob());
+  }
+
+  visit(nested: AnyNestedObject, name: string, currentPath: string) {
+    if ('values' in nested) {
+      let typeName = name;
+      if (currentPath !== this.config.packageName) {
+        typeName = pascalCase(currentPath.split('.').join('_') + '_' + typeName);
+      }
+      const enumTypeConfig: GraphQLEnumTypeConfig = {
+        name: typeName,
+        values: {},
+      };
+      for (const key in nested.values) {
+        enumTypeConfig.values[key] = {
+          value: key,
+        };
+      }
+      const enumType = new GraphQLEnumType(enumTypeConfig);
+      this.inputTypeMap.set(currentPath + '.' + name, enumType);
+    } else if ('fields' in nested) {
+      let typeName = name;
+      if (currentPath !== this.config.packageName) {
+        typeName = pascalCase(currentPath.split('.').join('_') + '_' + typeName);
+      }
+      const inputTypeConfig: GraphQLInputObjectTypeConfig = {
+        name: pascalCase(typeName + '_Input'),
+        fields: {},
+      };
+      const outputTypeConfig: GraphQLObjectTypeConfig<any, any> = {
+        name: typeName,
+        fields: {},
+      };
+      for (const fieldName in nested.fields) {
+        const { type, rule } = nested.fields[fieldName];
+        let inputType = this.inputTypeMap.get(type) || this.inputTypeMap.get(currentPath + '.' + type);
+        let outputType = this.outputTypeMap.get(type) || this.outputTypeMap.get(currentPath + '.' + type);
+        // If not resolved, do it later
+        if (!inputType || !outputType) {
+          this.futureJobs.push(() => this.visit(nested, name, currentPath));
+          return;
+        }
+        if (rule === 'repeated') {
+          inputType = new GraphQLList(inputType);
+          outputType = new GraphQLList(outputType);
+        }
+        inputTypeConfig.fields[fieldName] = {
+          type: inputType,
+        };
+        outputTypeConfig.fields[fieldName] = {
+          type: outputType,
+        };
+      }
+      const inputType = new GraphQLInputObjectType(inputTypeConfig);
+      const outputType = new GraphQLObjectType(outputTypeConfig);
+      this.inputTypeMap.set(currentPath + '.' + name, inputType);
+      this.outputTypeMap.set(currentPath + '.' + name, outputType);
+    } else if ('methods' in nested) {
+      const methods = nested.methods;
+      const client = grpcCaller(this.config.endpoint, this.config.protoFilePath, name, null, {
+        'grpc.max_send_message_length': -1,
+        'grpc.max_receive_message_length': -1,
+      });
+      for (const methodName in methods) {
+        const method = methods[methodName];
+        const inputType =
+          this.inputTypeMap.get(method.requestType) || this.inputTypeMap.get(currentPath + '.' + method.requestType);
+        // If not resolved, do it later
+        if (!inputType) {
+          this.futureJobs.push(() => this.visit(nested, name, currentPath));
+          return;
+        }
+        const args: GraphQLFieldConfigArgumentMap = {
+          input: {
+            type: inputType,
+            defaultValue: {},
+          },
+        };
+        let rootFieldName = methodName;
+        if (name !== this.config.serviceName) {
+          rootFieldName = camelCase(name + '_' + rootFieldName);
+        }
+        if (currentPath !== this.config.packageName) {
+          rootFieldName = camelCase(currentPath.split('.').join('_') + '_' + rootFieldName);
+        }
+        const outputType =
+          this.outputTypeMap.get(method.responseType) ||
+          this.outputTypeMap.get(currentPath + '.' + method.responseType);
+        // If not resolved, do it later
+        if (!outputType) {
+          this.futureJobs.push(() => this.visit(nested, name, currentPath));
+          return;
+        }
+        const fieldConfig: GraphQLFieldConfig<any, any> = {
+          type: outputType,
+          args,
+        };
+        if (method.responseStream) {
+          this.subscriptionFields[rootFieldName] = {
+            ...fieldConfig,
+            subscribe: async (__, args) => {
+              const response: Readable & { cancel: () => void } = await client[methodName](args.input, {});
+
+              response.on('error', (error: Error & { code: number }) => {
+                if (error.code === 1) {
+                  // cancelled
+                  response.removeAllListeners('error');
+                  response.removeAllListeners();
+                }
+              });
+
+              response.on('end', () => {
+                response.removeAllListeners();
+              });
+
+              const asyncIterator = asyncMap(data => ({ [rootFieldName]: data }), response);
+
+              return withAsyncIteratorCancel(asyncIterator, () => {
+                response.cancel();
+              });
+            },
+          };
+        } else {
+          this.nonSubscriptionFields[rootFieldName] = {
+            ...fieldConfig,
+            resolve: (_, args) => {
+              return client[methodName](
+                args.input,
+                {},
+                {
+                  deadline: Date.now() + (Number(process.env.REQUEST_TIMEOUT) || 200000),
+                }
+              );
+            },
+          };
+        }
+      }
+    } else if ('nested' in nested) {
+      for (const key in nested.nested) {
+        const currentNested = nested.nested[key];
+        this.visit(currentNested, key, currentPath ? currentPath + '.' + name : name);
+      }
+    }
+  }
+
+  buildSchema() {
+    return new GraphQLSchema({
+      query: new GraphQLObjectType({
+        name: 'Query',
+        fields: {
+          foo: {
+            type: GraphQLString,
+            resolve: () => 'FOO',
+          },
+        },
+      }),
+      mutation: new GraphQLObjectType({
+        name: 'Mutation',
+        fields: this.nonSubscriptionFields,
+      }),
+      subscription: new GraphQLObjectType({
+        name: 'Subscription',
+        fields: this.subscriptionFields,
+      }),
+    });
+  }
+}
--- website/docs/handlers/mysql.md
+++ website/docs/handlers/mysql.md
@@ -5,7 +5,7 @@ sidebar_label: MySQL
 ---
 ![image](https://user-images.githubusercontent.com/20847995/79219205-84e55580-7e5a-11ea-96e2-c39581b90e61.png)
 
-This handler allows you to generate GraphQL schema based on a MySQL database schema or an SQL dump file.
+This handler allows you to generate GraphQL schema using `graphql-compose-mysql` based on a MySQL database schema or an SQL dump file.
 
 To get started, install the handler library from NPM:
 
@@ -20,11 +20,19 @@ sources:
   - name: Employees
     handler:
       mysql:
-        connectionString: path/to/database.sqlite
+        host: localhost
+        port: 3306
+        user: root
+        password: passwd
+        database: employees
 ```
 
-> You can check out our example that uses MySQL handler.
-[Click here to open the example on GitHub](https://github.com/Urigo/graphql-mesh/tree/master/examples/mysql-employees)
+<iframe
+     src="https://codesandbox.io/embed/github/Urigo/graphql-mesh/tree/master/examples/mysql-rfam?fontsize=14&hidenavigation=1&theme=dark&module=%2F.meshrc.yml"
+     style={{width:"100%", height:"500px", border:"0", borderRadius: "4px", overflow:"hidden"}}
+     title="mysql-rfam-example"
+     allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+     sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"/>
 
 ## Config API Reference
 
