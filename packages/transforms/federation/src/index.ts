a966c000afafdfb8cb5eec840fc964f84e1a6d01
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-05-13 01:18:06 +0300

    Force GraphQL for keyFields

--- packages/transforms/federation/src/index.ts
+++ packages/transforms/federation/src/index.ts
@@ -1,33 +1,43 @@
-import { GraphQLSchema } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLID } from 'graphql';
 import { TransformFn, YamlConfig } from '@graphql-mesh/types';
+import { loadFromModuleExportExpression } from '@graphql-mesh/utils';
 import { transformSchemaFederation, FederationConfig, FederationFieldsConfig } from 'graphql-transform-federation';
 
-export const extendFederation: TransformFn<YamlConfig.Transform['federation']> = ({
+export const extendFederation: TransformFn<YamlConfig.Transform['federation']> = async ({
   schema,
-  config
-}): GraphQLSchema => {
+  config,
+}): Promise<GraphQLSchema> => {
+  const federationConfig: FederationConfig<any> = {};
 
-    const federationConfig: FederationConfig<any> = {};
-
-    if (config?.types) {
-        for (const type of config.types) {
-            const fields: FederationFieldsConfig = {};
-            if (type.config?.fields) {
-                for (const field of type.config.fields) {
-                    fields[field.name] = field.config;
-                }
-            }
-            federationConfig[type.name] = {
-                ...type.config,
-                fields,
-            };
+  if (config?.types) {
+    for (const type of config.types) {
+      const fields: FederationFieldsConfig = {};
+      if (type.config?.fields) {
+        for (const field of type.config.fields) {
+          fields[field.name] = field.config;
         }
+      }
+      // If a field is a key field, it should be GraphQLID
+      if (type.config?.keyFields) {
+        for (const fieldName of type.config.keyFields) {
+          const objectType = schema.getType(type.name) as GraphQLObjectType;
+          objectType.getFields()[fieldName].type = GraphQLID;
+        }
+      }
+      const resolveReference =
+        type.config?.resolveReference &&
+        (await loadFromModuleExportExpression(type.config.resolveReference, '__resolveReference'));
+      federationConfig[type.name] = {
+        ...type.config,
+        resolveReference,
+        fields,
+      };
     }
+  }
 
-    const federationSchema = transformSchemaFederation(schema, federationConfig);
+  const federationSchema = transformSchemaFederation(schema, federationConfig);
 
-    return federationSchema;
-
+  return federationSchema;
 };
 
 export default extendFederation;
