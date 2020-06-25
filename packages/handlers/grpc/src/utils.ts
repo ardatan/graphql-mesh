3ad997bf839cd85358d000c73fb187d230e15db0
Stephane Rufer <app.s@rufer.casa>
2020-06-25 15:29:45 -0700

    Add new grpc handler functionality, refactor and add tests (#617)
    * fix(handlers): add functionality to grpc and refactor
    
    - Protobuf supports an empty type, add support for those types in
      graphql
    - Protobuf messages can be defined in a nested way. Support this
      nested way of defining and referencing proto messages.
    - Refactor the grpc handler, so that things are more manageable.
      - Move helper functions to utils into their own file.
      - Moive scalar related stuff into it's own file. This will allow for
        manipulating scalar mappings externally.
    
    * chore(handlers) add tests for grpc
    
    Add tests for helper functions and some example protos to cover various
    different valid use cases.
    
    * feat(handlers): allow ssl with installed rootCA
    
    If there is a root certificate installed, then you can configure calls to be
    over ssl by just specifing the `rootCA` config option as "rootCA".

---
+++ packages/handlers/grpc/src/utils.ts
@@ -0,0 +1,208 @@
+import { KeyValueCache } from '@graphql-mesh/types';
+import { readFileOrUrlWithCache } from '@graphql-mesh/utils';
+import { ClientReadableStream, ClientUnaryCall, Metadata, MetadataValue } from '@grpc/grpc-js';
+import { pathExistsSync } from 'fs-extra';
+import { GraphQLEnumTypeConfig } from 'graphql';
+import { InputTypeComposer, ObjectTypeComposer, SchemaComposer } from 'graphql-compose';
+import { get } from 'lodash';
+import { pascalCase } from 'pascal-case';
+import { isAbsolute, join } from 'path';
+import { IField, Root } from 'protobufjs';
+
+import { getGraphQLScalar, isScalarType } from './scalars';
+
+export type ClientMethod = (
+  input: unknown,
+  metaData?: Metadata
+) => Promise<ClientUnaryCall> | AsyncIterator<ClientReadableStream<unknown>>;
+
+interface InputOutputTypes {
+  input: string;
+  output: string;
+}
+
+export function toSnakeCase(str: string): string {
+  return str.split('.').join('_');
+}
+
+export function addIncludePathResolver(root: Root, includePaths: string[]): void {
+  const originalResolvePath = root.resolvePath;
+  root.resolvePath = (origin: string, target: string) => {
+    if (isAbsolute(target)) {
+      return target;
+    }
+    for (const directory of includePaths) {
+      const fullPath: string = join(directory, target);
+      if (pathExistsSync(fullPath)) {
+        return fullPath;
+      }
+    }
+    const path = originalResolvePath(origin, target);
+    if (path === null) {
+      console.warn(`${target} not found in any of the include paths ${includePaths}`);
+    }
+    return path;
+  };
+}
+
+export function addMetaDataToCall(
+  call: ClientMethod,
+  input: unknown,
+  context: Record<string, unknown>,
+  metaData: Record<string, string | string[] | Buffer>
+): Promise<ClientUnaryCall> | AsyncIterator<ClientReadableStream<unknown>> {
+  if (metaData) {
+    const meta = new Metadata();
+    for (const [key, value] of Object.entries(metaData)) {
+      let metaValue: unknown = value;
+      if (Array.isArray(value)) {
+        // Extract data from context
+        metaValue = get(context, value);
+      }
+      // Ensure that the metadata is compatible with what node-grpc expects
+      if (typeof metaValue !== 'string' && !(metaValue instanceof Buffer)) {
+        metaValue = JSON.stringify(metaValue);
+      }
+
+      meta.add(key, metaValue as MetadataValue);
+    }
+
+    return call(input, meta);
+  }
+  return call(input);
+}
+
+export async function getBuffer(path: string, cache: KeyValueCache): Promise<Buffer> {
+  if (path) {
+    const result = await readFileOrUrlWithCache<string>(path, cache, {
+      allowUnknownExtensions: true,
+    });
+    return Buffer.from(result);
+  }
+  return undefined;
+}
+
+export function getTypeName(
+  schemaComposer: SchemaComposer<unknown>,
+  typePath: string,
+  isInput: boolean,
+  packageName: string
+): string {
+  if (isScalarType(typePath)) {
+    return getGraphQLScalar(typePath);
+  }
+  let baseTypeName = pascalCase(toSnakeCase(typePath.replace(packageName + '.', '')));
+  if (isInput && !schemaComposer.isEnumType(baseTypeName)) {
+    baseTypeName += 'Input';
+  }
+  return baseTypeName;
+}
+
+export function createEnum(typeName: string, values: Record<string, number>): GraphQLEnumTypeConfig {
+  const enumTypeConfig: GraphQLEnumTypeConfig = {
+    name: typeName,
+    values: {},
+  };
+  for (const [key, value] of Object.entries(values)) {
+    enumTypeConfig.values[key] = {
+      value,
+    };
+  }
+  return enumTypeConfig;
+}
+
+export function createFieldsType(typeName: string): InputOutputTypes {
+  return {
+    input: typeName + 'Input',
+    output: typeName,
+  };
+}
+
+async function addInputOutputFields(
+  schemaComposer: SchemaComposer<unknown>,
+  inputTC: InputTypeComposer,
+  outputTC: ObjectTypeComposer,
+  fields: { [k: string]: IField },
+  name: string,
+  currentPath: string,
+  packageName: string
+): Promise<void> {
+  const fieldKeys = Object.keys(fields);
+  if (!fields.length) {
+    // This is a empty proto type
+    inputTC.addFields({
+      _: {
+        type: () => {
+          return getTypeName(schemaComposer, 'bool', true, packageName);
+        },
+      },
+    });
+    outputTC.addFields({
+      _: {
+        type: () => {
+          return getTypeName(schemaComposer, 'bool', true, packageName);
+        },
+      },
+    });
+  }
+  await Promise.all(
+    fieldKeys.map(async fieldName => {
+      const { type, rule } = fields[fieldName];
+      let fullType = type;
+      if (
+        packageName &&
+        !isScalarType(type) &&
+        !type.includes('.') &&
+        currentPath.length &&
+        name.includes(currentPath)
+      ) {
+        fullType = pascalCase(currentPath + type);
+      }
+
+      inputTC.addFields({
+        [fieldName]: {
+          type: () => {
+            const inputTypeName = getTypeName(schemaComposer, fullType, true, packageName);
+            return rule === 'repeated' ? `[${inputTypeName}]` : inputTypeName;
+          },
+        },
+      });
+      outputTC.addFields({
+        [fieldName]: {
+          type: () => {
+            const typeName = getTypeName(schemaComposer, fullType, false, packageName);
+            return rule === 'repeated' ? `[${typeName}]` : typeName;
+          },
+        },
+      });
+    })
+  );
+}
+
+export function createInputOutput(
+  schemaComposer: SchemaComposer<unknown>,
+  name: string,
+  currentPath: string,
+  packageName: string,
+  fields: { [k: string]: IField }
+): void {
+  const { input, output } = createFieldsType(name);
+
+  const inputTC = schemaComposer.createInputTC({
+    name: input,
+    fields: {},
+  });
+  const outputTC = schemaComposer.createObjectTC({
+    name: output,
+    fields: {},
+  });
+  addInputOutputFields(
+    schemaComposer,
+    inputTC,
+    outputTC,
+    fields,
+    name,
+    pascalCase(toSnakeCase(currentPath)),
+    packageName
+  );
+}
---
+++ packages/handlers/grpc/test/utils.spec.ts
@@ -0,0 +1,138 @@
+import { Metadata } from '@grpc/grpc-js';
+import { SchemaComposer } from 'graphql-compose';
+
+import { addMetaDataToCall, createEnum, createFieldsType, getTypeName, toSnakeCase } from '../src/utils';
+
+describe('grpc utils', () => {
+  describe('addMetaDataToCall', () => {
+    const grpcClientMethod = jest.fn();
+    const input = { sport: 'Baseball' };
+    const context = { team: 'Oakland As', players: { pitcher: 'Kershaw' }, number: 42 };
+    const binarySportsTeam = Buffer.from([68, 111, 100, 103, 101, 114, 115, 32, 82, 117, 108, 101, 33]);
+    const binaryPlayer = Buffer.from([75, 101, 114, 115, 104, 97, 119]);
+
+    function createExpectedMetadata(key: string, value: string | Buffer): Metadata {
+      const meta = new Metadata();
+      meta.add(key, value);
+
+      return meta;
+    }
+
+    test(`when no metadata is supplied by the config`, () => {
+      addMetaDataToCall(grpcClientMethod, input, context, undefined);
+      expect(grpcClientMethod).toHaveBeenCalledWith(input);
+    });
+
+    describe.each<[string, Record<string, string | Buffer | string[]>, Metadata]>([
+      ['static', { sportsTeam: 'Dodgers' }, createExpectedMetadata('sportsTeam', 'Dodgers')],
+      ['static all lowercase', { sportsteam: 'Dodgers' }, createExpectedMetadata('sportsteam', 'Dodgers')],
+      ['dynamic', { bestPlayer: ['players', 'pitcher'] }, createExpectedMetadata('bestplayer', 'Kershaw')],
+      ['dynamic number', { jerseyNumber: ['number'] }, createExpectedMetadata('jerseynumber', '42')],
+      [
+        'dynamic underscore key',
+        { best_player: ['players', 'pitcher'] },
+        createExpectedMetadata('best_player', 'Kershaw'),
+      ],
+      [
+        'static binary',
+        { 'sportsTeam-bin': binarySportsTeam },
+        createExpectedMetadata('sportsTeam-bin', binarySportsTeam),
+      ],
+      ['dynamic binary', { 'bestPlayer-bin': binaryPlayer }, createExpectedMetadata('bestPlayer-bin', binaryPlayer)],
+    ])('should generate gRPC Metadata', (type, config, expectedMetadata) => {
+      beforeEach(() => {
+        grpcClientMethod.mockClear();
+      });
+
+      test(`when ${type} metadata is supplied by the config`, () => {
+        addMetaDataToCall(grpcClientMethod, input, context, config);
+        expect(grpcClientMethod).toHaveBeenCalledWith(input, expectedMetadata);
+      });
+    });
+
+    describe.each<[string, Record<string, string | Buffer | string[]>]>([
+      ['static binary', { sportsTeam: binarySportsTeam }],
+      ['dynamic binary', { bestPlayer: binaryPlayer }],
+    ])('should throw errors when generating gRPC Metadata', (type, config) => {
+      test(`when ${type} metadata is supplied by the config`, () => {
+        expect(() => addMetaDataToCall(grpcClientMethod, input, context, config)).toThrow(
+          /keys that don't end with '-bin' must have String values/
+        );
+      });
+    });
+  });
+
+  describe('createEnum', () => {
+    test('should create an enum with the proper type name and values', () => {
+      const typeName = 'Sports';
+      const fields = { BASEBALL: 0, BASKETBALL: 1 };
+
+      expect(createEnum(typeName, fields)).toEqual({
+        name: typeName,
+        values: { BASEBALL: { value: 0 }, BASKETBALL: { value: 1 } },
+      });
+    });
+  });
+
+  describe('getTypeName', () => {
+    const schemaComposer = new SchemaComposer();
+    const enumType = 'Arena';
+    schemaComposer.createEnumTC({ name: enumType, values: {} });
+    const inputFlag = 'Input';
+    const packageName = 'Sports';
+    const type = 'Team';
+
+    describe.each<[string, string]>([
+      ['bool', 'Boolean'],
+      ['bytes', 'Byte'],
+      ['double', 'Float'],
+      ['fixed32', 'Int'],
+      ['fixed64', 'BigInt'],
+      ['float', 'Float'],
+      ['int32', 'Int'],
+      ['int64', 'BigInt'],
+      ['sfixed32', 'Int'],
+      ['sfixed64', 'BigInt'],
+      ['sint32', 'Int'],
+      ['sint64', 'BigInt'],
+      ['string', 'String'],
+      ['uint32', 'Int'],
+      ['uint64', 'BigInt'],
+    ])('scalar types', (scalarType, scalarGqlType) => {
+      test(`should return the proper name for ${scalarType}`, () => {
+        expect(getTypeName(schemaComposer, scalarType, false, 'Sports')).toBe(scalarGqlType);
+      });
+
+      test(`should return the same name for ${scalarType} regardless of input status or package name`, () => {
+        expect(getTypeName(schemaComposer, scalarType, true, '')).toBe(scalarGqlType);
+      });
+    });
+
+    test('should return type without package name prefix', () => {
+      const fullTypeName = `${packageName}.Team`;
+
+      expect(getTypeName(schemaComposer, fullTypeName, false, packageName)).toBe(type);
+      expect(getTypeName(schemaComposer, fullTypeName, true, packageName)).toBe(type + inputFlag);
+    });
+
+    test('should return type with input flag postfix, when it is an input', () => {
+      expect(getTypeName(schemaComposer, type, true, packageName)).toBe(type + inputFlag);
+    });
+
+    test('should return type without input flag postfix, when it is an input, but also an enum in the schema', () => {
+      expect(getTypeName(schemaComposer, enumType, true, packageName)).toBe(enumType);
+    });
+  });
+
+  describe('createFieldsType', () => {
+    test('should create input and output field types', () => {
+      expect(createFieldsType('Dodgers')).toEqual({ input: 'DodgersInput', output: 'Dodgers' });
+    });
+  });
+
+  describe('toSnakeCase', () => {
+    test('should convert dot separated string to snake case', () => {
+      expect(toSnakeCase('Sports.Baseball.Team')).toEqual('Sports_Baseball_Team');
+    });
+  });
+});
