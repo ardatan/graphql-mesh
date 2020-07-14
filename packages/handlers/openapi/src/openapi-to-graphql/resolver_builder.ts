20bb5cf8bf38f45bd9f1db065afda53bca882934
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-07-14 16:26:23 +0300

    fix(openapi): don't set request content type if body is empty

--- packages/handlers/openapi/src/openapi-to-graphql/resolver_builder.ts
+++ packages/handlers/openapi/src/openapi-to-graphql/resolver_builder.ts
@@ -205,8 +205,14 @@ export function getResolver(getResolverParams: () => GetResolverParams): Resolve
      *
      * NOTE: This may cause the user to encounter unexpected changes
      */
-    headers['content-type'] =
-      typeof operation.payloadContentType !== 'undefined' ? operation.payloadContentType : 'application/json';
+    if (operation.method !== 'get') {
+      /**
+       * the check is done on the 'get' operation to determine if there is a payload because
+       * operation.payloadRequired is not always correctly initialized (at least during the unit test)
+       */
+      headers['content-type'] =
+        typeof operation.payloadContentType !== 'undefined' ? operation.payloadContentType : 'application/json';
+    }
     headers.accept =
       typeof operation.responseContentType !== 'undefined' ? operation.responseContentType : 'application/json';
 
