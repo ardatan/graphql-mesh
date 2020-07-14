de6022717c7ca93a233305d2324fc13e43efc7d9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-07-14 15:13:40 +0300

    feat(graphql-handler): Support string interpolation

--- packages/handlers/graphql/src/index.ts
+++ packages/handlers/graphql/src/index.ts
@@ -1,32 +1,68 @@
-import { MeshHandlerLibrary, YamlConfig } from '@graphql-mesh/types';
+import { MeshHandlerLibrary, ResolverData, YamlConfig } from '@graphql-mesh/types';
 import { fetchache, Request } from 'fetchache';
 import { UrlLoader } from '@graphql-tools/url-loader';
 import { GraphQLSchema, buildClientSchema, introspectionFromSchema } from 'graphql';
 import { introspectSchema } from '@graphql-tools/wrap';
+import { getInterpolatedHeadersFactory, ResolverDataBasedFactory } from '@graphql-mesh/utils';
+import { ExecutionParams } from '@graphql-tools/delegate';
 
 const handler: MeshHandlerLibrary<YamlConfig.GraphQLHandler> = {
   async getMeshSource({ name, config, cache }) {
     const urlLoader = new UrlLoader();
     const customFetch: WindowOrWorkerGlobalScope['fetch'] = (...args) =>
       fetchache(args[0] instanceof Request ? args[0] : new Request(...args), cache);
-    const { executor, subscriber } = await urlLoader.getExecutorAndSubscriber(config.endpoint, {
-      customFetch,
-      ...config,
-    });
+    const getExecutorAndSubscriberForParams = (
+      params: ExecutionParams,
+      headersFactory: ResolverDataBasedFactory<Headers>
+    ) => {
+      const resolverData: ResolverData = {
+        root: {},
+        args: params.variables,
+        context: params.context,
+      };
+      const headersObj = headersFactory(resolverData);
+      const headers: HeadersInit = {};
+      headersObj.forEach((val, key) => {
+        headers[key] = val;
+      });
+      return urlLoader.getExecutorAndSubscriber(config.endpoint, {
+        customFetch,
+        ...config,
+        headers,
+      });
+    };
     let schema: GraphQLSchema;
+    const schemaHeadersFactory = getInterpolatedHeadersFactory(config.schemaHeaders);
     if (config.cacheIntrospection) {
       const cacheKey = name + '_introspection';
       const introspectionData: any = await cache.get(cacheKey);
       if (introspectionData) {
         schema = buildClientSchema(introspectionData);
       } else {
-        schema = await introspectSchema(executor);
+        schema = await introspectSchema(async params => {
+          const { executor } = await getExecutorAndSubscriberForParams(params, schemaHeadersFactory);
+          return executor(params);
+        });
         await cache.set(cacheKey, introspectionFromSchema(schema) as any);
       }
     } else {
-      schema = await introspectSchema(executor);
+      schema = await introspectSchema(async params => {
+        const { executor } = await getExecutorAndSubscriberForParams(params, schemaHeadersFactory);
+        return executor(params);
+      });
     }
-    return { schema, executor, subscriber };
+    const operationHeadersFactory = getInterpolatedHeadersFactory(config.operationHeaders);
+    return {
+      schema,
+      executor: async params => {
+        const { executor } = await getExecutorAndSubscriberForParams(params, operationHeadersFactory);
+        return executor(params);
+      },
+      subscriber: async params => {
+        const { subscriber } = await getExecutorAndSubscriberForParams(params, operationHeadersFactory);
+        return subscriber(params);
+      },
+    };
   },
 };
 
--- packages/types/src/config.ts
+++ packages/types/src/config.ts
@@ -66,9 +66,15 @@ export interface GraphQLHandler {
    */
   endpoint: string;
   /**
-   * JSON object representing the Headers to add to the runtime of the API calls
+   * JSON object representing the Headers to add to the runtime of the API calls only for schema introspection
    */
-  headers?: {
+  schemaHeaders?: {
+    [k: string]: any;
+  };
+  /**
+   * JSON object representing the Headers to add to the runtime of the API calls only for operation during runtime
+   */
+  operationHeaders?: {
     [k: string]: any;
   };
   /**
@@ -78,7 +84,7 @@ export interface GraphQLHandler {
   /**
    * HTTP method used for GraphQL operations (Allowed values: GET, POST)
    */
-  method?: "GET" | "POST";
+  method?: 'GET' | 'POST';
   /**
    * Enable GraphQL Subscriptions using WebSocket
    */
@@ -166,11 +172,11 @@ export interface JsonSchemaOperation {
   /**
    * Allowed values: Query, Mutation
    */
-  type: "Query" | "Mutation";
+  type: 'Query' | 'Mutation';
   /**
    * Allowed values: GET, DELETE, POST, PUT
    */
-  method: "GET" | "DELETE" | "POST" | "PUT";
+  method: 'GET' | 'DELETE' | 'POST' | 'PUT';
   requestSchema?: string;
   responseSchema?: string;
   requestSample?: string;
@@ -440,7 +446,7 @@ export interface ODataHandler {
   /**
    * Enable batching (Allowed values: multipart, json)
    */
-  batch?: "multipart" | "json";
+  batch?: 'multipart' | 'json';
   /**
    * Use $expand for navigation props instead of seperate HTTP requests (Default: false)
    */
@@ -587,7 +593,7 @@ export interface ThriftHandler {
   /**
    * Name of the Thrift protocol type to use. Defaults to 'binary'. (Allowed values: binary, compact, json)
    */
-  protocol?: "binary" | "compact" | "json";
+  protocol?: 'binary' | 'compact' | 'json';
   /**
    * The name of your service. Used for logging.
    */
@@ -779,53 +785,53 @@ export interface NamingConventionTransformConfig {
    * Allowed values: camelCase, capitalCase, constantCase, dotCase, headerCase, noCase, paramCase, pascalCase, pathCase, sentenceCase, snakeCase, upperCase, lowerCase
    */
   typeNames?:
-    | "camelCase"
-    | "capitalCase"
-    | "constantCase"
-    | "dotCase"
-    | "headerCase"
-    | "noCase"
-    | "paramCase"
-    | "pascalCase"
-    | "pathCase"
-    | "sentenceCase"
-    | "snakeCase"
-    | "upperCase"
-    | "lowerCase";
+    | 'camelCase'
+    | 'capitalCase'
+    | 'constantCase'
+    | 'dotCase'
+    | 'headerCase'
+    | 'noCase'
+    | 'paramCase'
+    | 'pascalCase'
+    | 'pathCase'
+    | 'sentenceCase'
+    | 'snakeCase'
+    | 'upperCase'
+    | 'lowerCase';
   /**
    * Allowed values: camelCase, capitalCase, constantCase, dotCase, headerCase, noCase, paramCase, pascalCase, pathCase, sentenceCase, snakeCase, upperCase, lowerCase
    */
   fieldNames?:
-    | "camelCase"
-    | "capitalCase"
-    | "constantCase"
-    | "dotCase"
-    | "headerCase"
-    | "noCase"
-    | "paramCase"
-    | "pascalCase"
-    | "pathCase"
-    | "sentenceCase"
-    | "snakeCase"
-    | "upperCase"
-    | "lowerCase";
+    | 'camelCase'
+    | 'capitalCase'
+    | 'constantCase'
+    | 'dotCase'
+    | 'headerCase'
+    | 'noCase'
+    | 'paramCase'
+    | 'pascalCase'
+    | 'pathCase'
+    | 'sentenceCase'
+    | 'snakeCase'
+    | 'upperCase'
+    | 'lowerCase';
   /**
    * Allowed values: camelCase, capitalCase, constantCase, dotCase, headerCase, noCase, paramCase, pascalCase, pathCase, sentenceCase, snakeCase, upperCase, lowerCase
    */
   enumValues?:
-    | "camelCase"
-    | "capitalCase"
-    | "constantCase"
-    | "dotCase"
-    | "headerCase"
-    | "noCase"
-    | "paramCase"
-    | "pascalCase"
-    | "pathCase"
-    | "sentenceCase"
-    | "snakeCase"
-    | "upperCase"
-    | "lowerCase";
+    | 'camelCase'
+    | 'capitalCase'
+    | 'constantCase'
+    | 'dotCase'
+    | 'headerCase'
+    | 'noCase'
+    | 'paramCase'
+    | 'pascalCase'
+    | 'pathCase'
+    | 'sentenceCase'
+    | 'snakeCase'
+    | 'upperCase'
+    | 'lowerCase';
 }
 /**
  * Prefix transform
