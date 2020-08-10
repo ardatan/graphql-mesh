9bfdf769bc44067c4874944964616f1cbe6b4a22
Arda Tanrıkulu <20847995+ardatan@users.noreply.github.com>
2020-08-10 04:45:15 -0400

    fix(openapi): handle integers better for enums and 64-bit values (#784)
    * fix(openapi): do not create enum if type is integer
    
    * Create enum only if the values are string
    
    * Use GraphQLBigInt for 64 bit integers

--- packages/handlers/openapi/test/example_api.test.ts
+++ packages/handlers/openapi/test/example_api.test.ts
@@ -631,7 +631,7 @@ test('Ensure good naming for operations with duplicated schemas', () => {
  * CASE: 64 bit int - return number instead of integer, leading to use of
  * GraphQLFloat, which can support 64 bits:
  */
-test('Get response containing 64-bit integer (using GraphQLFloat)', () => {
+test('Get response containing 64-bit integer (using GraphQLBigInt)', () => {
   const query = `{
     productReviews (id: "100") {
       timestamp
@@ -641,7 +641,7 @@ test('Get response containing 64-bit integer (using GraphQLFloat)', () => {
   return graphql(createdSchema, query).then(result => {
     expect(result).toEqual({
       data: {
-        productReviews: [{ timestamp: 1502787600000000 }, { timestamp: 1502787400000000 }],
+        productReviews: [{ timestamp: 1502787600000000n }, { timestamp: 1502787400000000n }],
       },
     });
   });
@@ -929,24 +929,6 @@ test('Capitalized enum values can be returned', () => {
       data: {
         car: {
           kind: 'SEDAN',
-        },
-      },
-    });
-  });
-});
-
-test('Enum values that started as numbers in OAS can be returned as strings', () => {
-  const query = `{
-    car (username: "arlene") {
-      rating
-    }
-  }`;
-
-  return graphql(createdSchema, query, null, {}).then(result => {
-    expect(result).toEqual({
-      data: {
-        car: {
-          rating: '_100',
         },
       },
     });
