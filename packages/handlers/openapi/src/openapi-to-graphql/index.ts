2833112cc29dc5e13a8de15dbcfb2f8c18c8689a
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-08-16 14:43:11 -0400

    Fix tests

--- packages/handlers/openapi/src/openapi-to-graphql/index.ts
+++ packages/handlers/openapi/src/openapi-to-graphql/index.ts
@@ -88,8 +88,6 @@ export async function createGraphQLSchema(spec: Oas3 | Oas2 | (Oas3 | Oas2)[], o
   options.equivalentToMessages =
     typeof options.equivalentToMessages === 'boolean' ? options.equivalentToMessages : true;
 
-  options.fetch = typeof options.fetch === 'function' ? options.fetch : await import('cross-fetch').then(m => m.fetch);
-
   options.resolverMiddleware =
     typeof options.resolverMiddleware === 'function'
       ? options.resolverMiddleware
