3e7beb5490ccea476c2af97c9c58e71a74667351
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-08-26 09:04:45 +0300

    feat(config): able to configure serve command in mesh config file

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -20,28 +20,15 @@ const logger = createLogger({
 
 export async function graphqlMesh() {
   return yargs
-    .command<{ fork: string | number; port: string | number; 'example-query': string }>(
+    .command(
       'serve',
       'Serves a GraphiQLApolloServer interface to test your Mesh API',
-      builder => {
-        builder.option('fork', {
-          required: false,
-          number: true,
-          count: true,
-        });
-        builder.option('port', {
-          required: false,
-        });
-        builder.option('example-query', {
-          required: false,
-          string: true,
-        });
-      },
-      async ({ fork, port, 'example-query': exampleQuery }) => {
+      builder => {},
+      async () => {
         try {
           const meshConfig = await findAndParseConfig();
           const { schema, contextBuilder } = await getMesh(meshConfig);
-          await serveMesh(logger, schema, contextBuilder, fork, port, exampleQuery);
+          await serveMesh(logger, schema, contextBuilder, meshConfig.config.serve);
         } catch (e) {
           logger.error('Unable to serve mesh: ', e);
         }
