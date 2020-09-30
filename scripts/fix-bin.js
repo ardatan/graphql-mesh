e9f2ba2f43300f79f6930a845ac4ec2f75621547
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-09-30 11:21:28 +0300

    Improve fix-bin.js

--- scripts/fix-bin.js
+++ scripts/fix-bin.js
@@ -7,8 +7,11 @@ const dir = readdirSync(absoluteExamplesDirPath);
 for (const path of dir) {
     const absolutePath = join(absoluteExamplesDirPath, path);
     if (lstatSync(absolutePath).isDirectory()) {
-        const targetPath = join(absolutePath, 'node_modules', '.bin', 'graphql-mesh');
+        const execNames = ['mesh', 'gql-mesh', 'graphql-mesh'];
+        for (const execName of execNames) {
+        const targetPath = join(absolutePath, 'node_modules', '.bin', execName);
         ensureSymlinkSync(absoluteGraphqlMeshBinPath, targetPath);
         chmodSync(targetPath, '755');
+        }
     }
 }
