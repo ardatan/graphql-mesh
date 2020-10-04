1e0445eec71edb78194366c8a92f43c50947ab58
Arda Tanrıkulu <20847995+ardatan@users.noreply.github.com>
2020-10-04 18:16:20 +0300

    feat(neo4j): add typeDefs option to provide custom schema (#1024)

--- packages/handlers/neo4j/src/index.ts
+++ packages/handlers/neo4j/src/index.ts
@@ -1,10 +1,15 @@
 import { makeAugmentedSchema, inferSchema } from 'neo4j-graphql-js';
 import neo4j, { Driver } from 'neo4j-driver';
 import { YamlConfig, MeshHandler, GetMeshSourceOptions, KeyValueCache, MeshPubSub } from '@graphql-mesh/types';
+import { loadTypedefs } from '@graphql-tools/load';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { CodeFileLoader } from '@graphql-tools/code-file-loader';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+import { DocumentNode } from 'graphql';
 
 export default class Neo4JHandler implements MeshHandler {
   private name: string;
-  private cache: KeyValueCache;
+  private cache: KeyValueCache<any>;
   private config: YamlConfig.Neo4JHandler;
   private pubsub: MeshPubSub;
 
@@ -26,22 +31,33 @@ export default class Neo4JHandler implements MeshHandler {
   }
 
   async getMeshSource() {
-    let typeDefs: string;
+    let typeDefs: DocumentNode | string;
 
     const cacheKey = this.name + '_introspection';
 
-    if (this.config.cacheIntrospection) {
-      typeDefs = await this.cache.get(cacheKey);
-    }
-
-    if (!typeDefs) {
-      const inferredSchema = await inferSchema(this.getDriver(), {
-        alwaysIncludeRelationships: this.config.alwaysIncludeRelationships,
+    if (this.config.typeDefs) {
+      const typeDefsArr = await loadTypedefs(this.config.typeDefs, {
+        loaders: [new GraphQLFileLoader(), new CodeFileLoader()],
+        assumeValid: true,
+        assumeValidSDL: true,
       });
-      typeDefs = inferredSchema.typeDefs;
-
+      typeDefs = mergeTypeDefs(typeDefsArr.map(source => source.document));
+    } else {
       if (this.config.cacheIntrospection) {
-        await this.cache.set(cacheKey, typeDefs);
+        typeDefs = await this.cache.get(cacheKey);
+      }
+
+      if (!typeDefs) {
+        const inferredSchema = await inferSchema(this.getDriver(), {
+          alwaysIncludeRelationships: this.config.alwaysIncludeRelationships,
+        });
+        typeDefs = inferredSchema.typeDefs;
+
+        if (this.config.cacheIntrospection) {
+          await this.cache.set(cacheKey, typeDefs, {
+            ttl: typeof this.config.cacheIntrospection === 'object' && this.config.cacheIntrospection.ttl,
+          });
+        }
       }
     }
 
