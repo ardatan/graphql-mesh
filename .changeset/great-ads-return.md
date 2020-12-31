44348de0565f362b5b32a17ea7f7629b341a9e41
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-12-31 14:39:07 +0300

    Introduce merger-bare

---
+++ .changeset/great-ads-return.md
@@ -0,0 +1,5 @@
+---
+'@graphql-mesh/merger-bare': minor
+---
+
+Introduce merger-bare
---
+++ packages/mergers/bare/src/index.ts
@@ -0,0 +1,53 @@
+import { MergerFn, MeshTransform } from '@graphql-mesh/types';
+import { wrapSchema } from '@graphql-tools/wrap';
+import { groupTransforms, applySchemaTransforms } from '@graphql-mesh/utils';
+import { mergeSchemas } from '@graphql-tools/merge';
+
+const mergeBare: MergerFn = ({ rawSources, typeDefs, resolvers, transforms }) => {
+  const sourceMap = new Map();
+  const schemas = rawSources.map(source => {
+    let schema = source.schema;
+
+    if (source.executor || source.subscriber || source.transforms.length) {
+      schema = wrapSchema({
+        ...source,
+        schema,
+        transforms: source.transforms,
+      });
+    }
+    sourceMap.set(source, schema);
+    return schema;
+  });
+
+  let schema = mergeSchemas({
+    schemas,
+    typeDefs,
+    resolvers,
+  });
+
+  let wrapTransforms: MeshTransform[] = [];
+  let noWrapTransforms: MeshTransform[] = [];
+
+  if (transforms?.length) {
+    const transformGroups = groupTransforms(transforms);
+    wrapTransforms = transformGroups.wrapTransforms;
+    noWrapTransforms = transformGroups.noWrapTransforms;
+  }
+
+  if (wrapTransforms.length) {
+    schema = wrapSchema({
+      schema,
+      transforms: [...wrapTransforms, ...noWrapTransforms],
+    });
+  } else if (noWrapTransforms.length) {
+    schema = applySchemaTransforms(schema, undefined, schema, noWrapTransforms);
+  }
+
+  schema.extensions = schema.extensions || {};
+  Object.defineProperty(schema.extensions, 'sourceMap', {
+    get: () => sourceMap,
+  });
+  return schema;
+};
+
+export default mergeBare;
