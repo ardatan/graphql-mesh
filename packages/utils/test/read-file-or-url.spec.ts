e9fc4677bce75f8083405345499b0c0cbe3357a7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-02-03 16:37:52 +0300

    chore(utils): add tests for isAbsolute check

---
+++ packages/utils/test/read-file-or-url.spec.ts
@@ -0,0 +1,29 @@
+import { writeFileSync } from 'fs';
+import { tmpdir } from 'os';
+import InMemoryLRUCache from '@graphql-mesh/cache-inmemory-lru';
+import { join, relative } from 'path';
+import { readFileWithCache } from '../src/read-file-or-url';
+
+describe('readFileWithCache', () => {
+  it('should convert relative paths to absolute paths correctly', async () => {
+    const cache = new InMemoryLRUCache();
+    const tmpFileAbsolutePath = join(tmpdir(), './tmpfile.json');
+    const tmpFileContent = {
+      test: 'TEST',
+    };
+    writeFileSync(tmpFileAbsolutePath, JSON.stringify(tmpFileContent));
+    const tmpFileRelativePath = relative(process.cwd(), tmpFileAbsolutePath);
+    const receivedFileContent = await readFileWithCache(tmpFileRelativePath, cache);
+    expect(receivedFileContent).toStrictEqual(tmpFileContent);
+  });
+  it('should respect absolute paths correctly', async () => {
+    const cache = new InMemoryLRUCache();
+    const tmpFileAbsolutePath = join(tmpdir(), './tmpfile.json');
+    const tmpFileContent = {
+      test: 'TEST',
+    };
+    writeFileSync(tmpFileAbsolutePath, JSON.stringify(tmpFileContent));
+    const receivedFileContent = await readFileWithCache(tmpFileAbsolutePath, cache);
+    expect(receivedFileContent).toStrictEqual(tmpFileContent);
+  });
+});
--- packages/utils/test/utils.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('utils', () => {
-  it('dummy', async () => {});
-});
