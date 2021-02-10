03741895ff24baa5ec0fd8fccdf2abc3d8336ceb
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-02-10 16:36:09 +0300

    Improvements

--- packages/cli/src/commands/serve/playground.ts
+++ packages/cli/src/commands/serve/playground.ts
@@ -1,21 +1,43 @@
+import { CodeFileLoader } from '@graphql-tools/code-file-loader';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadDocuments } from '@graphql-tools/load';
 import { Request, Response, RequestHandler } from 'express';
 import { renderGraphiQL } from 'graphql-helix';
 
+let defaultQuery$: Promise<string>;
+
 export const playgroundMiddlewareFactory = ({
-  defaultQuery,
+  baseDir,
+  exampleQuery,
   graphqlPath,
 }: {
-  defaultQuery: string;
+  baseDir: string;
+  exampleQuery: string;
   graphqlPath: string;
 }): RequestHandler =>
   function (req: Request, res: Response, next) {
+    defaultQuery$ =
+      defaultQuery$ ||
+      Promise.resolve().then(async () => {
+        let defaultQuery: string;
+        if (exampleQuery) {
+          const documents = await loadDocuments(exampleQuery, {
+            loaders: [new CodeFileLoader(), new GraphQLFileLoader()],
+            cwd: baseDir,
+          });
+
+          defaultQuery = documents.reduce((acc, doc) => (acc += doc.rawSDL! + '\n'), '');
+        }
+        return defaultQuery;
+      });
     if (req.query.query) {
       next();
       return;
     }
 
-    res.send(
-      `
+    defaultQuery$.then(defaultQuery => {
+      res.send(
+        `
   <script>
     let fakeStorageObj = {};
     const fakeStorageInstance = {
@@ -42,9 +64,10 @@ export const playgroundMiddlewareFactory = ({
       value: fakeStorageInstance,
     });
   </script>` +
-        renderGraphiQL({
-          defaultQuery,
-          endpoint: graphqlPath,
-        })
-    );
+          renderGraphiQL({
+            defaultQuery,
+            endpoint: graphqlPath,
+          })
+      );
+    });
   };
