6dbfa796f7200d5048814a0b3643ef6a69435bab
Santino Puleio <santinopuleio@gmail.com>
2021-02-10 21:00:05 +0100

    Introduce RegExp matching to bare-rename transform
    according to existing API for rename transform that uses useRegExpForTypes and useRegExpForFields properties

--- packages/transforms/rename/src/bareRename.ts
+++ packages/transforms/rename/src/bareRename.ts
@@ -2,10 +2,11 @@ import { GraphQLSchema, GraphQLFieldConfig } from 'graphql';
 import { MeshTransform, MeshTransformOptions, YamlConfig } from '@graphql-mesh/types';
 import { renameType, MapperKind, mapSchema } from '@graphql-tools/utils';
 
+type RenameMapObject = Map<string | RegExp, string>;
 export default class BareRename implements MeshTransform {
   noWrap = true;
-  typesMap: Map<string, string>;
-  fieldsMap: Map<string, string>;
+  typesMap: RenameMapObject;
+  fieldsMap: Map<string, RenameMapObject>;
 
   constructor(options: MeshTransformOptions<YamlConfig.RenameTransform>) {
     const { config } = options;
@@ -16,18 +17,39 @@ export default class BareRename implements MeshTransform {
       const {
         from: { type: fromTypeName, field: fromFieldName },
         to: { type: toTypeName, field: toFieldName },
+        useRegExpForTypes,
+        useRegExpForFields,
       } = rename;
       if (fromTypeName && !fromFieldName && toTypeName && !toFieldName && fromTypeName !== toTypeName) {
-        this.typesMap.set(fromTypeName, toTypeName);
+        this.typesMap.set(useRegExpForTypes ? new RegExp(fromTypeName) : fromTypeName, toTypeName);
       }
       if (fromTypeName && fromFieldName && toTypeName && toFieldName && fromFieldName !== toFieldName) {
-        this.fieldsMap.set(`${fromTypeName}_${fromFieldName}`, toFieldName);
+        const fromName = useRegExpForFields ? new RegExp(fromFieldName) : fromFieldName;
+        this.fieldsMap.set(
+          fromTypeName,
+          this.fieldsMap.has(fromTypeName)
+            ? this.fieldsMap.get(fromTypeName).set(fromName, toFieldName)
+            : new Map().set(fromName, toFieldName)
+        );
       }
     }
   }
 
+  matchInMap(map: RenameMapObject, toMatch: string) {
+    const mapKeyIsString = map.has(toMatch);
+    const mapKey = mapKeyIsString ? toMatch : [...map.keys()].find(key => typeof key !== 'string' && key.test(toMatch));
+    if (!mapKey) return null;
+
+    const newName = mapKeyIsString ? map.get(mapKey) : toMatch.replace(mapKey, map.get(mapKey));
+
+    // avoid iterating over strings that have already been renamed
+    if (mapKeyIsString) map.delete(mapKey);
+
+    return newName;
+  }
+
   renameType(type: any) {
-    const newTypeName = this.typesMap.get(type.toString());
+    const newTypeName = this.matchInMap(this.typesMap, type.toString());
     return newTypeName ? renameType(type, newTypeName) : undefined;
   }
 
@@ -42,8 +64,14 @@ export default class BareRename implements MeshTransform {
         fieldName: string,
         typeName: string
       ) => {
-        const newFieldName = this.fieldsMap.get(`${typeName}_${fieldName}`);
+        const mapType = this.fieldsMap.get(typeName);
+        if (!mapType) return undefined;
+
+        const newFieldName = this.matchInMap(mapType, fieldName);
         if (!newFieldName) return undefined;
+
+        // remove type with emptied rules from Map
+        if (!mapType.size) this.fieldsMap.delete(typeName);
 
         // Root fields always have a default resolver and so don't need mapping
         if (!rootFields.includes(typeName)) {
