f13e4e616b1ba71086d2b1f0b5e5bbdf5583afae
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-02-26 01:36:39 +0300

    Improve CI (#1704)
    * Improve CI
    
    * check caching
    
    * do not use globs
    
    * use ts-jest again
    
    * another attempts
    
    * Better cache keys'
    
    * better cache keys
    
    * BETTER BETTER CACHE KEYS
    
    * ANOTHER ROUND

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-proposal-class-properties'],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
 };
