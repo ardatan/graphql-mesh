7765ca34bb4133afdc622d0607a1ee4f75c358c8
ntziolis <nikolas@tziolis.de>
2021-04-30 10:30:59 +0200

    fix(playground): enable subscriptions
    It was not possible to run subscriptions in the graphiql interface that is started after a mesh starts since `subscriptionsEndpoint` was not specified.

--- packages/cli/src/commands/serve/playground.ts
+++ packages/cli/src/commands/serve/playground.ts
@@ -69,6 +69,7 @@ export const playgroundMiddlewareFactory = ({
           renderGraphiQL({
             defaultQuery,
             endpoint: graphqlPath,
+            subscriptionsEndpoint: graphqlPath
           })
       );
     });
