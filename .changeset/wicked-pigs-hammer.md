acb45130b005ed4fcf3b688a298e3dba8db130e9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-05-04 18:45:47 +0300

    Improve Mocking transform (#2001)
    * [1909] Extend the mock test cases - draft 1
    
    * fix(mock): fix field mockings
    
    * Fix build
    
    * feat(mock): initial impl of stateful mocking
    
    * Fix docs
    
    * Another fix
    
    * Update tests
    
    * Update test name
    
    * Update docs
    
    * Add declarative API
    
    * Fix type mocks
    
    * initializeStore
    
    Co-authored-by: Nemanja Milosavljevic <nemanjamilosavljevic@gmail.com>

---
+++ .changeset/wicked-pigs-hammer.md
@@ -0,0 +1,5 @@
+---
+'@graphql-mesh/transform-mock': patch
+---
+
+fix(mock): fix field mockings
--- packages/types/src/config.ts
+++ packages/types/src/config.ts
@@ -1040,6 +1040,10 @@ export interface MockingConfig {
    * Mock configurations
    */
   mocks?: MockingFieldConfig[];
+  /**
+   * The path to the code runs before the store is attached to the schema
+   */
+  initializeStore?: any;
 }
 export interface MockingFieldConfig {
   /**
@@ -1066,6 +1070,31 @@ export interface MockingFieldConfig {
    * Both "moduleName#exportName" or only "moduleName" would work
    */
   custom?: string;
+  /**
+   * Length of the mock list
+   * For the list types `[ObjectType]`, how many `ObjectType` you want to return?
+   * default: 2
+   */
+  length?: number;
+  store?: GetFromMockStoreConfig;
+  /**
+   * Update the data on the mock store
+   */
+  updateStore?: UpdateMockStoreConfig[];
+}
+/**
+ * Get the data from the mock store
+ */
+export interface GetFromMockStoreConfig {
+  type?: string;
+  key?: string;
+  fieldName?: string;
+}
+export interface UpdateMockStoreConfig {
+  type?: string;
+  key?: string;
+  fieldName?: string;
+  value?: string;
 }
 /**
  * Transformer to apply naming convention to GraphQL Types
