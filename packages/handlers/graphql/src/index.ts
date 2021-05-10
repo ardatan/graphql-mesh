674bbadc925fd9de40f53d99bf849c226902bc31
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-05-10 23:47:55 +0300

    fix(graphql): respect introspection cache for federation (#2127)
    * fix(graphql): respect introspection cache for federation
    
    * use getQueryType instead

--- packages/handlers/graphql/src/index.ts
+++ packages/handlers/graphql/src/index.ts
@@ -7,7 +7,16 @@ import {
   KeyValueCache,
 } from '@graphql-mesh/types';
 import { UrlLoader } from '@graphql-tools/url-loader';
-import { GraphQLSchema, buildClientSchema, introspectionFromSchema, buildSchema, IntrospectionQuery } from 'graphql';
+import {
+  GraphQLSchema,
+  buildClientSchema,
+  introspectionFromSchema,
+  buildSchema,
+  IntrospectionQuery,
+  parse,
+  execute,
+  GraphQLResolveInfo,
+} from 'graphql';
 import { introspectSchema } from '@graphql-tools/wrap';
 import {
   getInterpolatedHeadersFactory,
@@ -21,7 +30,16 @@ import { ExecutionParams, AsyncExecutor } from '@graphql-tools/delegate';
 
 export interface GraphQLHandlerIntrospection {
   introspection?: IntrospectionQuery;
+  sdl?: string;
 }
+
+const APOLLO_GET_SERVICE_DEFINITION_QUERY = /* GraphQL */ `
+  query __ApolloGetServiceDefinition__ {
+    _service {
+      sdl
+    }
+  }
+`;
 
 export default class GraphQLHandler implements MeshHandler {
   private config: YamlConfig.GraphQLHandler;
@@ -87,11 +105,11 @@ export default class GraphQLHandler implements MeshHandler {
       schemaHeaders = await schemaHeaders;
     }
     const schemaHeadersFactory = getInterpolatedHeadersFactory(schemaHeaders || {});
+    const introspectionExecutor: AsyncExecutor = async (params): Promise<any> => {
+      const { executor } = await getExecutorAndSubscriberForParams(params, schemaHeadersFactory, () => endpoint);
+      return executor(params);
+    };
     if (!this.introspectionCache.introspection) {
-      const introspectionExecutor: AsyncExecutor = async (params): Promise<any> => {
-        const { executor } = await getExecutorAndSubscriberForParams(params, schemaHeadersFactory, () => endpoint);
-        return executor(params);
-      };
       if (introspection) {
         const result = await urlLoader.handleSDLAsync(introspection, {
           customFetch,
@@ -106,9 +124,34 @@ export default class GraphQLHandler implements MeshHandler {
     const nonExecutableSchema = buildClientSchema(this.introspectionCache.introspection);
     const operationHeadersFactory = getInterpolatedHeadersFactory(this.config.operationHeaders);
     const endpointFactory = getInterpolatedStringFactory(endpoint);
+    const queryType = nonExecutableSchema.getQueryType();
+    const queryTypeFieldMap = queryType.getFields();
+    if ('_service' in queryTypeFieldMap) {
+      const _serviceField = queryTypeFieldMap._service;
+      if ('resolve' in _serviceField) {
+        _serviceField.resolve = async () => {
+          if (!this.introspectionCache.sdl) {
+            const sdlQueryResult = await introspectionExecutor({
+              document: parse(APOLLO_GET_SERVICE_DEFINITION_QUERY),
+            });
+            this.introspectionCache.sdl = sdlQueryResult?.data?._service?.sdl;
+          }
+          return {
+            sdl: this.introspectionCache.sdl,
+          };
+        };
+      }
+    }
+    const isSdlQuery = (info: GraphQLResolveInfo) =>
+      info.fieldName === '_service' &&
+      info.fieldNodes[0].selectionSet.selections[0].kind === 'Field' &&
+      info.fieldNodes[0].selectionSet.selections[0].name.value === 'sdl';
     return {
       schema: nonExecutableSchema,
       executor: async params => {
+        if (isSdlQuery(params.info)) {
+          return execute(nonExecutableSchema, params.document);
+        }
         const { executor } = await getExecutorAndSubscriberForParams(params, operationHeadersFactory, endpointFactory);
         return executor(params) as any;
       },
