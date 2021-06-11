644c280069f30c7b3662bfcfd249e53ca9286463
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-06-11 14:51:48 +0300

    enhance(redis): enable auto-pipelining and remove dataloader

---
+++ .changeset/fair-seals-act.md
@@ -0,0 +1,5 @@
+---
+'@graphql-mesh/cache-redis': patch
+---
+
+enhance(redis): enable auto-pipelining and remove dataloader
--- packages/cache/redis/src/index.ts
+++ packages/cache/redis/src/index.ts
@@ -1,29 +1,31 @@
-import { KeyValueCache, KeyValueCacheSetOptions } from '@graphql-mesh/types';
-import Redis, { RedisOptions } from 'ioredis';
-import DataLoader from 'dataloader';
+import { KeyValueCache, KeyValueCacheSetOptions, YamlConfig } from '@graphql-mesh/types';
+import Redis from 'ioredis';
 import { jsonFlatStringify } from '@graphql-mesh/utils';
 
 export default class RedisCache<V = string> implements KeyValueCache<V> {
   readonly client: Redis.Redis;
 
-  private loader: DataLoader<string, string | null>;
-
-  constructor(options?: RedisOptions) {
-    this.client = new Redis(options);
-
-    this.loader = new DataLoader(keys => this.client.mget(...keys), {
-      cache: false,
+  constructor(options: YamlConfig.Transform['redis'] = {}) {
+    this.client = new Redis({
+      host: options.host,
+      port: options.port,
+      password: options.password,
+      lazyConnect: true,
+      enableAutoPipelining: true,
     });
   }
 
   async set(key: string, value: V, options?: KeyValueCacheSetOptions): Promise<void> {
-    const ttl = options?.ttl || 300;
     const stringifiedValue = jsonFlatStringify(value);
-    await this.client.set(key, stringifiedValue, 'EX', ttl);
+    if (options?.ttl) {
+      await this.client.set(key, stringifiedValue, 'EX', options.ttl);
+    } else {
+      await this.client.set(key, stringifiedValue);
+    }
   }
 
   async get(key: string): Promise<V | undefined> {
-    const reply = await this.loader.load(key);
+    const reply = await this.client.get(key);
     if (reply !== null) {
       const value = JSON.parse(reply);
       return value;
