8374bc1791ec0e54b37e21df4a7fbe74536083a3
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-07-01 15:11:46 +0300

    Federation test

--- examples/federation-example/tests/federation-example.test.js
+++ examples/federation-example/tests/federation-example.test.js
@@ -2,7 +2,7 @@ const { findAndParseConfig } = require('@graphql-mesh/config');
 const { getMesh } = require('@graphql-mesh/runtime');
 const { basename, join } = require('path');
 
-const { lexicographicSortSchema, printSchema } = require('graphql');
+const { lexicographicSortSchema } = require('graphql');
 const { mkdirSync, writeFileSync } = require('fs');
 const { printSchemaWithDirectives } = require('@graphql-tools/utils');
 
@@ -30,7 +30,7 @@ describe('Federation Example', () => {
   it('should generate correct schema', async () => {
     const { schema } = await mesh$;
     expect(
-      printSchema(lexicographicSortSchema(schema), {
+      printSchemaWithDirectives(lexicographicSortSchema(schema), {
         descriptions: false,
       })
     ).toMatchSnapshot('federation-example-schema');
