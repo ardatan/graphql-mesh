0dbaf03201ad472285f9ced6bef6346226e1518a
Sven-Erik Kägebein <s.kaegebein@gmx.de>
2021-08-10 08:02:10 +0200

    fix(lint): remove unused import

--- packages/transforms/rename/src/wrapRename.ts
+++ packages/transforms/rename/src/wrapRename.ts
@@ -1,12 +1,6 @@
 import { GraphQLSchema } from 'graphql';
 import { MeshTransform, YamlConfig, MeshTransformOptions } from '@graphql-mesh/types';
-import {
-  RenameTypes,
-  RenameObjectFields,
-  RenameRootFields,
-  RenameRootTypes,
-  RenameInputObjectFields,
-} from '@graphql-tools/wrap';
+import { RenameTypes, RenameObjectFields, RenameRootTypes, RenameInputObjectFields } from '@graphql-tools/wrap';
 import { ExecutionResult, ExecutionRequest } from '@graphql-tools/utils';
 import { Transform, SubschemaConfig, DelegationContext } from '@graphql-tools/delegate';
 import { applyRequestTransforms, applyResultTransforms, applySchemaTransforms } from '@graphql-mesh/utils';
