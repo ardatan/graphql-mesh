4bb82f911f141c4ab48bbb24adabf17e53110a0d
enisdenjo <badurinadenis@gmail.com>
2021-08-15 12:21:45 +0200

    docs: we do completely ignore

--- packages/cli/src/commands/serve/serve.ts
+++ packages/cli/src/commands/serve/serve.ts
@@ -116,7 +116,7 @@ export async function serveMesh({ baseDir, argsPort, getBuiltMesh, logger, rawCo
           ),
         context: async ({ connectionParams, extra: { request } }) => {
           // spread connectionParams.headers to upgrade request headers.
-          // we dont completely ignore the root connectionParams because
+          // we completely ignore the root connectionParams because
           // [@graphql-tools/url-loader adds the headers there](https://github.com/ardatan/graphql-tools/blob/9a13357c4be98038c645f6efb26f0584828177cf/packages/loaders/url/src/index.ts#L597)
           for (const [key, value] of Object.entries(connectionParams.headers ?? {})) {
             // dont overwrite existing upgrade headers due to security reasons
