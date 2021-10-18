c4f3f8300688a789b3212c9e0e70235525b5f784
ntziolis <nikolas@tziolis.de>
2021-10-18 15:59:31 +0200

    feat(cache): only make a request when no active request for this cache key exists

--- packages/transforms/cache/src/index.ts
+++ packages/transforms/cache/src/index.ts
@@ -7,7 +7,15 @@ import { GraphQLSchema } from 'graphql';
 
 export default class CacheTransform implements MeshTransform {
   noWrap = true;
+  private activeRequests: {
+    [cacheKey: string]: {
+      complete: () => void;
+      completed: Promise<void>;
+    };
+  } = {};
+
   constructor(private options: MeshTransformOptions<YamlConfig.CacheTransformConfig[]>) {}
+
   transformSchema(schema: GraphQLSchema) {
     const { config, pubsub, cache } = this.options;
     const sourceResolvers = extractResolvers(schema);
@@ -41,19 +49,36 @@ export default class CacheTransform implements MeshTransform {
           info,
         });
 
-        const cachedValue = await cache.get(cacheKey);
+        let cachedValue = await cache.get(cacheKey);
+
+        // this needs to be a loop because last request is async
+        // and we need to ensure that no other task in the event loop
+        // has already started a request for the current cacheKey
+        while (!cachedValue && this.activeRequests[cacheKey]) {
+          await this.activeRequests[cacheKey].completed;
+          cachedValue = await cache.get(cacheKey);
+        }
 
         if (cachedValue) {
           return cachedValue;
         }
 
-        const resolverResult = await originalResolver(root, args, context, info);
+        this.startRequest(cacheKey);
 
-        await cache.set(cacheKey, resolverResult, {
-          ttl: cacheItem.invalidate?.ttl || undefined,
-        });
+        try {
+          const resolverResult = await originalResolver(root, args, context, info);
 
-        return resolverResult;
+          await cache.set(cacheKey, resolverResult, {
+            ttl: cacheItem.invalidate?.ttl || undefined,
+          });
+
+          this.completeRequest(cacheKey);
+
+          return resolverResult;
+        } catch (error) {
+          this.completeRequest(cacheKey);
+          throw error;
+        }
       }) as ResolversComposition;
     }
 
@@ -63,5 +88,23 @@ export default class CacheTransform implements MeshTransform {
       resolvers: wrappedResolvers,
       updateResolversInPlace: true,
     });
+  }
+
+  private startRequest(cacheKey: string) {
+    let resolveActiveRequestFn;
+    const completed = new Promise<void>(resolve => {
+      resolveActiveRequestFn = resolve;
+    });
+
+    this.activeRequests[cacheKey] = {
+      complete: resolveActiveRequestFn,
+      completed,
+    };
+  }
+
+  private completeRequest(cacheKey: string) {
+    const activeRequest = this.activeRequests[cacheKey];
+    delete this.activeRequests[cacheKey];
+    activeRequest.complete();
   }
 }
