ae24a2969caccc73f9a4dc302de994ba92bdd2cf
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-10-21 04:19:42 +0300

    chore(cli-serve/playground): update graphql-helix and url-loader

---
+++ .changeset/chilly-peas-thank.md
@@ -0,0 +1,6 @@
+---
+'@graphql-mesh/cli': patch
+'@graphql-mesh/playground': patch
+---
+
+chore(cli-serve/playground): update graphql-helix and url-loader
--- packages/cli/src/commands/serve/graphql-handler.ts
+++ packages/cli/src/commands/serve/graphql-handler.ts
@@ -1,9 +1,8 @@
 import { getMesh } from '@graphql-mesh/runtime';
-import { flatString, jsonFlatStringify, parseWithCache } from '@graphql-mesh/utils';
+import { parseWithCache } from '@graphql-mesh/utils';
 import { RequestHandler } from 'express';
 import { GraphQLError } from 'graphql';
-import { getGraphQLParameters, processRequest, shouldRenderGraphiQL } from 'graphql-helix';
-import { inspect } from '@graphql-tools/utils';
+import { getGraphQLParameters, processRequest, sendResult, shouldRenderGraphiQL } from 'graphql-helix';
 
 function normalizeGraphQLError(error: GraphQLError) {
   return {
@@ -34,28 +33,16 @@ function normalizeGraphQLError(error: GraphQLError) {
 }
 
 export const graphqlHandler = (mesh$: ReturnType<typeof getMesh>): RequestHandler =>
-  function (req, res, next) {
-    // Create a generic Request object that can be consumed by Graphql Helix's API
-    const request = {
-      body: req.body,
-      headers: req.headers,
-      method: req.method,
-      query: req.query,
-    };
-
+  function (request, response, next) {
     // Determine whether we should render GraphiQL instead of returning an API response
     if (shouldRenderGraphiQL(request)) {
       next();
     } else {
       // Extract the GraphQL parameters from the request
       const { operationName, query, variables } = getGraphQLParameters(request);
-
-      Promise.resolve()
-        .then(async function () {
-          const { schema, execute, subscribe } = await mesh$;
-
-          // Validate and execute the query
-          const result = await processRequest({
+      mesh$
+        .then(({ schema, execute, subscribe }) =>
+          processRequest({
             operationName,
             query,
             variables,
@@ -66,89 +53,18 @@ export const graphqlHandler = (mesh$: ReturnType<typeof getMesh>): RequestHandle
               execute(query, variableValues, contextValue, rootValue, operationName),
             subscribe: (_schema, _documentAST, rootValue, contextValue, variableValues, operationName) =>
               subscribe(query, variableValues, contextValue, rootValue, operationName),
-            contextFactory: () => req,
-          });
-
-          // processRequest returns one of three types of results depending on how the server should respond
-          // 1) RESPONSE: a regular JSON payload
-          // 2) MULTIPART RESPONSE: a multipart response (when @stream or @defer directives are used)
-          // 3) PUSH: a stream of events to push back down the client for a subscription
-          switch (result.type) {
-            case 'RESPONSE':
-              // We set the provided status and headers and just the send the payload back to the client
-              result.headers.forEach(({ name, value }) => res.setHeader(name, value));
-              res.status(result.status);
-              if (result.payload.errors?.length) {
-                result.payload.errors = result.payload.errors.map(graphQLError =>
-                  normalizeGraphQLError(graphQLError)
-                ) as any;
-              }
-              res.json(result.payload);
-              break;
-            case 'MULTIPART_RESPONSE':
-              // Indicate we're sending a multipart response
-              res.writeHead(200, {
-                Connection: 'keep-alive',
-                'Content-Type': 'multipart/mixed; boundary="-"',
-                'Transfer-Encoding': 'chunked',
-              });
-
-              // If the request is closed by the client, we unsubscribe and stop executing the request
-              req.on('close', () => {
-                result.unsubscribe();
-              });
-
-              res.write('---');
-
-              // Subscribe and send back each result as a separate chunk. We await the subscribe
-              // call. Once we're done executing the request and there are no more results to send
-              // to the client, the Promise returned by subscribe will resolve and we can end the response.
-              await result.subscribe(result => {
-                const chunk = Buffer.from(jsonFlatStringify(result), 'utf8');
-                const data = [
-                  '',
-                  'Content-Type: application/json; charset=utf-8',
-                  'Content-Length: ' + String(chunk.length),
-                  '',
-                  chunk,
-                ];
-
-                if (result.hasNext) {
-                  data.push('---');
-                }
-
-                res.write(data.join('\r\n'));
-              });
-
-              res.write('\r\n-----\r\n');
-              res.end();
-              break;
-            case 'PUSH':
-              // Indicate we're sending an event stream to the client
-              res.writeHead(200, {
-                'Content-Type': 'text/event-stream',
-                Connection: 'keep-alive',
-                'Cache-Control': 'no-cache',
-              });
-
-              // If the request is closed by the client, we unsubscribe and stop executing the request
-              req.on('close', () => {
-                result.unsubscribe();
-              });
-
-              // We subscribe to the event stream and push any new events to the client
-              await result.subscribe(result => {
-                const chunk = flatString(`data: ${JSON.stringify(result)}\n\n`);
-                res.write(chunk);
-              });
-              break;
-            default:
-              throw new Error(`Unknown GraphQL Result: ${inspect(result)}`);
-          }
-        })
-        .catch(e => {
-          res.status(500);
-          res.write(
+            contextFactory: () => request,
+          })
+        )
+        .then(result =>
+          sendResult(result, response, (result: { data: any; errors: any[] }) => {
+            result.errors = result.errors?.map(normalizeGraphQLError);
+            return result;
+          })
+        )
+        .catch((e: Error | AggregateError) => {
+          response.status(500);
+          response.write(
             JSON.stringify({
               errors:
                 'errors' in e
@@ -166,7 +82,7 @@ export const graphqlHandler = (mesh$: ReturnType<typeof getMesh>): RequestHandle
                     ],
             })
           );
-          res.end();
+          response.end();
         });
     }
   };
