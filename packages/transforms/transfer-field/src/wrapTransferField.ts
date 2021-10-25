253fd33e72e68c5a65cc9735f0877fc8f0f79403
ntziolis <nikolas@tziolis.de>
2021-10-25 06:39:56 +0200

    chore: cleanup

--- packages/transforms/transfer-field/src/wrapTransferField.ts
+++ packages/transforms/transfer-field/src/wrapTransferField.ts
@@ -1,5 +1,4 @@
 import { MeshTransform, MeshTransformOptions, YamlConfig } from '@graphql-mesh/types';
-import { renameFieldNode, wrapFieldNode, unwrapValue } from '@graphql-tools/wrap/transforms/HoistField';
 
 export default class WrapTransferField implements MeshTransform {
   noWrap = false;
