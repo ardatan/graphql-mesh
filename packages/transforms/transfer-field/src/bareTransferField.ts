b37089139633abdbdc6e25a084417386e45d8efc
ntziolis <nikolas@tziolis.de>
2021-10-25 06:37:55 +0200

    feat(transfer-field): move init code into ctor

--- packages/transforms/transfer-field/src/bareTransferField.ts
+++ packages/transforms/transfer-field/src/bareTransferField.ts
@@ -1,18 +1,19 @@
 import { MeshTransform, MeshTransformOptions, YamlConfig } from '@graphql-mesh/types';
 import { appendObjectFields, pruneSchema, removeObjectFields } from '@graphql-tools/utils';
-import { GraphQLSchema } from 'graphql';
+import { GraphQLFieldConfigMap, GraphQLSchema } from 'graphql';
+
+type FieldConfig = YamlConfig.TransferFieldTransformFieldConfigObject & {
+  removeObjectFieldsTestFn: (fieldName: string) => boolean;
+};
 
 export default class TransferFieldTransform implements MeshTransform {
   noWrap = true;
 
-  private config: YamlConfig.TransferFieldTransformConfig;
+  private transfers: FieldConfig[] = [];
 
   constructor(options: MeshTransformOptions<YamlConfig.TransferFieldTransformConfig>) {
     const { config } = options;
-    this.config = config;
-  }
 
-  transformSchema(schema: GraphQLSchema) {
     const {
       defaults: {
         useRegExp: useRegExpDefault = false,
@@ -20,43 +21,68 @@ export default class TransferFieldTransform implements MeshTransform {
         action: actionDefault = 'move',
       } = {},
       fields: fieldConfigs,
-    } = this.config;
+    } = config;
 
-    let transformedSchema: GraphQLSchema = schema;
     for (const fieldConfig of fieldConfigs) {
       const {
-        from: { type: fromTypeName, field: fromFieldName },
-        to: { type: toTypeName, field: toFieldName },
+        from,
+        to,
         useRegExp = useRegExpDefault,
         regExpFlags = regExpFlagsDefault,
         action = actionDefault,
       } = fieldConfig;
 
+      const fieldConfigWithDefaults: FieldConfig = {
+        from,
+        to,
+        useRegExp,
+        regExpFlags,
+        action,
+        removeObjectFieldsTestFn: useRegExp
+          ? fieldName => new RegExp(from.field, regExpFlags).test(fieldName)
+          : fieldName => fieldName === from.field,
+      };
+
+      this.transfers.push(fieldConfigWithDefaults);
+    }
+  }
+
+  transformSchema(schema: GraphQLSchema) {
+    let transformedSchema = schema;
+
+    for (const fieldConfig of this.transfers) {
+      const {
+        from: { type: fromTypeName, field: fromFieldName },
+        to: { type: toTypeName, field: toFieldName },
+        useRegExp,
+        regExpFlags,
+        action,
+        removeObjectFieldsTestFn,
+      } = fieldConfig;
+
       const [schemaWithoutFields, sourceFieldConfigMap] = removeObjectFields(
         transformedSchema,
         fromTypeName,
-        fieldName => {
-          return useRegExp ? new RegExp(fromFieldName, regExpFlags).test(fieldName) : fieldName === fromFieldName;
-        }
+        removeObjectFieldsTestFn
       );
 
       if (action === 'move') {
         transformedSchema = schemaWithoutFields;
       }
 
-      const sourceFieldName: string = (Object.keys(sourceFieldConfigMap) as string[]).find((fieldName: string) =>
-        useRegExp ? new RegExp(fromFieldName, regExpFlags).test(fieldName) : fieldName === fromFieldName
-      );
+      const sourceFieldNames = Object.keys(sourceFieldConfigMap) as string[];
 
-      const targetFieldName = useRegExp
-        ? sourceFieldName.replace(new RegExp(fromFieldName, regExpFlags), toFieldName)
-        : toFieldName;
+      const appendFieldConfigMap: GraphQLFieldConfigMap<any, any> = {};
+      for (const sourceFieldName of sourceFieldNames) {
+        const targetFieldName = useRegExp
+          ? sourceFieldName.replace(new RegExp(fromFieldName, regExpFlags), toFieldName)
+          : toFieldName;
 
-      const sourceField = sourceFieldConfigMap[sourceFieldName];
+        const sourceField = sourceFieldConfigMap[sourceFieldName];
+        appendFieldConfigMap[targetFieldName] = sourceField;
+      }
 
-      transformedSchema = appendObjectFields(transformedSchema, toTypeName, {
-        [targetFieldName]: sourceField,
-      });
+      transformedSchema = appendObjectFields(transformedSchema, toTypeName, appendFieldConfigMap);
     }
 
     return pruneSchema(transformedSchema);
