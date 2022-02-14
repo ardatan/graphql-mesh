05ec30255cf4266fb51bfb54dd2df3a3447c6d67
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-02-14 20:18:31 +0300

    fix(json-schema): revert breaking change in the API  (export getComposerFromJSONSchema)

---
+++ .changeset/cyan-cows-compete.md
@@ -0,0 +1,5 @@
+---
+'@omnigraph/json-schema': patch
+---
+
+fix(json-schema): revert breaking change in the API (export getComposerFromJSONSchema)
--- packages/loaders/json-schema/src/index.ts
+++ packages/loaders/json-schema/src/index.ts
@@ -1,6 +1,7 @@
 import { loadGraphQLSchemaFromJSONSchemas } from './loadGraphQLSchemaFromJSONSchemas';
 export default loadGraphQLSchemaFromJSONSchemas;
 export { loadGraphQLSchemaFromJSONSchemas };
+export * from './getComposerFromJSONSchema';
 export * from './getDereferencedJSONSchemaFromOperations';
 export * from './getGraphQLSchemaFromDereferencedJSONSchema';
 export * from './types';
