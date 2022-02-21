cbc67b690d9475853b0446f39debaec34ac4ebe3
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-02-21 21:30:11 +0300

    Fix example

--- examples/openapi-javascript-wiki/additional-resolvers.ts
+++ examples/openapi-javascript-wiki/additional-resolvers.ts
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { Access5, Agent3, Granularity22, Resolvers } from './.mesh';
+import type { Access5, Agent3, Granularity22, Resolvers } from './.mesh';
 
 export const resolvers: Resolvers = {
   Query: {
@@ -7,12 +7,12 @@ export const resolvers: Resolvers = {
       const result = await context.Wiki.Query.getMetricsPageviewsAggregateProjectAccessAgentGranularityStartEnd({
         root,
         args: {
-          access: Access5.ALL_ACCESS,
-          agent: Agent3.USER,
+          access: 'all-access',
+          agent: 'user',
           end: moment().format('YYYYMMDD'),
           start: moment().startOf('month').subtract(1, 'month').format('YYYYMMDD'),
           project,
-          granularity: Granularity22.DAILY,
+          granularity: 'daily',
         },
         context,
         info,
@@ -25,11 +25,7 @@ export const resolvers: Resolvers = {
         `,
       });
 
-      if (!result?.items || result?.items.length === 0) {
-        return 0;
-      }
-
-      return result?.items[0].views;
+      return result?.items?.[0]?.views || 0;
     },
   },
 };
