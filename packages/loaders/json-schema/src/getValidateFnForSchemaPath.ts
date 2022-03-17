6b7ae02f6e95dd0e6602a622d7f13735b554fb5d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-03-17 20:36:56 +0300

    Compile on the fly

--- packages/loaders/json-schema/src/getValidateFnForSchemaPath.ts
+++ packages/loaders/json-schema/src/getValidateFnForSchemaPath.ts
@@ -15,5 +15,14 @@ const ajvMemoizedCompile = memoize2(function ajvCompile(ajv: Ajv, jsonSchema: JS
 
 export function getValidateFnForSchemaPath(ajv: Ajv, path: string, schema: JSONSchema) {
   const subSchema = resolvePath(path, schema);
-  return ajvMemoizedCompile(ajv, subSchema);
+  const fn = function validateFn(data: any) {
+    const ajvValidateFn = ajvMemoizedCompile(ajv, subSchema);
+    return ajvValidateFn(data);
+  };
+  Object.defineProperty(fn, 'errors', {
+    get() {
+      return ajvMemoizedCompile(ajv, subSchema).errors;
+    },
+  });
+  return fn;
 }
