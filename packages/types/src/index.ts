286f5d5d38454ee6f3a703d6997cadea209acf26
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-03-21 00:28:44 +0300

    Simplify

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -56,12 +56,12 @@ export type AllHooks = {
 export type HookName = keyof AllHooks & string;
 
 export interface MeshPubSub {
-  publish<THook extends HookName>(triggerName: THook, payload: AllHooks[THook]): Promise<void>;
+  publish<THook extends HookName>(triggerName: THook, payload: AllHooks[THook]): Promise<void> | void;
   subscribe<THook extends HookName>(
     triggerName: THook,
     onMessage: (data: AllHooks[THook]) => void,
     options?: any
-  ): Promise<number>;
+  ): Promise<number> | number;
   unsubscribe(subId: number): void;
   asyncIterator<THook extends HookName>(triggers: THook): AsyncIterable<AllHooks[THook]>;
 }
--- packages/utils/src/pubsub.ts
+++ packages/utils/src/pubsub.ts
@@ -1,20 +1,15 @@
-import { HookName, AllHooks } from '@graphql-mesh/types';
+import { HookName, AllHooks, MeshPubSub } from '@graphql-mesh/types';
 import { observableToAsyncIterable } from '@graphql-tools/utils';
 
-class PubSubEvent<THook extends HookName> extends Event implements CustomEvent<AllHooks[THook]> {
+class PubSubEvent<THook extends HookName> extends Event {
   detail: AllHooks[THook];
   constructor(type: string, eventInitDict?: CustomEventInit<any>) {
     super(type, eventInitDict);
     this.detail = eventInitDict?.detail;
   }
-
-  initCustomEvent(type: string, bubbles?: boolean, cancelable?: boolean, detail?: AllHooks[THook]): void {
-    super.initEvent(type, bubbles, cancelable);
-    this.detail = detail;
-  }
 }
 
-export class PubSub {
+export class PubSub implements MeshPubSub {
   private eventTarget: EventTarget;
   constructor(eventTarget?: EventTarget) {
     if (eventTarget) {
@@ -24,27 +19,26 @@ export class PubSub {
     }
   }
 
-  async publish<THook extends HookName>(triggerName: THook, payload: AllHooks[THook]): Promise<void> {
-    this.eventTarget.dispatchEvent(new PubSubEvent(triggerName, { detail: payload }));
+  publish<THook extends HookName>(triggerName: THook, detail: AllHooks[THook]): void {
+    this.eventTarget.dispatchEvent(new PubSubEvent(triggerName, { detail }));
   }
 
   private listenerIdMap = new Map<
     number,
     {
       triggerName: HookName;
-      onMessage: (data: AllHooks[HookName]) => void;
       eventListener: EventListener;
     }
   >();
 
-  subscribe<THook extends HookName>(triggerName: THook, onMessage: (data: AllHooks[THook]) => void): Promise<number> {
-    const eventListener = function eventListener(event: PubSubEvent<THook>) {
+  subscribe<THook extends HookName>(triggerName: THook, onMessage: (data: AllHooks[THook]) => void): number {
+    function eventListener(event: PubSubEvent<THook>) {
       onMessage(event.detail);
-    };
+    }
     this.eventTarget.addEventListener(triggerName, eventListener);
     const subId = this.listenerIdMap.size;
-    this.listenerIdMap.set(subId, { triggerName, onMessage, eventListener });
-    return Promise.resolve(subId);
+    this.listenerIdMap.set(subId, { triggerName, eventListener });
+    return subId;
   }
 
   unsubscribe(subId: number): void {
@@ -58,9 +52,9 @@ export class PubSub {
   asyncIterator<THook extends HookName>(triggerName: THook): AsyncIterable<AllHooks[THook]> {
     return observableToAsyncIterable({
       subscribe: observer => {
-        const subId$ = this.subscribe(triggerName, data => observer.next(data));
+        const subId = this.subscribe(triggerName, data => observer.next(data));
         return {
-          unsubscribe: () => subId$.then(subId => this.unsubscribe(subId)),
+          unsubscribe: () => this.unsubscribe(subId),
         };
       },
     });
