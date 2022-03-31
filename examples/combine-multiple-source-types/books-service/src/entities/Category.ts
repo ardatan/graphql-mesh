3affeec1fd56d68130ae1d6b0b8aafc769ddd458
Charly POLY <cpoly55@gmail.com>
2022-03-31 18:38:38 +0200

    doc: WIP, "Combine multiple Sources" Guide

--- examples/combine-multiple-source-types/books-service/src/entities/Category.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class Category {
-  @ApiProperty({ example: '1', type: String })
-  id: string;
-
-  @ApiProperty({ example: 'Fiction', type: String })
-  name: string;
-}
