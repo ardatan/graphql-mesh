eeaaa7d2a74219f960ddfda327697081e070d017
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-04-06 14:15:13 +0300

    Fix build

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,14 +1,10 @@
-import { createRequire } from 'node:module';
+import { getRoutes } from './routes';
 import { withGuildDocs } from '@guild-docs/server';
 import nextBundleAnalyzer from '@next/bundle-analyzer';
 import { register } from 'esbuild-register/dist/node.js';
 import { i18n } from './next-i18next.config.js';
 
-const require = createRequire(import.meta.url);
-
 register({ extensions: ['.ts', '.tsx'] });
-
-const { getRoutes } = require('./routes.ts'); // with import got error Unknown file extension ".ts"
 
 const withBundleAnalyzer = nextBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' });
 
