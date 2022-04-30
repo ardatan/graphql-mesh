c7571cbd9579906be0ba3b79969cc7f3ff5917cd
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-04-30 15:52:00 +0300

    Improve code

--- packages/cache/localforage/src/index.ts
+++ packages/cache/localforage/src/index.ts
@@ -2,31 +2,22 @@ import { KeyValueCache, KeyValueCacheSetOptions, YamlConfig } from '@graphql-mes
 import { createInMemoryLRUDriver } from './InMemoryLRUDriver';
 import LocalForage from 'localforage';
 
-let INMEMORY_LRU_DEFINITION$: Promise<void>;
+LocalForage.defineDriver(createInMemoryLRUDriver()).catch(err =>
+  console.error('Failed at defining InMemoryLRU driver', err)
+);
 
 export default class LocalforageCache<V = any> implements KeyValueCache<V> {
   private localforage: LocalForage;
   constructor(config?: YamlConfig.LocalforageConfig) {
     const driverNames = config?.driver || ['INDEXEDDB', 'WEBSQL', 'LOCALSTORAGE', 'INMEMORY_LRU'];
-    if (INMEMORY_LRU_DEFINITION$ == null) {
-      INMEMORY_LRU_DEFINITION$ = LocalForage.defineDriver(createInMemoryLRUDriver());
-    }
-    this.ready$ = INMEMORY_LRU_DEFINITION$.then(() => {
-      this.localforage = LocalForage.createInstance({
-        name: config?.name,
-        storeName: config?.storeName,
-        driver: driverNames.map(driverName => LocalForage[driverName] ?? driverName),
-      });
+    this.localforage = LocalForage.createInstance({
+      name: config?.name,
+      storeName: config?.storeName,
+      driver: driverNames.map(driverName => LocalForage[driverName] ?? driverName),
     });
-    if (config?.cleanOnStart) {
-      this.ready$ = this.ready$.then(() => this.localforage.clear());
-    }
   }
 
-  private ready$: Promise<void>;
-
   async get(key: string) {
-    await this.ready$;
     const expiresAt = await this.localforage.getItem<number>(`${key}.expiresAt`);
     if (expiresAt && Date.now() > expiresAt) {
       await this.localforage.removeItem(key);
@@ -35,7 +26,6 @@ export default class LocalforageCache<V = any> implements KeyValueCache<V> {
   }
 
   async set(key: string, value: V, options?: KeyValueCacheSetOptions) {
-    await this.ready$;
     const jobs: Promise<any>[] = [this.localforage.setItem<V>(key, value)];
     if (options?.ttl) {
       jobs.push(this.localforage.setItem(`${key}.expiresAt`, Date.now() + options.ttl * 1000));
@@ -43,8 +33,7 @@ export default class LocalforageCache<V = any> implements KeyValueCache<V> {
     await Promise.all(jobs);
   }
 
-  async delete(key: string) {
-    await this.ready$;
+  delete(key: string) {
     return this.localforage.removeItem(key);
   }
 }
