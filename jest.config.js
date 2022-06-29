829c36059fcd20617d4c439142f3b990e157e6e6
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-06-29 12:27:54 +0300

    Go go go

--- jest.config.js
+++ jest.config.js
@@ -18,6 +18,7 @@ module.exports = {
     ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
       prefix: `${ROOT_DIR}/`,
     }),
+    "/^@graphql\-mesh\/cross\-helpers$/": "/home/runner/work/graphql-mesh/graphql-mesh/packages/cross-helpers/node.js",
     'formdata-node': '<rootDir>/node_modules/formdata-node/lib/cjs/index.js',
     uuid: '<rootDir>/node_modules/uuid/index.js',
   },
