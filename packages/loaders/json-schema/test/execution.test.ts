46c47ab3436403bda189f9e0503b06868d033d60
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-07-27 15:15:43 +0300

    Tests

--- packages/loaders/json-schema/test/execution.test.ts
+++ packages/loaders/json-schema/test/execution.test.ts
@@ -10,6 +10,8 @@ describe('Execution', () => {
         let request: Request;
         if (typeof info !== 'object') {
           request = new Request(info, init);
+        } else {
+          request = info;
         }
         return new Response(JSON.stringify(getHeadersObj(request.headers)), {
           headers: {
@@ -75,6 +77,120 @@ describe('Execution', () => {
           test: null,
         },
       },
+    });
+  });
+  describe('Query Stringify Configuration', () => {
+    it('should not send array values without indices by default', async () => {
+      const schema = await loadGraphQLSchemaFromJSONSchemas('test', {
+        async fetch(info: RequestInfo, init?: RequestInit) {
+          let request: Request;
+          if (typeof info !== 'object') {
+            request = new Request(info, init);
+          } else {
+            request = info;
+          }
+          return new Response(
+            JSON.stringify({
+              url: request.url,
+            }),
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          );
+        },
+        baseUrl: 'http://localhost:3000',
+        operations: [
+          {
+            type: OperationTypeNode.QUERY,
+            field: 'test',
+            method: 'GET',
+            path: '/test',
+            requestSample: {
+              foo: ['bar', 'baz'],
+            },
+            responseSample: {
+              url: 'http://localhost:3000/test?foo=bar&foo=baz',
+            },
+          },
+        ],
+      });
+      const query = /* GraphQL */ `
+        query Test {
+          test(input: { foo: ["bar", "baz"] }) {
+            url
+          }
+        }
+      `;
+      const result = await execute({
+        schema,
+        document: parse(query),
+      });
+      console.log(result.errors);
+      expect(result).toEqual({
+        data: {
+          test: {
+            url: 'http://localhost:3000/test?foo=bar&foo=baz',
+          },
+        },
+      });
+    });
+    it('should not send array values with indices if configured', async () => {
+      const schema = await loadGraphQLSchemaFromJSONSchemas('test', {
+        async fetch(info: RequestInfo, init?: RequestInit) {
+          let request: Request;
+          if (typeof info !== 'object') {
+            request = new Request(info, init);
+          } else {
+            request = info;
+          }
+          return new Response(
+            JSON.stringify({
+              url: request.url,
+            }),
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          );
+        },
+        baseUrl: 'http://localhost:3000',
+        indices: true,
+        operations: [
+          {
+            type: OperationTypeNode.QUERY,
+            field: 'test',
+            method: 'GET',
+            path: '/test',
+            requestSample: {
+              foo: ['bar', 'baz'],
+            },
+            responseSample: {
+              url: 'http://localhost:3000/test?foo[0]=bar&foo[1]=baz',
+            },
+          },
+        ],
+      });
+      const query = /* GraphQL */ `
+        query Test {
+          test(input: { foo: ["bar", "baz"] }) {
+            url
+          }
+        }
+      `;
+      const result = await execute({
+        schema,
+        document: parse(query),
+      });
+      expect(result).toEqual({
+        data: {
+          test: {
+            url: 'http://localhost:3000/test?foo%5B0%5D=bar&foo%5B1%5D=baz',
+          },
+        },
+      });
     });
   });
 });
