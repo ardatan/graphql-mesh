bc32bbc4ed2e688af7f2550ec4f6f15f5929799b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-08-02 19:39:48 +0300

    Better visitor code

--- packages/json-machete/src/visitJSONSchema.ts
+++ packages/json-machete/src/visitJSONSchema.ts
@@ -9,6 +9,19 @@ export type JSONSchemaVisitor = (subSchema: any, context: JSONSchemaVisitorConte
 
 const identicalFn = <T>(a: T) => a;
 
+const objectFields = [
+  'additionalProperties',
+  'additionalItems',
+  'contains',
+  'else',
+  'if',
+  'items',
+  'not',
+  'then',
+] as const;
+
+const dictFields = ['anyOf', 'allOf', 'oneOf', 'definitions', 'properties', 'patternProperties'] as const;
+
 export async function visitJSONSchema(
   schema: JSONSchema,
   {
@@ -30,160 +43,29 @@ export async function visitJSONSchema(
         path,
       });
       visitedSubschemaResultMap.set(schema, enterResult);
-      if (enterResult.additionalItems) {
-        enterResult.additionalItems = await visitJSONSchema(
-          enterResult.additionalItems,
-          { enter, leave },
-          {
-            visitedSubschemaResultMap,
-            path: path + '/additionalItems',
-          }
-        );
-      }
-      if (enterResult.additionalProperties) {
-        enterResult.additionalProperties = await visitJSONSchema(
-          enterResult.additionalProperties,
-          { enter, leave },
-          {
-            visitedSubschemaResultMap,
-            path: path + '/additionalProperties',
-          }
-        );
-      }
-      if (enterResult.allOf) {
-        for (const subSchemaIndex in enterResult.allOf) {
-          enterResult.allOf[subSchemaIndex] = await visitJSONSchema(
-            enterResult.allOf[subSchemaIndex],
+      for (const key of objectFields) {
+        if (enterResult[key]) {
+          enterResult[key] = await visitJSONSchema(
+            enterResult[key],
             { enter, leave },
             {
               visitedSubschemaResultMap,
-              path: path + '/allOf/' + subSchemaIndex,
+              path: `${path}/${key}`,
             }
           );
         }
       }
-      if (enterResult.anyOf) {
-        for (const subSchemaIndex in enterResult.anyOf) {
-          enterResult.anyOf[subSchemaIndex] = await visitJSONSchema(
-            enterResult.anyOf[subSchemaIndex],
-            { enter, leave },
-            {
-              visitedSubschemaResultMap,
-              path: path + '/anyOf/' + subSchemaIndex,
-            }
-          );
+      for (const key of dictFields) {
+        if (enterResult[key]) {
+          const entries = Object.entries(enterResult[key]);
+          for (const [itemKey, itemValue] of entries) {
+            enterResult[key][itemKey] = await visitJSONSchema(
+              itemValue,
+              { enter, leave },
+              { visitedSubschemaResultMap, path: `${path}/${key}/${itemKey}` }
+            );
+          }
         }
-      }
-      if (enterResult.contains) {
-        enterResult.contains = await visitJSONSchema(
-          enterResult.contains,
-          { enter, leave },
-          {
-            visitedSubschemaResultMap,
-            path: path + '/contains',
-          }
-        );
-      }
-      if (enterResult.definitions) {
-        const entries = Object.entries(enterResult.definitions);
-        for (const [definitionName, subSchema] of entries) {
-          enterResult.definitions[definitionName] = await visitJSONSchema(
-            subSchema,
-            { enter, leave },
-            {
-              visitedSubschemaResultMap,
-              path: path + '/definitions/' + definitionName,
-            }
-          );
-        }
-      }
-      if (enterResult.else) {
-        enterResult.else = await visitJSONSchema(
-          enterResult.else,
-          { enter, leave },
-          {
-            visitedSubschemaResultMap,
-            path: path + '/else',
-          }
-        );
-      }
-      if (enterResult.if) {
-        enterResult.if = await visitJSONSchema(
-          enterResult.if,
-          { enter, leave },
-          {
-            visitedSubschemaResultMap,
-            path: path + '/if',
-          }
-        );
-      }
-      if (enterResult.items) {
-        enterResult.items = await visitJSONSchema(
-          enterResult.items,
-          { enter, leave },
-          {
-            visitedSubschemaResultMap,
-            path: path + '/items',
-          }
-        );
-      }
-      if (enterResult.not) {
-        enterResult.not = await visitJSONSchema(
-          enterResult.not,
-          { enter, leave },
-          {
-            visitedSubschemaResultMap,
-            path: path + '/not',
-          }
-        );
-      }
-      if (enterResult.oneOf) {
-        for (const subSchemaIndex in enterResult.oneOf) {
-          enterResult.oneOf[subSchemaIndex] = await visitJSONSchema(
-            enterResult.oneOf[subSchemaIndex],
-            { enter, leave },
-            {
-              visitedSubschemaResultMap,
-              path: path + '/oneOf/' + subSchemaIndex,
-            }
-          );
-        }
-      }
-      if (enterResult.patternProperties) {
-        const entries = Object.entries(enterResult.patternProperties);
-        for (const [pattern, subSchema] of entries) {
-          enterResult.patternProperties[pattern] = await visitJSONSchema(
-            subSchema,
-            { enter, leave },
-            {
-              visitedSubschemaResultMap,
-              path: path + '/patternProperties/' + pattern,
-            }
-          );
-        }
-      }
-      if (enterResult.properties) {
-        const entries = Object.entries(enterResult.properties);
-        for (const [propertyName, subSchema] of entries) {
-          enterResult.properties[propertyName] = await visitJSONSchema(
-            subSchema,
-            { enter, leave },
-            {
-              visitedSubschemaResultMap,
-              path: path + '/properties/' + propertyName,
-            }
-          );
-        }
-      }
-      if (enterResult.then) {
-        enterResult.then = await visitJSONSchema(
-          enterResult.then,
-          { enter, leave },
-          {
-            visitedSubschemaResultMap,
-            path: path + '/then',
-          }
-        );
       }
 
       if (enterResult.components?.schema) {
@@ -192,10 +74,7 @@ export async function visitJSONSchema(
           enterResult.components.schemas[schemaName] = await visitJSONSchema(
             subSchema,
             { enter, leave },
-            {
-              visitedSubschemaResultMap,
-              path: path + '/components/schemas/' + schemaName,
-            }
+            { visitedSubschemaResultMap, path: `${path}/components/schemas/${schemaName}` }
           );
         }
       }
