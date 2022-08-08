4c69b623866d8f050559d22277839f8aa1f4d15c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-08-08 12:13:16 +0300

    ..

--- packages/loaders/json-schema/test/schema-generation.test.ts
+++ packages/loaders/json-schema/test/schema-generation.test.ts
@@ -50,7 +50,7 @@ describe('Schema Generation', () => {
         `"{\\"name\\":\\"test\\",\\"operations\\":[{\\"type\\":\\"query\\",\\"field\\":\\"book\\",\\"path\\":\\"/book\\",\\"responseSchema\\":\\"./fixtures/book.json#/definitions/Book\\"}],\\"operationHeaders\\":{},\\"referencedSchema\\":{\\"$ref\\":\\"#/definitions/_schema\\",\\"definitions\\":{\\"_schema\\":{\\"type\\":\\"object\\",\\"title\\":\\"_schema\\",\\"properties\\":{\\"query\\":{\\"$ref\\":\\"#/definitions/Query\\"},\\"queryInput\\":{\\"$ref\\":\\"#/definitions/QueryInput\\"}},\\"required\\":[\\"query\\"]},\\"Query\\":{\\"type\\":\\"object\\",\\"title\\":\\"Query\\",\\"properties\\":{\\"book\\":{\\"$ref\\":\\"#/definitions/Book\\"}}},\\"Book\\":{\\"type\\":\\"object\\",\\"title\\":\\"Book\\",\\"properties\\":{\\"title\\":{\\"type\\":\\"string\\"},\\"author\\":{\\"type\\":\\"string\\"},\\"price\\":{\\"type\\":\\"number\\"},\\"similarBooks\\":{\\"type\\":\\"array\\",\\"items\\":{\\"$ref\\":\\"#/definitions/Book\\"}}},\\"$resolvedRef\\":\\"/definitions/Book\\"},\\"QueryInput\\":{\\"type\\":\\"object\\",\\"title\\":\\"QueryInput\\",\\"additionalProperties\\":true}}}}"`
       );
     });
-    it('should keep duplicates with circular deps if noDeduplication is true', async () => {
+    it('should keep duplicate types with circular deps if noDeduplication is true', async () => {
       const schema = await loadGraphQLSchemaFromJSONSchemas('test', {
         noDeduplication: true,
         operations: [
