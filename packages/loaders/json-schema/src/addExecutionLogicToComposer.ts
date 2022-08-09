2a6de7287d42001bfa72226189ab5db7f7f070f3
gilgardosh <gilgardosh@gmail.com>
2022-08-09 19:30:55 +0300

    */* access header fix

--- packages/loaders/json-schema/src/addExecutionLogicToComposer.ts
+++ packages/loaders/json-schema/src/addExecutionLogicToComposer.ts
@@ -165,7 +165,7 @@ ${operationConfig.description || ''}
         }
         const requestInit: RequestInit = {
           method: httpMethod,
-          headers,
+          headers: { accept: 'application/json', ...headers },
         };
         if (queryParams) {
           const interpolatedQueryParams: Record<string, any> = {};
