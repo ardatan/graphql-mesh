be3b2374156510f09f76784d08c9a9ae9ec9e7cd
gilgardosh <gilgardosh@gmail.com>
2022-08-22 15:09:56 +0300

    some naive tests for new sources

---
+++ packages/loaders/openapi/tests/instagram.test.ts
@@ -0,0 +1,26 @@
+import { printSchemaWithDirectives } from '@graphql-tools/utils';
+import { fetch } from '@whatwg-node/fetch';
+import { GraphQLSchema } from 'graphql/type';
+
+import { loadGraphQLSchemaFromOpenAPI } from '../src/loadGraphQLSchemaFromOpenAPI';
+
+let createdSchema: GraphQLSchema;
+
+describe('OpenAPI loader: instagram', () => {
+  beforeAll(async () => {
+    createdSchema = await loadGraphQLSchemaFromOpenAPI('example_api', {
+      fetch,
+      oasFilePath: '../../../handlers/openapi/test/fixtures/instagram.json',
+      cwd: __dirname,
+    });
+  });
+
+  it('should generate the schema correctly', () => {
+    expect(printSchemaWithDirectives(createdSchema)).toMatchSnapshot();
+  });
+
+  it('All Instagram query endpoints present', () => {
+    const gqlTypes = Object.keys(createdSchema.getQueryType().getFields()).length;
+    expect(gqlTypes).toEqual(22);
+  });
+});
