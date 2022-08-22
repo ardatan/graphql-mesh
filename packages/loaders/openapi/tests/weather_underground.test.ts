d86571436542adb4d6539bd9efcd732afac77429
gilgardosh <gilgardosh@gmail.com>
2022-08-22 16:58:42 +0300

    another source test

---
+++ packages/loaders/openapi/tests/weather_underground.test.ts
@@ -0,0 +1,30 @@
+import { printSchemaWithDirectives } from '@graphql-tools/utils';
+import { fetch } from '@whatwg-node/fetch';
+import { GraphQLSchema } from 'graphql';
+
+import { loadGraphQLSchemaFromOpenAPI } from '../src/loadGraphQLSchemaFromOpenAPI';
+
+let createdSchema: GraphQLSchema;
+
+describe('example_api', () => {
+  /**
+   * Set up the schema first
+   */
+  beforeAll(async () => {
+    // const validOas = await getValidOAS3(oas);
+    createdSchema = await loadGraphQLSchemaFromOpenAPI('example_api', {
+      fetch,
+      oasFilePath: '../../../handlers/openapi/test/fixtures/weather_underground.json',
+      cwd: __dirname,
+    });
+  });
+
+  it('should generate the schema correctly', () => {
+    expect(printSchemaWithDirectives(createdSchema)).toMatchSnapshot();
+  });
+
+  it('All Weather Underground query endpoints present', () => {
+    const gqlTypes = Object.keys(createdSchema.getQueryType().getFields()).length;
+    expect(gqlTypes).toEqual(3);
+  });
+});
