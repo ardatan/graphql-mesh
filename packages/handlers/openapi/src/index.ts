260af5cb47de745dd00aa383a193bafabe9e8dec
gilgardosh <gilgardosh@gmail.com>
2022-08-23 14:59:41 +0300

    remove old handler code, relocate new one

--- packages/handlers/openapi/src/index.ts
+++ packages/handlers/openapi/src/index.ts
@@ -1,241 +1,86 @@
-import { readFileOrUrl } from '@graphql-mesh/utils';
-import { asArray } from '@graphql-tools/utils';
-import { createGraphQLSchema, GraphQLOperationType } from './openapi-to-graphql';
-import { Oas3 } from './openapi-to-graphql/types/oas3';
-import {
-  MeshHandler,
-  YamlConfig,
-  GetMeshSourceOptions,
-  MeshSource,
-  MeshPubSub,
-  Logger,
-  ImportFn,
-} from '@graphql-mesh/types';
-import { OasTitlePathMethodObject } from './openapi-to-graphql/types/options';
-import { GraphQLArgument, GraphQLID, GraphQLInputType } from 'graphql';
 import { PredefinedProxyOptions, StoreProxy } from '@graphql-mesh/store';
-import openapiDiff from 'openapi-diff';
-import { getValidOAS3 } from './openapi-to-graphql/oas_3_tools';
-import { Oas2 } from './openapi-to-graphql/types/oas2';
-import { path, process } from '@graphql-mesh/cross-helpers';
 import {
-  stringInterpolator,
-  getInterpolatedHeadersFactory,
-  ResolverDataBasedFactory,
-  getInterpolatedStringFactory,
-  ResolverData,
-  parseInterpolationStrings,
-} from '@graphql-mesh/string-interpolation';
+  GetMeshSourceOptions,
+  ImportFn,
+  Logger,
+  MeshHandler,
+  MeshPubSub,
+  MeshSource,
+  YamlConfig,
+} from '@graphql-mesh/types';
+import { loadFromModuleExportExpression } from '@graphql-mesh/utils';
+import { createBundle, getGraphQLSchemaFromBundle, OpenAPILoaderBundle } from '@omnigraph/openapi';
 
 export default class OpenAPIHandler implements MeshHandler {
-  private config: YamlConfig.OpenapiHandler;
+  private name: string;
+  private config: YamlConfig.NewOpenapiHandler;
+  private bundleStoreProxy: StoreProxy<OpenAPILoaderBundle>;
   private baseDir: string;
-  private fetchFn: typeof fetch;
-  private importFn: ImportFn;
-  private pubsub: MeshPubSub;
-  private oasSchema: StoreProxy<Oas3[]>;
   private logger: Logger;
-
+  private fetch: typeof fetch;
+  private pubsub: MeshPubSub;
+  private importFn: ImportFn;
   constructor({
     name,
     config,
     baseDir,
     fetchFn,
-    importFn,
-    pubsub,
     store,
+    pubsub,
     logger,
-  }: GetMeshSourceOptions<YamlConfig.OpenapiHandler>) {
+    importFn,
+  }: GetMeshSourceOptions<YamlConfig.NewOpenapiHandler>) {
+    this.name = name;
     this.config = config;
     this.baseDir = baseDir;
+    this.fetch = fetchFn;
+    this.bundleStoreProxy = store.proxy('jsonSchemaBundle', PredefinedProxyOptions.JsonWithoutValidation);
     this.pubsub = pubsub;
     this.logger = logger;
-    this.fetchFn = fetchFn;
     this.importFn = importFn;
-    // TODO: This validation here should be more flexible, probably specific to OAS
-    // Because we can handle json/swagger files, and also we might want to use this:
-    // https://github.com/Azure/openapi-diff
-    this.oasSchema = store.proxy<Oas3[]>('oas-schema', {
-      ...PredefinedProxyOptions.JsonWithoutValidation,
-      validate: async (oldOass, newOass) => {
-        for (const index in oldOass) {
-          const oldOas = oldOass[index];
-          const newOas = newOass[index];
-          const result = await openapiDiff.diffSpecs({
-            sourceSpec: {
-              content: JSON.stringify(oldOas),
-              location: path.join(this.baseDir, `.mesh/sources/${name}/oas-schema.js`),
-              format: 'openapi3',
-            },
-            destinationSpec: {
-              content: JSON.stringify(newOas),
-              location: config.source,
-              format: 'openapi3',
-            },
-          });
-          if (result.breakingDifferencesFound) {
-            throw new Error('Breaking changes found!');
-          }
-        }
-      },
-    });
   }
 
-  private getCachedSpec(): Promise<Oas3[]> {
-    const { source: nonInterpolatedSource } = this.config;
-    const source = stringInterpolator.parse(nonInterpolatedSource, {
-      env: process.env,
-    });
-    const schemaHeadersFactory = getInterpolatedHeadersFactory(this.config.schemaHeaders);
-    return this.oasSchema.getWithSet(async () => {
-      let rawSpec: Oas3 | Oas2 | (Oas3 | Oas2)[];
-      if (typeof source !== 'string') {
-        rawSpec = source;
-      } else {
-        rawSpec = await readFileOrUrl(source, {
-          cwd: this.baseDir,
-          fallbackFormat: this.config.sourceFormat,
-          headers: schemaHeadersFactory({
-            env: process.env,
-          }),
-          importFn: this.importFn,
-          fetch: this.fetchFn,
-          logger: this.logger,
-        });
-      }
-      return Promise.all(asArray(rawSpec).map(singleSpec => getValidOAS3(singleSpec)));
+  async getDereferencedBundle() {
+    return this.bundleStoreProxy.getWithSet(() => {
+      this.logger?.debug('Creating the bundle');
+      return createBundle(this.name, {
+        ...this.config,
+        cwd: this.baseDir,
+        fetch: this.fetch,
+        logger: this.logger,
+        ignoreErrorResponses: this.config.ignoreErrorResponses,
+        selectQueryOrMutationField: this.config.selectQueryOrMutationField?.map(({ type, fieldName }) => ({
+          type: type.toLowerCase() as any,
+          fieldName,
+        })),
+        fallbackFormat: this.config.fallbackFormat,
+        operationHeaders: typeof this.config.operationHeaders === 'string' ? {} : this.config.operationHeaders,
+      });
     });
   }
 
   async getMeshSource(): Promise<MeshSource> {
-    const { addLimitArgument, baseUrl, genericPayloadArgName, operationHeaders, qs, selectQueryOrMutationField } =
-      this.config;
-
-    const spec = await this.getCachedSpec();
-
-    const headersFactory = getInterpolatedHeadersFactory(operationHeaders);
-    const queryStringFactoryMap = new Map<string, ResolverDataBasedFactory<string>>();
-    for (const queryName in qs || {}) {
-      queryStringFactoryMap.set(queryName, getInterpolatedStringFactory(qs[queryName]));
-    }
-    const searchParamsFactory = (resolverData: ResolverData, searchParams: URLSearchParams) => {
-      for (const queryName in qs || {}) {
-        searchParams.set(queryName, queryStringFactoryMap.get(queryName)(resolverData));
-      }
-      return searchParams;
-    };
-
-    const { schema } = await createGraphQLSchema(spec, {
-      fetch: this.fetchFn,
-      baseUrl,
-      operationIdFieldNames: this.config.operationIdFieldNames,
-      fillEmptyResponses: true,
-      includeHttpDetails: this.config.includeHttpDetails,
-      provideErrorExtensions: this.config.provideErrorExtensions,
-      genericPayloadArgName: genericPayloadArgName === undefined ? false : genericPayloadArgName,
-      selectQueryOrMutationField:
-        selectQueryOrMutationField === undefined
-          ? {}
-          : selectQueryOrMutationField.reduce((acc, curr) => {
-              let operationType: GraphQLOperationType;
-              switch (curr.type) {
-                case 'Query':
-                  operationType = GraphQLOperationType.Query;
-                  break;
-                case 'Mutation':
-                  operationType = GraphQLOperationType.Mutation;
-                  break;
-              }
-              return {
-                ...acc,
-                [curr.title]: {
-                  ...acc[curr.title],
-                  [curr.path]: {
-                    ...((acc[curr.title] && acc[curr.title][curr.path]) || {}),
-                    [curr.method]: operationType,
-                  },
-                },
-              };
-            }, {} as OasTitlePathMethodObject<GraphQLOperationType>),
-      addLimitArgument: addLimitArgument === undefined ? true : addLimitArgument,
-      sendOAuthTokenInQuery: true,
-      viewer: false,
-      equivalentToMessages: true,
+    this.logger?.debug('Getting the bundle');
+    const bundle = await this.getDereferencedBundle();
+    this.logger?.debug('Generating GraphQL Schema from bundle');
+    const operationHeadersConfig =
+      typeof this.config.operationHeaders === 'string'
+        ? await loadFromModuleExportExpression<Record<string, string>>(this.config.operationHeaders, {
+            cwd: this.baseDir,
+            importFn: this.importFn,
+            defaultExportName: 'default',
+          })
+        : this.config.operationHeaders;
+    const schema = await getGraphQLSchemaFromBundle(bundle, {
+      cwd: this.baseDir,
+      fetch: this.fetch,
       pubsub: this.pubsub,
       logger: this.logger,
-      resolverMiddleware: (getResolverParams, originalFactory) => (root, args, context, info: any) => {
-        const resolverData: ResolverData = { root, args, context, info, env: process.env };
-        const resolverParams = getResolverParams();
-        resolverParams.requestOptions = {
-          headers: headersFactory(resolverData),
-        };
-        resolverParams.qs = qs;
-
-        /* FIXME: baseUrl is coming from Fastify Request
-        if (context?.baseUrl) {
-          resolverParams.baseUrl = context.baseUrl;
-        }
-        */
-
-        if (baseUrl) {
-          resolverParams.baseUrl = stringInterpolator.parse(baseUrl, resolverData);
-        }
-
-        if (resolverParams.baseUrl) {
-          const urlObj = new URL(resolverParams.baseUrl);
-          searchParamsFactory(resolverData, urlObj.searchParams);
-        } else {
-          this.logger.debug(
-            () =>
-              `Warning: There is no 'baseUrl' defined for this OpenAPI definition. We recommend you to define one manually!`
-          );
-        }
-
-        if (context?.fetch) {
-          resolverParams.fetch = context.fetch;
-        }
-
-        if (context?.qs) {
-          resolverParams.qs = context.qs;
-        }
-
-        return originalFactory(() => resolverParams, this.logger)(root, args, context, info);
-      },
-      allowUndefinedSchemaRefTags: this.config.allowUndefinedSchemaRefTags,
-      defaultUndefinedSchemaType: this.config.defaultUndefinedSchemaType,
+      baseUrl: this.config.baseUrl,
+      operationHeaders: operationHeadersConfig,
     });
-
-    const { args, contextVariables } = parseInterpolationStrings(Object.values(operationHeaders || {}));
-
-    const rootFields = [
-      ...Object.values(schema.getQueryType()?.getFields() || {}),
-      ...Object.values(schema.getMutationType()?.getFields() || {}),
-      ...Object.values(schema.getSubscriptionType()?.getFields() || {}),
-    ];
-
-    await Promise.all(
-      rootFields.map(rootField =>
-        Promise.all(
-          Object.entries(args).map(async ([argName, { type }]) =>
-            (rootField?.args as GraphQLArgument[]).push({
-              name: argName,
-              description: undefined,
-              defaultValue: undefined,
-              extensions: undefined,
-              astNode: undefined,
-              deprecationReason: undefined,
-              type: (typeof type === 'string' ? (schema.getType(type) as GraphQLInputType) : type) || GraphQLID,
-            })
-          )
-        )
-      )
-    );
-
-    contextVariables.fetch = 'typeof fetch';
-
     return {
       schema,
-      contextVariables,
     };
   }
 }
