fab48c8122cf96f158ec0cc17dc87f5eb60aabcf
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-08-24 10:24:25 +0300

    Add more tests

--- packages/utils/test/fetch.spec.ts
+++ packages/utils/test/fetch.spec.ts
@@ -34,7 +34,7 @@ describe('MeshFetch', () => {
     server.close();
   });
 
-  it('should deduplicate the same GET requests in the same context but sequentially', async () => {
+  it('should deduplicate the same GET requests in the same context sequentially', async () => {
     const context = {};
     const fetchFn = createDefaultMeshFetch(new LocalforageCache());
     const url = 'http://localhost:9856/somePath';
@@ -60,7 +60,7 @@ describe('MeshFetch', () => {
     await response2.text();
     expect(reqCount).toBe(1);
   });
-  it('should deduplicate the same GET request in the same context but parallel', async () => {
+  it('should deduplicate the same GET request in the same context in parallel', async () => {
     const context = {};
     const fetchFn = createDefaultMeshFetch(new LocalforageCache());
     const url = 'http://localhost:9856/somePath';
@@ -86,5 +86,113 @@ describe('MeshFetch', () => {
     ]);
     await Promise.all([response.text(), response2.text()]);
     expect(reqCount).toBe(1);
+  });
+  it('should not deduplicate the different GET requests in the same context sent sequentially', async () => {
+    const context = {};
+    const fetchFn = createDefaultMeshFetch(new LocalforageCache());
+    const url = 'http://localhost:9856/somePath';
+    const response = await fetchFn(
+      url,
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+      context
+    );
+    await response.text();
+    const response2 = await fetchFn(
+      url + '2',
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+      context
+    );
+    await response2.text();
+    expect(reqCount).toBe(2);
+  });
+  it('should not deduplicate the different GET requests in the same context sent in parallel', async () => {
+    const context = {};
+    const fetchFn = createDefaultMeshFetch(new LocalforageCache());
+    const url = 'http://localhost:9856/somePath';
+    const [response, response2] = await Promise.all([
+      fetchFn(
+        url,
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+        context
+      ),
+      fetchFn(
+        url + '2',
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+        context
+      ),
+    ]);
+    await Promise.all([response.text(), response2.text()]);
+    expect(reqCount).toBe(2);
+  });
+  it('should not deduplicate the same GET request in different contexts sent sequentially', async () => {
+    const context = {};
+    const context2 = {};
+    const fetchFn = createDefaultMeshFetch(new LocalforageCache());
+    const url = 'http://localhost:9856/somePath';
+    const response = await fetchFn(
+      url,
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+      context
+    );
+    await response.text();
+    const response2 = await fetchFn(
+      url,
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+      context2
+    );
+    await response2.text();
+    expect(reqCount).toBe(2);
+  });
+  it('should not deduplicate the same GET request in different contexts sent in parallel', async () => {
+    const context = {};
+    const context2 = {};
+    const fetchFn = createDefaultMeshFetch(new LocalforageCache());
+    const url = 'http://localhost:9856/somePath';
+    const [response, response2] = await Promise.all([
+      fetchFn(
+        url,
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+        context
+      ),
+      fetchFn(
+        url,
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        },
+        context2
+      ),
+    ]);
+    await Promise.all([response.text(), response2.text()]);
+    expect(reqCount).toBe(2);
   });
 });
