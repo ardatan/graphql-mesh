620c55572ddf95d09196197d5291b218d51a61f3
TuvalSimha <tuval.simha@gmail.com>
2022-08-30 16:43:08 +0300

    remove crisp and GA script

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,21 +1,15 @@
 import { ReactElement } from 'react';
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
-import { useGoogleAnalytics } from 'guild-docs';
 import { FooterExtended, Header, ThemeProvider } from '@theguild/components';
 import 'guild-docs/style.css';
 
 export default function App({ Component, pageProps, router }: AppProps): ReactElement {
-  const analytics = useGoogleAnalytics({ router, trackingId: 'G-TPQZLLF5T5' });
   // @ts-expect-error -- getLayout is custom function from nextra
   const { getLayout = page => page } = Component;
 
   return (
     <ThemeProvider>
       <Header accentColor="#1cc8ee" themeSwitch searchBarProps={{ version: 'v2' }} />
-      <Script {...analytics.loadScriptProps} />
-      <Script {...analytics.configScriptProps} />
-      <Script src="https://the-guild.dev/static/crisp.js" />
       {getLayout(<Component {...pageProps} />)}
       <FooterExtended />
     </ThemeProvider>
