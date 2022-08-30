9d05ee486d24c3e207921c997c3871fe231dac15
TuvalSimha <tuval.simha@gmail.com>
2022-08-30 16:21:04 +0300

    Adjust next.config
    typo
    
    experimental

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,19 +1,24 @@
 import { withGuildDocs } from 'guild-docs/next.config';
 
 export default withGuildDocs({
+  basePath: process.env.NEXT_BASE_PATH && process.env.NEXT_BASE_PATH !== '' ? process.env.NEXT_BASE_PATH : undefined,
+  experimental: {
+    images: {
+        unoptimized: true, // doesn't work with `next export`
+        allowFutureImage: true,
+      },
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  redirects: () => [
-    {
-      source: '/docs/introduction',
-      destination: '/docs',
+  redirects: () =>
+  Object.entries({
+    '/docs/introduction': '/docs',
+    '/docs/recipes/build-mesh-artifacts': '/docs/guides/mesh-sdk',
+  })
+    .map(([from, to]) => ({
+      source: from,
+      destination: to,
       permanent: true,
-    },
-    {
-      source: '/docs/recipes/build-mesh-artifacts',
-      destination: '/docs/guides/mesh-sdk',
-      permanent: true,
-    },
-  ],
+    })),
 });
