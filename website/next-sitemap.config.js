08dce5d9054fa8c077403c87990e4fea6c0dcfcd
TuvalSimha <tuval.simha@gmail.com>
2022-09-05 11:06:23 +0300

    fix Dotan code review

--- website/next-sitemap.config.js
+++ website/next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://the-guild.dev/graphql/mesh',
-  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
 };
