066009a81a7613a2466bac8418248b81a13c208f
TuvalSimha <tuval.simha@gmail.com>
2022-09-05 14:12:15 +0300

    fix theme

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -22,7 +22,7 @@ const config: DocsThemeConfig = {
       </div>
     </>
   ),
-  head: (
+  head: () => (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content={`${SITE_NAME}: documentation`} />
