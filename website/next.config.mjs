cb544fc9612f2c8a0f95daada7b99caed453c318
TuvalSimha <tuval.simha@gmail.com>
2022-09-07 17:09:53 +0300

    Fix redirects

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -4,19 +4,26 @@ export default withGuildDocs({
   basePath: process.env.NEXT_BASE_PATH && process.env.NEXT_BASE_PATH !== '' ? process.env.NEXT_BASE_PATH : undefined,
   experimental: {
     images: {
-        unoptimized: true, // doesn't work with `next export`
-        allowFutureImage: true,
-      },
+      unoptimized: true, // doesn't work with `next export`
+      allowFutureImage: true,
+    },
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
   redirects: () =>
-  Object.entries({
-    '/docs/introduction': '/docs',
-    '/docs/recipes/build-mesh-artifacts': '/docs/guides/mesh-sdk',
-  })
-    .map(([from, to]) => ({
+    Object.entries({
+      '/docs/introduction': '/docs',
+      '/docs/recipes': '/docs',
+      '/docs/api': '/docs',
+      '/docs/modules': '/docs',
+      '/docs/api/classes/apollo_link_src.MeshApolloLink': '/docs',
+      '/docs/api/classes': '/docs',
+      '/docs/recipes/as-gateway': '/docs',
+      '/docs/guides/combine-many-sources': '/docs',
+      '/docs/guides': '/docs',
+      '/docs/recipes/build-mesh-artifacts': '/docs/guides/mesh-sdk',
+    }).map(([from, to]) => ({
       source: from,
       destination: to,
       permanent: true,
