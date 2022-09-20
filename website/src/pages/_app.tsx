df70e3930f99c098315e9e0de1b504834a97a14a
gilgardosh <gilgardosh@gmail.com>
2022-09-20 23:32:36 +0300

    Add error boundary

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -2,13 +2,16 @@ import { ReactElement } from 'react';
 import type { AppProps } from 'next/app';
 import { FooterExtended, Header, ThemeProvider } from '@theguild/components';
 import 'guild-docs/style.css';
+import { ErrorBoundary } from '../components/error-boundary';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
     <ThemeProvider>
-      <Header accentColor="#1cc8ee" themeSwitch searchBarProps={{ version: 'v2' }} />
-      <Component {...pageProps} />
-      <FooterExtended />
+      <ErrorBoundary>
+        <Header accentColor="#1cc8ee" themeSwitch searchBarProps={{ version: 'v2' }} />
+        <Component {...pageProps} />
+        <FooterExtended />
+      </ErrorBoundary>
     </ThemeProvider>
   );
 }
