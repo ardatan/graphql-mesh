fe0086ba530705c4264447ce8b171ab528d2a25e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-09-21 09:48:02 +0300

    Satisfy ts

--- packages/loaders/openapi/tests/calendly.test.ts
+++ packages/loaders/openapi/tests/calendly.test.ts
@@ -12,7 +12,7 @@ describe('Calendly', () => {
     expect(printSchemaWithDirectives(schema)).toMatchSnapshot();
   });
 
-  it.only('Querying by organization', async () => {
+  it('Querying by organization', async () => {
     const query = /* GraphQL */ `
       query {
         getScheduledEvents(organization: "http://a") {
@@ -34,7 +34,7 @@ describe('Calendly', () => {
       fetch,
     });
     return graphql({ schema: createdSchema, source: query }).then((result: any) => {
-      expect(fetch.mock.calls[0][0]).toEqual(
+      expect(fetch.mock.calls[0].at(0)).toEqual(
         'https://api.calendly.com/scheduled_events?organization=http%3A%2F%2Fa%2F&count=20'
       );
     });
@@ -63,7 +63,7 @@ describe('Calendly', () => {
     });
 
     return graphql({ schema: createdSchema, source: query }).then((result: any) => {
-      expect(fetch.mock.calls[0][0]).toEqual(
+      expect(fetch.mock.calls[0].at(0)).toEqual(
         'https://api.calendly.com/scheduled_events?invitee_email=a%40b.com&count=20'
       );
     });
