eba73c6261a2fdde8ece31915202203b70ff0e5f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-11-28 16:54:24 +0300

    fix(utils): ESM mode needs .js extension so add it if not provided

--- packages/utils/src/defaultImportFn.ts
+++ packages/utils/src/defaultImportFn.ts
@@ -1,11 +1,20 @@
-export async function defaultImportFn(path: string) {
-  let module = await import(/* @vite-ignore */ path).catch(e => {
-    if (e.code === 'ERR_REQUIRE_ESM') {
-      // eslint-disable-next-line no-new-func
-      return new Function(`return import(${JSON.stringify(path)})`)();
-    }
-    throw e;
-  });
+import { path as pathModule } from '@graphql-mesh/cross-helpers';
+
+export async function defaultImportFn(path: string): Promise<any> {
+  let module = await import(/* @vite-ignore */ path)
+    .catch(e => {
+      if (e.code === 'ERR_REQUIRE_ESM') {
+        // eslint-disable-next-line no-new-func
+        return new Function(`return import(${JSON.stringify(path)})`)();
+      }
+      throw e;
+    })
+    .catch(e => {
+      if (pathModule.isAbsolute(path) && !path.endsWith('.js') && !path.endsWith('.ts')) {
+        return defaultImportFn(`${path}.ts`);
+      }
+      throw e;
+    });
   if (module.default != null) {
     module = module.default;
   }
