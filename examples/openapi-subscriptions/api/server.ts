a31a88063aabd16b7615ed65b143e5539e339357
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-01-23 22:58:21 +0300

    Go

--- examples/openapi-subscriptions/api/server.ts
+++ examples/openapi-subscriptions/api/server.ts
@@ -1,12 +1,12 @@
 import { createServer } from 'http';
 import { createApp } from './app';
 
-const { app, dispose: disponse } = createApp();
+const { app, dispose } = createApp();
 
 const server = createServer(app).listen(4001, () => {
   console.info('API Server Listening on 4001');
 });
 
 server.once('close', () => {
-  disponse();
+  dispose();
 });
