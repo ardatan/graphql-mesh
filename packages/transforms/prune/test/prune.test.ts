451235b763c964bf4c0d5a0aa0f786663f69c154
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-01-24 14:34:38 +0300

    Test prune transform's skipPruning

---
+++ packages/transforms/prune/test/prune.test.ts
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { buildSchema } from 'graphql';
+import InMemoryLRUCache from '@graphql-mesh/cache-localforage';
+import { defaultImportFn, DefaultLogger, PubSub } from '@graphql-mesh/utils';
+import PruneTransform from '../src';
+
+describe('Prune', () => {
+  const apiName = 'test';
+  const baseDir = __dirname;
+  const cache = new InMemoryLRUCache();
+  const pubsub = new PubSub();
+  const logger = new DefaultLogger();
+  const importFn = defaultImportFn;
+  it('should skip specified types while pruning', () => {
+    const transform = new PruneTransform({
+      apiName,
+      baseDir,
+      cache,
+      pubsub,
+      logger,
+      importFn,
+      config: {
+        skipPruning: ['Mutation'],
+      },
+    });
+    const schema = buildSchema(/* GraphQL */ `
+      type Query {
+        foo: String
+      }
+      type Mutation
+    `);
+    const prunedSchema = transform.transformSchema(schema);
+    expect(prunedSchema.getType('Query')).toBeDefined();
+    expect(prunedSchema.getType('Mutation')).toBeFalsy();
+  });
+});
