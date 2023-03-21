74836cea7129c5fd1ddb738be6afdc2ddc53e4c3
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-03-21 17:42:03 +0300

    Tests

--- packages/utils/src/getHeadersObj.ts
+++ packages/utils/src/getHeadersObj.ts
@@ -13,7 +13,7 @@ export function getHeadersObj(headers: Headers): Record<string, string> {
         if (name === 'toJSON') {
           return () => headersToJSON(headers);
         }
-        return headers.get(name.toString());
+        return headers.get(name.toString()) || undefined;
       },
       has(_target, name) {
         if (name === 'toJSON') {
@@ -27,6 +27,24 @@ export function getHeadersObj(headers: Headers): Record<string, string> {
       set(_target, name, value) {
         headers.set(name.toString(), value);
         return true;
+      },
+      defineProperty(_target, name, descriptor) {
+        if (descriptor.value != null) {
+          headers.set(name.toString(), descriptor.value);
+        }
+        return true;
+      },
+      getOwnPropertyDescriptor(_target, name) {
+        const value = headers.get(name.toString());
+        if (value == null) {
+          return undefined;
+        }
+        return {
+          configurable: true,
+          enumerable: true,
+          value,
+          writable: true,
+        };
       },
       deleteProperty(_target, name) {
         headers.delete(name.toString());
