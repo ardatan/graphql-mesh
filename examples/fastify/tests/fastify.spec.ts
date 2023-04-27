34e95a6d4cff0f94da70700297f02995ce695ac2
Drew McMillan <drew.mcmillan@thetrainline.com>
2023-04-27 17:41:33 +0100

    fastify-integration-tests-force-error

--- examples/fastify/tests/fastify.spec.ts
+++ examples/fastify/tests/fastify.spec.ts
@@ -1,14 +1,20 @@
 import { fetch } from '@whatwg-node/fetch';
 import { app } from '../src/app';
+import { upstream } from '../src/upstream';
 
 describe('fastify', () => {
   beforeAll(async () => {
     await app.listen({
       port: 4000,
     });
+    await upstream.listen({
+      port: 4001
+    })
   });
+
   afterAll(async () => {
     await app.close();
+    await upstream.close();
   });
 
   it('should work', async () => {
@@ -20,11 +26,8 @@ describe('fastify', () => {
       body: JSON.stringify({
         query: /* GraphQL */ `
           {
-            allFilms {
-              films {
-                id
-                title
-              }
+            pet_by_petId(petId: "pet200") {
+              name
             }
           }
         `,
@@ -32,6 +35,28 @@ describe('fastify', () => {
     });
 
     const json = await response.json();
-    expect(json.data.allFilms.films.length).toBeGreaterThan(0);
+    expect(json.data).toEqual({"pet_by_petId": {"name": "Bob"}});
+  });
+
+  it('should work too', async () => {
+    const response = await fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          {
+            pet_by_petId(petId: "pet500") {
+              name
+            }
+          }
+        `,
+      }),
+    });
+
+    const jsonText = await response.text();
+
+    expect(jsonText).not.toEqual("{\"data\":{\"pet_by_");
   });
 });
