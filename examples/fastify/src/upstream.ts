7792c0945cf727f05f736377b5c632ea3dfeb068
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-04-30 16:57:30 +0300

    Fix prettier

--- examples/fastify/src/upstream.ts
+++ examples/fastify/src/upstream.ts
@@ -1,17 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 export const upstream = fastify();
 
 upstream.route({
-  method: ["GET", "POST"],
-  url: "/pet/:petId",
+  method: ['GET', 'POST'],
+  url: '/pet/:petId',
   async handler(request, reply) {
     const { petId } = request.params as { petId: string };
 
-    if (petId === "pet200") {
-      return reply
-        .status(200)
-        .send({ name: "Bob" });
+    if (petId === 'pet200') {
+      return reply.status(200).send({ name: 'Bob' });
     }
 
     return reply.status(500).send({ error: `Error` });
