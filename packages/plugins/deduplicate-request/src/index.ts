bd92eeeb707544f58a88f6c0d2b5b30fb7833c2f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-05-09 14:47:19 +0300

    fix(deduplicate-request): support other fetch impl

--- packages/plugins/deduplicate-request/src/index.ts
+++ packages/plugins/deduplicate-request/src/index.ts
@@ -10,6 +10,7 @@ export default function useDeduplicateRequest(): MeshPlugin<any> {
     }
     return reqResMap;
   }
+  const resPropMap = new WeakMap<Response, Map<keyof Response, Promise<any>>>();
   return {
     onFetch({ url, options, context, info, fetchFn, setFetchFn }) {
       if (context != null) {
@@ -32,7 +33,28 @@ export default function useDeduplicateRequest(): MeshPlugin<any> {
               reqResMap.set(dedupCacheKey, dedupRes$);
             }
 
-            return dedupRes$;
+            return dedupRes$.then(res => {
+              let resPropMapByRes = resPropMap.get(res);
+              if (!resPropMapByRes) {
+                resPropMapByRes = new Map();
+                resPropMap.set(res, resPropMapByRes);
+              }
+              return new Proxy(res, {
+                get(target, prop: keyof Response) {
+                  if (typeof target[prop] === 'function') {
+                    return (...args: any) => {
+                      let resPropResult$ = resPropMapByRes.get(prop);
+                      if (!resPropResult$) {
+                        resPropResult$ = (target[prop] as any)(...args);
+                        resPropMapByRes.set(prop, resPropResult$);
+                      }
+                      return resPropResult$;
+                    };
+                  }
+                  return target[prop];
+                },
+              });
+            });
           });
         }
       }
--- packages/plugins/deduplicate-request/tests/useDeduplicateRequest.test.ts
+++ packages/plugins/deduplicate-request/tests/useDeduplicateRequest.test.ts
@@ -1,232 +1,247 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Response } from '@whatwg-node/fetch';
+/* eslint-disable import/no-extraneous-dependencies */
+import { Response as NodeFetchResponse } from 'node-fetch';
+import { Response as UndiciResponse } from 'undici';
+import { Response as WhatwgNodeResponse } from '@whatwg-node/fetch';
 import { wrapFetchWithPlugins } from '../../../runtime/src/get-mesh.js';
 import useDeduplicateRequest from '../src/index.js';
 
 describe('useDeduplicateRequest', () => {
-  let reqCount: number;
+  (
+    [
+      [NodeFetchResponse, 'node-fetch'],
+      [UndiciResponse, 'undici'],
+      [WhatwgNodeResponse, '@whatwg-node/fetch'],
+    ] as const
+  ).forEach(([Response, fetchImplName]) => {
+    if (!Response) {
+      return;
+    }
+    describe(fetchImplName, () => {
+      let reqCount: number;
 
-  async function originalFetch() {
-    reqCount++;
-    return new Response(
-      JSON.stringify({
-        data: {
-          hello: 'world',
-        },
-      }),
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-  }
+      async function originalFetch() {
+        reqCount++;
+        return new Response(
+          JSON.stringify({
+            data: {
+              hello: 'world',
+            },
+          }),
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        ) as Response;
+      }
 
-  beforeEach(() => {
-    reqCount = 0;
-  });
+      beforeEach(() => {
+        reqCount = 0;
+      });
 
-  it('should deduplicate the same GET requests in the same context sequentially', async () => {
-    const context = {};
-    const fetchFn = wrapFetchWithPlugins([
-      {
-        onFetch({ setFetchFn }) {
-          setFetchFn(originalFetch);
-        },
-      },
-      useDeduplicateRequest(),
-    ]);
-    const url = 'http://localhost:9856/somePath';
-    const response = await fetchFn(
-      url,
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-      context,
-    );
-    await response.text();
-    const response2 = await fetchFn(
-      url,
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-      context,
-    );
-    await response2.text();
-    expect(reqCount).toBe(1);
-  });
-  it('should deduplicate the same GET request in the same context in parallel', async () => {
-    const context = {};
-    const fetchFn = wrapFetchWithPlugins([
-      {
-        onFetch({ setFetchFn }) {
-          setFetchFn(originalFetch);
-        },
-      },
-      useDeduplicateRequest(),
-    ]);
-    const url = 'http://localhost:9856/somePath';
-    const [response, response2] = await Promise.all([
-      fetchFn(
-        url,
-        {
-          headers: {
-            Accept: 'application/json',
+      it('should deduplicate the same GET requests in the same context sequentially', async () => {
+        const context = {};
+        const fetchFn = wrapFetchWithPlugins([
+          {
+            onFetch({ setFetchFn }) {
+              setFetchFn(originalFetch);
+            },
           },
-        },
-        context,
-      ),
-      fetchFn(
-        url,
-        {
-          headers: {
-            Accept: 'application/json',
+          useDeduplicateRequest(),
+        ]);
+        const url = 'http://localhost:9856/somePath';
+        const response = await fetchFn(
+          url,
+          {
+            headers: {
+              Accept: 'application/json',
+            },
           },
-        },
-        context,
-      ),
-    ]);
-    await Promise.all([response.text(), response2.text()]);
-    expect(reqCount).toBe(1);
-  });
-  it('should not deduplicate the different GET requests in the same context sent sequentially', async () => {
-    const context = {};
-    const fetchFn = wrapFetchWithPlugins([
-      {
-        onFetch({ setFetchFn }) {
-          setFetchFn(originalFetch);
-        },
-      },
-      useDeduplicateRequest(),
-    ]);
-    const url = 'http://localhost:9856/somePath';
-    const response = await fetchFn(
-      url,
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-      context,
-    );
-    await response.text();
-    const response2 = await fetchFn(
-      url + '2',
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-      context,
-    );
-    await response2.text();
-    expect(reqCount).toBe(2);
-  });
-  it('should not deduplicate the different GET requests in the same context sent in parallel', async () => {
-    const context = {};
-    const fetchFn = wrapFetchWithPlugins([
-      {
-        onFetch({ setFetchFn }) {
-          setFetchFn(originalFetch);
-        },
-      },
-      useDeduplicateRequest(),
-    ]);
-    const url = 'http://localhost:9856/somePath';
-    const [response, response2] = await Promise.all([
-      fetchFn(
-        url,
-        {
-          headers: {
-            Accept: 'application/json',
+          context,
+        );
+        await response.text();
+        const response2 = await fetchFn(
+          url,
+          {
+            headers: {
+              Accept: 'application/json',
+            },
           },
-        },
-        context,
-      ),
-      fetchFn(
-        url + '2',
-        {
-          headers: {
-            Accept: 'application/json',
+          context,
+        );
+        await response2.text();
+        expect(reqCount).toBe(1);
+      });
+      it('should deduplicate the same GET request in the same context in parallel', async () => {
+        const context = {};
+        const fetchFn = wrapFetchWithPlugins([
+          {
+            onFetch({ setFetchFn }) {
+              setFetchFn(originalFetch);
+            },
           },
-        },
-        context,
-      ),
-    ]);
-    await Promise.all([response.text(), response2.text()]);
-    expect(reqCount).toBe(2);
-  });
-  it('should not deduplicate the same GET request in different contexts sent sequentially', async () => {
-    const context = {};
-    const context2 = {};
-    const fetchFn = wrapFetchWithPlugins([
-      {
-        onFetch({ setFetchFn }) {
-          setFetchFn(originalFetch);
-        },
-      },
-      useDeduplicateRequest(),
-    ]);
-    const url = 'http://localhost:9856/somePath';
-    const response = await fetchFn(
-      url,
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-      context,
-    );
-    await response.text();
-    const response2 = await fetchFn(
-      url,
-      {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
-      context2,
-    );
-    await response2.text();
-    expect(reqCount).toBe(2);
-  });
-  it('should not deduplicate the same GET request in different contexts sent in parallel', async () => {
-    const context = {};
-    const context2 = {};
-    const fetchFn = wrapFetchWithPlugins([
-      {
-        onFetch({ setFetchFn }) {
-          setFetchFn(originalFetch);
-        },
-      },
-      useDeduplicateRequest(),
-    ]);
-    const url = 'http://localhost:9856/somePath';
-    const [response, response2] = await Promise.all([
-      fetchFn(
-        url,
-        {
-          headers: {
-            Accept: 'application/json',
+          useDeduplicateRequest(),
+        ]);
+        const url = 'http://localhost:9856/somePath';
+        const [response, response2] = await Promise.all([
+          fetchFn(
+            url,
+            {
+              headers: {
+                Accept: 'application/json',
+              },
+            },
+            context,
+          ),
+          fetchFn(
+            url,
+            {
+              headers: {
+                Accept: 'application/json',
+              },
+            },
+            context,
+          ),
+        ]);
+        await Promise.all([response.text(), response2.text()]);
+        expect(reqCount).toBe(1);
+      });
+      it('should not deduplicate the different GET requests in the same context sent sequentially', async () => {
+        const context = {};
+        const fetchFn = wrapFetchWithPlugins([
+          {
+            onFetch({ setFetchFn }) {
+              setFetchFn(originalFetch);
+            },
           },
-        },
-        context,
-      ),
-      fetchFn(
-        url,
-        {
-          headers: {
-            Accept: 'application/json',
+          useDeduplicateRequest(),
+        ]);
+        const url = 'http://localhost:9856/somePath';
+        const response = await fetchFn(
+          url,
+          {
+            headers: {
+              Accept: 'application/json',
+            },
           },
-        },
-        context2,
-      ),
-    ]);
-    await Promise.all([response.text(), response2.text()]);
-    expect(reqCount).toBe(2);
+          context,
+        );
+        await response.text();
+        const response2 = await fetchFn(
+          url + '2',
+          {
+            headers: {
+              Accept: 'application/json',
+            },
+          },
+          context,
+        );
+        await response2.text();
+        expect(reqCount).toBe(2);
+      });
+      it('should not deduplicate the different GET requests in the same context sent in parallel', async () => {
+        const context = {};
+        const fetchFn = wrapFetchWithPlugins([
+          {
+            onFetch({ setFetchFn }) {
+              setFetchFn(originalFetch);
+            },
+          },
+          useDeduplicateRequest(),
+        ]);
+        const url = 'http://localhost:9856/somePath';
+        const [response, response2] = await Promise.all([
+          fetchFn(
+            url,
+            {
+              headers: {
+                Accept: 'application/json',
+              },
+            },
+            context,
+          ),
+          fetchFn(
+            url + '2',
+            {
+              headers: {
+                Accept: 'application/json',
+              },
+            },
+            context,
+          ),
+        ]);
+        await Promise.all([response.text(), response2.text()]);
+        expect(reqCount).toBe(2);
+      });
+      it('should not deduplicate the same GET request in different contexts sent sequentially', async () => {
+        const context = {};
+        const context2 = {};
+        const fetchFn = wrapFetchWithPlugins([
+          {
+            onFetch({ setFetchFn }) {
+              setFetchFn(originalFetch);
+            },
+          },
+          useDeduplicateRequest(),
+        ]);
+        const url = 'http://localhost:9856/somePath';
+        const response = await fetchFn(
+          url,
+          {
+            headers: {
+              Accept: 'application/json',
+            },
+          },
+          context,
+        );
+        await response.text();
+        const response2 = await fetchFn(
+          url,
+          {
+            headers: {
+              Accept: 'application/json',
+            },
+          },
+          context2,
+        );
+        await response2.text();
+        expect(reqCount).toBe(2);
+      });
+      it('should not deduplicate the same GET request in different contexts sent in parallel', async () => {
+        const context = {};
+        const context2 = {};
+        const fetchFn = wrapFetchWithPlugins([
+          {
+            onFetch({ setFetchFn }) {
+              setFetchFn(originalFetch);
+            },
+          },
+          useDeduplicateRequest(),
+        ]);
+        const url = 'http://localhost:9856/somePath';
+        const [response, response2] = await Promise.all([
+          fetchFn(
+            url,
+            {
+              headers: {
+                Accept: 'application/json',
+              },
+            },
+            context,
+          ),
+          fetchFn(
+            url,
+            {
+              headers: {
+                Accept: 'application/json',
+              },
+            },
+            context2,
+          ),
+        ]);
+        await Promise.all([response.text(), response2.text()]);
+        expect(reqCount).toBe(2);
+      });
+    });
   });
 });
