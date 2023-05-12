ded1e302eee488df3f7690fe1aaabb68eeabfaa9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-05-12 15:16:53 +0300

    Use uWebSockets

---
+++ packages/cli/src/commands/serve/fetch-uwebsockets.ts
@@ -0,0 +1,75 @@
+/* eslint-disable import/no-nodejs-modules */
+import { Readable } from 'stream';
+import { HttpRequest, HttpResponse } from 'uWebSockets.js';
+import { MeshHTTPHandler } from '@graphql-mesh/http';
+
+interface ServerContext {
+  req: HttpRequest;
+  res: HttpResponse;
+}
+
+export function getUwebsocketsHandlerForFetch({
+  fetchFn,
+  protocol,
+  hostname,
+  port,
+}: {
+  fetchFn: MeshHTTPHandler<ServerContext>['fetch'];
+  protocol: 'http' | 'https';
+  hostname: string;
+  port: number;
+}) {
+  return async function fetchUwebsocketsHandler(res: HttpResponse, req: HttpRequest) {
+    let body: any;
+    const method = req.getMethod();
+    if (method !== 'get' && method !== 'head') {
+      body = new Readable({
+        read() {},
+      });
+      res
+        .onData(function (chunk, isLast) {
+          body.push(Buffer.from(chunk));
+          if (isLast) {
+            body.push(null);
+          }
+        })
+        .onAborted(function () {
+          body.push(null);
+        });
+    }
+    const headers: Record<string, string> = {};
+    req.forEach((key, value) => {
+      headers[key] = value;
+    });
+    const response = await fetchFn(
+      `${protocol}://${hostname}:${port}${req.getUrl()}`,
+      {
+        method,
+        headers,
+        body,
+      },
+      {
+        req,
+        res,
+      },
+    );
+    res.writeStatus(`${response.status} ${response.statusText}`);
+    response.headers.forEach((value, key) => {
+      // content-length causes an error with Node.js's fetch
+      if (key === 'content-length') {
+        return;
+      }
+      res.writeHeader(key, value);
+    });
+    if (response.body) {
+      if (response.body instanceof Uint8Array) {
+        res.end(response.body);
+        return;
+      }
+      for await (const chunk of response.body) {
+        res.write(chunk);
+      }
+    }
+    res.end();
+  };
+}
