b6d78e27f701dae5333e4fdb5ddda5d88e7565d8
Drew McMillan <drew.mcmillan@thetrainline.com>
2023-05-16 16:17:04 +0100

    Nested resolver bug replication

--- examples/fastify/tests/fastify.spec.ts
+++ examples/fastify/tests/fastify.spec.ts
@@ -1,81 +1,90 @@
-import { AddressInfo } from 'net';
 import { fetch } from '@whatwg-node/fetch';
 import { app } from '../src/app';
 import { upstream } from '../src/upstream';
 
 describe('fastify', () => {
-  beforeAll(() => Promise.all([app.listen(), upstream.listen()]));
+  beforeAll(async () => {
+    await app.listen({
+      port: 4000,
+    });
+    await upstream.listen({
+      port: 4001,
+    });
+  });
 
-  afterAll(() => Promise.all([app.close(), upstream.close()]));
+  afterAll(async () => {
+    await app.close();
+    await upstream.close();
+  });
 
   it('should work', async () => {
-    const upstreamPort = (upstream.server.address() as AddressInfo).port;
-    const response = await fetch(
-      `http://localhost:${(app.server.address() as AddressInfo).port}/graphql`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-upstream-port': upstreamPort.toString(),
-        },
-        body: JSON.stringify({
-          query: /* GraphQL */ `
-            {
-              pet_by_petId(petId: "pet200") {
-                name
+    const response = await fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          {
+            pet_by_petId(petId: "0fc9111f-570d-4ebe-a72e-ff4eb274bc65") {
+              __typename
+              ... on Pet {
+                id
+              }
+              ... on Error {
+                errors
               }
             }
-          `,
-        }),
-      },
-    );
+          }
+        `,
+      }),
+    });
 
     const json = await response.json();
-    expect(json).toMatchObject({
+    expect(json).toEqual({
       data: {
         pet_by_petId: {
-          name: 'Bob',
+          __typename: 'Pet',
+          id: '0fc9111f-570d-4ebe-a72e-ff4eb274bc65',
         },
       },
     });
   });
 
-  it.skip('should work too', async () => {
-    const upstreamPort = (upstream.server.address() as AddressInfo).port;
-    const response = await fetch(
-      `http://localhost:${(app.server.address() as AddressInfo).port}/graphql`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-upstream-port': upstreamPort.toString(),
-        },
-        body: JSON.stringify({
-          query: /* GraphQL */ `
-            {
-              pet_by_petId(petId: "pet500") {
-                name
+  it('should work too', async () => {
+    const response = await fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          {
+            newPet(
+              extraId: "dbd9e7c1-24f1-42e8-bd34-e7ce5bbafd7b"
+              petId: "0fc9111f-570d-4ebe-a72e-ff4eb274bc65"
+            ) {
+              __typename
+              ... on Error {
+                errors
+              }
+              ... on NewPetResponse {
+                foo
               }
             }
-          `,
-        }),
-      },
-    );
+          }
+        `,
+      }),
+    });
 
-    const resJson = await response.json();
-
-    expect(resJson).toEqual({
-      data: { pet_by_petId: null },
-      errors: [
-        {
-          message: 'HTTP Error: 500, Could not invoke operation GET /pet/{args.petId}',
-          path: ['pet_by_petId'],
-          extensions: {
-            request: { url: `http://localhost:${upstreamPort}/pet/pet500`, method: 'GET' },
-            responseJson: { error: 'Error' },
-          },
+    const json = await response.json();
+    expect(json).toEqual({
+      data: {
+        newPet: {
+          __typename: 'NewPetResponse',
+          foo: '{"__typename":"Pet", "id": "0fc9111f-570d-4ebe-a72e-ff4eb274bc65"}',
         },
-      ],
+      },
     });
   });
 });
