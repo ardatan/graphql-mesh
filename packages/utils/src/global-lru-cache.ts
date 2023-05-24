5967b8daf1fdc069c27b3e619f62c67c8afcb2b0
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-05-24 13:31:34 +0300

    ..

--- packages/utils/src/global-lru-cache.ts
+++ packages/utils/src/global-lru-cache.ts
@@ -1,6 +1,6 @@
-import lru, { Lru } from 'tiny-lru';
+import { lru, LRU } from 'tiny-lru';
 
-export type LRUCache<T = any> = Lru<T>;
+export type LRUCache<T = any> = LRU<T>;
 
 export function createLruCache<T = any>(max?: number, ttl?: number): LRUCache<T> {
   return lru(max, ttl);
