b647f509cb3606168ff1f3ba94513e83f9157c3d
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-07-28 16:36:17 +0300

    Test execution

--- packages/loaders/raml/tests/query-params.test.ts
+++ packages/loaders/raml/tests/query-params.test.ts
@@ -1,5 +1,6 @@
-import { GraphQLSchema } from 'graphql';
+import { execute, GraphQLSchema, parse } from 'graphql';
 import { printSchemaWithDirectives } from '@graphql-tools/utils';
+import { Response, URL } from '@whatwg-node/fetch';
 import { loadGraphQLSchemaFromRAML } from '../src/loadGraphQLSchemaFromRAML';
 
 describe('Query Paramters', () => {
@@ -8,9 +9,81 @@ describe('Query Paramters', () => {
     schema = await loadGraphQLSchemaFromRAML('query-params', {
       source: './fixtures/query-params.raml',
       cwd: __dirname,
+      async fetch(url: string) {
+        const parsedUrl = new URL(url);
+        if (parsedUrl.searchParams.get('isApproved') === 'true') {
+          return Response.json([
+            {
+              id: '1',
+              name: 'Approved Product',
+            },
+          ]);
+        } else {
+          return Response.json([
+            {
+              id: '2',
+              name: 'Not Approved Product',
+            },
+          ]);
+        }
+      },
     });
   });
   it('generates correct schema', () => {
     expect(printSchemaWithDirectives(schema)).toMatchSnapshot();
+  });
+  it('respects default values', async () => {
+    const result = await execute({
+      schema,
+      document: parse(/* GraphQL */ `
+        query {
+          listOfProducts {
+            id
+            name
+          }
+        }
+      `),
+    });
+    expect(result.errors).toBeFalsy();
+    expect(result.data).toEqual({
+      listOfProducts: [
+        {
+          id: '1',
+          name: 'Approved Product',
+        },
+      ],
+    });
+  });
+  it('respects arguments with values', async () => {
+    const result = await execute({
+      schema,
+      document: parse(/* GraphQL */ `
+        query {
+          falseOne: listOfProducts(isApproved: false) {
+            id
+            name
+          }
+          trueOne: listOfProducts(isApproved: true) {
+            id
+            name
+          }
+        }
+      `),
+    });
+    expect(result.errors).toBeFalsy();
+    expect(result.data).toEqual({
+      trueOne: [
+        {
+          id: '1',
+          name: 'Approved Product',
+        },
+      ],
+      falseOne: [
+        {
+          id: '2',
+          name: 'Not Approved Product',
+        },
+      ],
+    });
   });
 });
