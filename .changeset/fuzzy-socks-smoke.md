96bef0c8fd58f2a31ec3bb020ea222ba66c7af86
Babak Haj Azim Zanjani <85823065+BabakScript@users.noreply.github.com>
2023-09-15 15:41:20 +0200

    Fix: avoid set lazyConnect in case it's false for cache-redis package (#5970)
    * fix(redis): avoid setting lazyConnect when it's set to false
    
    * test: adopted tests based on new changes on cache-redis + add one new test
    
    * chore: add changeset
    
    * if condition
    
    ---------
    
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

---
+++ .changeset/fuzzy-socks-smoke.md
@@ -0,0 +1,5 @@
+---
+'@graphql-mesh/cache-redis': patch
+---
+
+Avoid setting lazyConnect in case it's set false
--- packages/cache/redis/src/index.ts
+++ packages/cache/redis/src/index.ts
@@ -27,7 +27,10 @@ export default class RedisCache<V = string> implements KeyValueCache<V> {
         throw new Error('Redis URL must use either redis:// or rediss://');
       }
 
-      redisUrl.searchParams.set('lazyConnect', lazyConnect.toString());
+      if (lazyConnect) {
+        redisUrl.searchParams.set('lazyConnect', 'true');
+      }
+
       redisUrl.searchParams.set('enableAutoPipelining', 'true');
       redisUrl.searchParams.set('enableOfflineQueue', 'true');
 
@@ -43,7 +46,7 @@ export default class RedisCache<V = string> implements KeyValueCache<V> {
           host: parsedHost,
           port: parseInt(parsedPort),
           password: parsedPassword,
-          lazyConnect,
+          ...(lazyConnect ? { lazyConnect: true } : {}),
           enableAutoPipelining: true,
           enableOfflineQueue: true,
         });
