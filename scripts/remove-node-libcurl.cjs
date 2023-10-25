01b6ef46c35168ca8e57a8693d07572c218425e3
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-10-25 20:45:59 +0300

    Add Node 20 and 21 (#6142)
    * Add Node 20 and 21
    
    * Go
    
    * Hmm
    
    * Go
    
    * Skip sqlite
    
    * Skip in a right way
    
    * :facepalm:
    
    * Ignore snapshot errors
    
    * Go

---
+++ scripts/remove-node-libcurl.cjs
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJson = fs.readFileSync(path.join(__dirname, '../packages/cli/package.json'), 'utf8');
+const packageJsonParsed = JSON.parse(packageJson);
+delete packageJsonParsed.dependencies['node-libcurl'];
+fs.writeFileSync(
+  path.join(__dirname, '../packages/cli/package.json'),
+  JSON.stringify(packageJsonParsed, null, 2),
+);
