10b01a1794eaf4313ba8084b409260b99b78ee87
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-12-11 13:28:02 +0300

    Fixes for Node 21 support (#6352)
    * Fixes for Node 21 support
    
    * chore(dependencies): updated changesets for modified dependencies
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ .changeset/@graphql-mesh_cli-6352-dependencies.md
@@ -0,0 +1,5 @@
+---
+"@graphql-mesh/cli": patch
+---
+dependencies updates:
+  - Removed dependency [`node-libcurl@^3.0.0` ↗︎](https://www.npmjs.com/package/node-libcurl/v/3.0.0) (from `dependencies`)
--- packages/loaders/sqlite/src/index.ts
+++ packages/loaders/sqlite/src/index.ts
@@ -1,5 +1,4 @@
 import { GraphQLSchema } from 'graphql';
-import { buildSchemaFromDatabase, buildSchemaFromInfile } from 'tuql';
 import { path, process } from '@graphql-mesh/cross-helpers';
 import { createDefaultExecutor } from '@graphql-tools/delegate';
 
@@ -16,14 +15,17 @@ export default function loadGraphQLSchemaFromSQLite(_name: string, opts: GraphQL
 export function loadGraphQLSchemaFromOptions(
   opts: GraphQLSQLiteLoaderOpts,
 ): Promise<GraphQLSchema> {
+  const tuql$ = import('tuql').catch(e => {
+    throw new Error('You need to install `tuql` package in order to use SQLite data source.');
+  });
   const cwd = opts.cwd || process.cwd();
   if (opts.infile != null) {
     const absolutePath = path.isAbsolute(opts.infile) ? opts.infile : path.join(cwd, opts.infile);
-    return buildSchemaFromInfile(absolutePath);
+    return tuql$.then(tuql => tuql.buildSchemaFromInfile(absolutePath));
   }
   if (opts.db != null) {
     const absolutePath = path.isAbsolute(opts.db) ? opts.db : path.join(cwd, opts.db);
-    return buildSchemaFromDatabase(absolutePath);
+    return tuql$.then(tuql => tuql.buildSchemaFromDatabase(absolutePath));
   }
   throw new Error('Invalid options');
 }
