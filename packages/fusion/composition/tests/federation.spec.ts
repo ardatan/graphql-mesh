cc4f921c6a150eaf840bf7b0879228297f1e3178
enisdenjo <denis@denelop.com>
2024-01-26 14:22:36 +0100

    dont write fusiongraph

--- packages/fusion/composition/tests/federation.spec.ts
+++ packages/fusion/composition/tests/federation.spec.ts
@@ -1,12 +1,11 @@
 // eslint-disable-next-line import/no-nodejs-modules
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync } from 'fs';
 // eslint-disable-next-line import/no-nodejs-modules
 import { join } from 'path';
 import { printSchemaWithDirectives } from '@graphql-tools/utils';
 import { convertSupergraphToFusiongraph } from '../src/supergraph';
 
 describe('Federation', () => {
-  const fusiongraphSdlPath = join(__dirname, 'fixtures/fusiongraph.graphql');
   it('converts a Supergraph to a Fusiongraph', async () => {
     const supergraphSdl = readFileSync(
       join(__dirname, 'fixtures/gateway/supergraph.graphql'),
@@ -14,7 +13,6 @@ describe('Federation', () => {
     );
     const fusiongraph = convertSupergraphToFusiongraph(supergraphSdl);
     const fusiongraphSdl = printSchemaWithDirectives(fusiongraph);
-    writeFileSync(fusiongraphSdlPath, fusiongraphSdl);
     expect(fusiongraphSdl).toMatchSnapshot();
   });
 });
