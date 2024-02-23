bb4f99b22af0fe92c6fd809fbc0080ad597c8fa4
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-02-23 17:13:21 +0300

    Update Neo4j

--- packages/legacy/cli/src/config.ts
+++ packages/legacy/cli/src/config.ts
@@ -11,37 +11,39 @@ export function validateConfig(
   initialLoggerPrefix: string,
   throwOnInvalidConfig = false,
 ): asserts config is YamlConfig.Config {
-  const ajv = new Ajv({
-    strict: false,
-  } as any);
-  jsonSchema.$schema = undefined;
-  const isValid = ajv.validate(jsonSchema, config);
-  if (!isValid) {
-    if (throwOnInvalidConfig) {
-      const aggregateError = new AggregateError(
-        ajv.errors.map(e => {
-          const error = new Error(e.message);
-          error.stack += `\n    at ${filepath}:0:0`;
-          return error;
-        }),
-        'Configuration file is not valid',
-      );
-      throw aggregateError;
+  if (jsonSchema) {
+    const ajv = new Ajv({
+      strict: false,
+    } as any);
+    jsonSchema.$schema = undefined;
+    const isValid = ajv.validate(jsonSchema, config);
+    if (!isValid) {
+      if (throwOnInvalidConfig) {
+        const aggregateError = new AggregateError(
+          ajv.errors.map(e => {
+            const error = new Error(e.message);
+            error.stack += `\n    at ${filepath}:0:0`;
+            return error;
+          }),
+          'Configuration file is not valid',
+        );
+        throw aggregateError;
+      }
+      const logger = new DefaultLogger(initialLoggerPrefix).child('config');
+      logger.warn('Configuration file is not valid!');
+      logger.warn("This is just a warning! It doesn't have any effects on runtime.");
+      ajv.errors.forEach(error => {
+        let errorMessage = '';
+        if (error.propertyName) {
+          errorMessage += `Property: ${error.propertyName} \n`;
+        }
+        if (error.data) {
+          errorMessage += `Given: ${error.data} \n`;
+        }
+        errorMessage += `Error: ${error.message}`;
+        logger.warn(errorMessage);
+      });
     }
-    const logger = new DefaultLogger(initialLoggerPrefix).child('config');
-    logger.warn('Configuration file is not valid!');
-    logger.warn("This is just a warning! It doesn't have any effects on runtime.");
-    ajv.errors.forEach(error => {
-      let errorMessage = '';
-      if (error.propertyName) {
-        errorMessage += `Property: ${error.propertyName} \n`;
-      }
-      if (error.data) {
-        errorMessage += `Given: ${error.data} \n`;
-      }
-      errorMessage += `Error: ${error.message}`;
-      logger.warn(errorMessage);
-    });
   }
 }
 
