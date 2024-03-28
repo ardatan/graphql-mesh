5aa88dc9d17beff6e29453c75ea39eb0e0a70050
enisdenjo <denis@denelop.com>
2024-03-28 13:55:23 +0100

    revert some things now that moduleres is node

--- packages/legacy/http/test/http.spec.ts
+++ packages/legacy/http/test/http.spec.ts
@@ -1,6 +1,6 @@
 import { createMeshHTTPHandler } from '@graphql-mesh/http';
 import { MeshInstance } from '@graphql-mesh/runtime';
-import { getTestMesh } from '../../testing/getTestMesh.mjs';
+import { getTestMesh } from '../../testing/getTestMesh.js';
 
 describe('http', () => {
   let mesh: MeshInstance;
