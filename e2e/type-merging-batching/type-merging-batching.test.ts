c45ecdb6497ede3a32dea8df4f9b6a14428ecbb9
enisdenjo <denis@denelop.com>
2024-04-03 21:17:18 +0200

    test plans

--- e2e/type-merging-batching/type-merging-batching.test.ts
+++ e2e/type-merging-batching/type-merging-batching.test.ts
@@ -1,13 +1,106 @@
+import { buildSchema, parse } from 'graphql';
+import {
+  createExecutablePlanForOperation,
+  serializeExecutableOperationPlan,
+} from '@graphql-mesh/fusion-execution';
 import { createTenv } from '../tenv';
 
 const { compose, subgraph } = createTenv(__dirname);
 
 it('should compose the appropriate schema', async () => {
-  const proc = await compose({
+  const { result } = await compose({
     subgraphs: [await subgraph('authors'), await subgraph('books')],
     maskSubgraphPorts: true,
   });
-  expect(proc.result).toMatchSnapshot();
+  expect(result).toMatchSnapshot();
 });
 
-// TODO: rest of tests
+const queries = [
+  {
+    name: 'Author',
+    document: parse(/* GraphQL */ `
+      query Author {
+        author(id: 1) {
+          id
+          name
+          books {
+            id
+            title
+            author {
+              id
+              name
+            }
+          }
+        }
+      }
+    `),
+  },
+  {
+    name: 'Authors',
+    document: parse(/* GraphQL */ `
+      query Authors {
+        authors {
+          id
+          name
+          books {
+            id
+            title
+            author {
+              id
+              name
+            }
+          }
+        }
+      }
+    `),
+  },
+  {
+    name: 'Book',
+    document: parse(/* GraphQL */ `
+      query Book {
+        book(id: 1) {
+          id
+          title
+          author {
+            id
+            name
+            books {
+              id
+              title
+            }
+          }
+        }
+      }
+    `),
+  },
+  {
+    name: 'Books',
+    document: parse(/* GraphQL */ `
+      query Books {
+        books {
+          id
+          title
+          author {
+            id
+            name
+          }
+        }
+      }
+    `),
+  },
+];
+
+it.each(queries)('should properly plan $name', async ({ document }) => {
+  const { result } = await compose({
+    subgraphs: [await subgraph('authors'), await subgraph('books')],
+  });
+
+  const plan = createExecutablePlanForOperation({
+    fusiongraph: buildSchema(result, {
+      assumeValid: true,
+    }),
+    document,
+  });
+
+  expect(serializeExecutableOperationPlan(plan)).toMatchSnapshot();
+});
