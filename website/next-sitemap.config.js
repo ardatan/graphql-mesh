425249afe7b58bc27a10bbe071a4565e88931a3f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-04-11 17:01:11 +0200

    aa

--- website/next-sitemap.config.js
+++ website/next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
+export default {
   siteUrl: process.env.SITE_URL || 'https://the-guild.dev/graphql/mesh',
   generateIndexSitemap: false,
   exclude: ['*/_meta'],
