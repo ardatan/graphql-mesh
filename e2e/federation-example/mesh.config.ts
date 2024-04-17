77ded272a6427c0609e7fe57af21933e97d2de4a
enisdenjo <denis@denelop.com>
2024-04-17 18:22:18 +0200

    simplify

--- e2e/federation-example/mesh.config.ts
+++
@@ -1,9 +0,0 @@
-import { Args } from '@e2e/args';
-import { defineConfig } from '@graphql-mesh/serve-cli';
-
-const args = Args(process.argv);
-
-export const serveConfig = defineConfig({
-  port: args.getPort(),
-  supergraph: args.get('supergraph'),
-});
