564fe08f25f63ae2103388ab97048b8cdf5d189a
enisdenjo <badurinadenis@gmail.com>
2024-05-08 16:17:35 +0200

    but not the same req instance

--- e2e/utils/workers/benchGraphQLServer.js
+++ e2e/utils/workers/benchGraphQLServer.js
@@ -11,14 +11,16 @@ import { expose } from 'threads/worker';
  * @returns {Promise<number>} The slowest request's duration in milliseconds.
  */
 export async function benchGraphQLServer(port, duration, parallelCount, params) {
-  const req = new Request(`http://0.0.0.0:${port}/graphql`, {
+  const url = `http://0.0.0.0:${port}/graphql`;
+  /** @type {RequestInit} */
+  const init = {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
       accept: 'application/graphql-response+json, application/json',
     },
     body: JSON.stringify(params),
-  });
+  };
   let slowestRequest = 0;
   const signal = AbortSignal.timeout(duration);
   while (!signal.aborted) {
@@ -26,7 +28,7 @@ export async function benchGraphQLServer(port, duration, parallelCount, params) 
       Array(parallelCount).fill(
         (async () => {
           const start = Date.now();
-          const res = await fetch(req);
+          const res = await fetch(url, init);
           if (!res.ok) {
             const err = new Error(`${res.status} ${res.statusText}\n${await res.text()}`);
             err.name = 'ResponseError';
