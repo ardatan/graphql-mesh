188c452f3832750f81b6b976da0154820b5ed34c
enisdenjo <denis@denelop.com>
2024-05-14 20:28:29 +0200

    thirft-calculator loadtest

---
+++ e2e/thrift-calculator/thrift-calculator.loadtest.ts
@@ -0,0 +1,43 @@
+import os from 'os';
+import { createTbench, Tbench, TbenchResult } from '@e2e/tbench';
+import { createTenv } from '@e2e/tenv';
+
+const { serve, compose, service } = createTenv(__dirname);
+
+let tbench: Tbench;
+beforeAll(async () => {
+  tbench = await createTbench(
+    // to give space for jest and the serve process.
+    os.availableParallelism() - 2,
+  );
+});
+
+const threshold: TbenchResult = {
+  maxCpu: Infinity, // we dont care
+  maxMem: 500, // MB
+  slowestRequest: 1, // second
+};
+
+it(`should perform within threshold ${JSON.stringify(threshold)}`, async () => {
+  const { output } = await compose({
+    services: [await service('calculator')],
+    output: 'graphql',
+  });
+  const server = await serve({ fusiongraph: output });
+  const result = await tbench.sustain({
+    server,
+    params: {
+      query: /* GraphQL */ `
+        query Add {
+          add(request: { left: 2, right: 3 })
+        }
+      `,
+    },
+  });
+
+  console.debug(result);
+
+  expect(result.maxCpu).toBeLessThan(threshold.maxCpu);
+  expect(result.maxMem).toBeLessThan(threshold.maxMem);
+  expect(result.slowestRequest).toBeLessThan(threshold.slowestRequest);
+});
