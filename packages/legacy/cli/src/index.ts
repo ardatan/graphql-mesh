b90e8746a76e5576de7155e8412ea5f629c18546
Gil Gardosh <gilgardosh@gmail.com>
2024-05-21 13:39:37 +0300

    changeset and some notes

--- packages/legacy/cli/src/index.ts
+++ packages/legacy/cli/src/index.ts
@@ -25,6 +25,7 @@ import { generateTsArtifacts } from './commands/ts-artifacts.js';
 import { findAndParseConfig } from './config.js';
 import { handleFatalError } from './handleFatalError.js';
 
+// Note: required to make build command compatible with esm
 register('ts-node/esm', pathToFileURL('./'));
 
 export { generateTsArtifacts, serveMesh, findAndParseConfig, handleFatalError };
