f82dea1a6409a849fcf3688fbfbcf3f94af3e538
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-06-18 16:42:17 +0300

    Add more

---
+++ website/src/pages/v1/serve/features/auth/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  index: 'Overview',
+  auth0: 'Auth0',
+  jwt: 'JSON Web Tokens',
+  'generic-auth': 'Generic Auth',
+  'operation-field-permissions': 'Operation Field Permissions',
+};
