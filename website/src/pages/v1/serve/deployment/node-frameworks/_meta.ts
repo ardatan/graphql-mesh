82b0c01a9f2033060930375e59294fd0ff2fcf3f
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-07-04 20:36:45 +0300

    [v1-docs] Deployment (#7201)
    * [v1-docs] Deployment
    
    * Finish
    
    * Fix healthcheck endpoint
    
    * Update docker.mdx

---
+++ website/src/pages/v1/serve/deployment/node-frameworks/_meta.ts
@@ -0,0 +1,10 @@
+export default {
+  index: 'Introduction',
+  express: 'Express',
+  fastify: 'Fastify',
+  koa: 'Koa',
+  hapi: 'Hapi',
+  nestjs: 'NestJS',
+  uwebsockets: 'µWebSockets.js',
+  nextjs: 'Next.js',
+};
