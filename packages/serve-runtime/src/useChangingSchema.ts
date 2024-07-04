8aa252d71a6769735f4186e14d59d59f858be8a2
enisdenjo <denis@denelop.com>
2024-07-04 18:32:18 +0200

    fix prometheus and double set

--- packages/serve-runtime/src/useChangingSchema.ts
+++ packages/serve-runtime/src/useChangingSchema.ts
@@ -6,33 +6,32 @@ export function useChangingSchema(
   getSchema: () => MaybePromise<GraphQLSchema>,
   schemaChanged: (cb: (schema: GraphQLSchema) => void) => void,
 ): MeshServePlugin {
-  let schemaSet = false;
+  let currentSchema: GraphQLSchema | undefined;
   const schemaByRequest = new WeakMap<Request, GraphQLSchema>();
   return {
     onPluginInit({ setSchema }) {
       schemaChanged(schema => {
-        setSchema(schema);
-        schemaSet = true;
+        if (currentSchema !== schema) {
+          setSchema(schema);
+          currentSchema = schema;
+        }
       });
     },
     onRequestParse({ request }) {
       return {
         async onRequestParseDone() {
-          if (!schemaSet) {
+          if (!currentSchema) {
             // only if the schema is not already set do we want to get it
-            schemaByRequest.set(request, await getSchema());
+            const schema = await getSchema();
+            currentSchema = schema;
+            schemaByRequest.set(request, schema);
           }
         },
       };
     },
     onEnveloped({ context, setSchema }) {
-      if (context?.request == null) {
-        throw new Error(
-          'Request object is not available in the context. Make sure you use this plugin with GraphQL Mesh.',
-        );
-      }
-      const schema = schemaByRequest.get(context.request);
-      if (schema && !schemaSet) {
+      const schema = context?.request && schemaByRequest.get(context.request);
+      if (schema && schema !== currentSchema) {
         // the schema will be available in the request only if schemaChanged was never invoked
         // also avoid setting the schema multiple times by checking whether it was already set
         setSchema(schema);
