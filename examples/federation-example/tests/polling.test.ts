a5a2832a775d68a081d730da00548c0ea20c9dad
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-07-05 19:31:08 +0300

    Try to fix

--- examples/federation-example/tests/polling.test.ts
+++ examples/federation-example/tests/polling.test.ts
@@ -60,7 +60,7 @@ describe('Polling Test', () => {
         }
       });
     });
-    const resp = await fetch('http://127.0.0.1:4000/graphql', {
+    const resp = await fetch('http://localhost:4000/graphql', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
