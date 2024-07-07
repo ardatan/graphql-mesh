aa749c4eca8c8e91595e9ced4b3d4d4d260b1713
Drew McMillan <drew.mcmillan@thetrainline.com>
2024-07-07 12:31:50 +0100

    Non-nullable additional resolver example

---
+++ e2e/openapi-additional-resolvers/mesh.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig as defineComposeConfig } from '@graphql-mesh/compose-cli';
+import { defineConfig as defineServeConfig } from '@graphql-mesh/serve-cli';
+import { loadOpenAPISubgraph } from '@omnigraph/openapi';
+
+export const composeConfig = defineComposeConfig({
+  subgraphs: [
+    {
+      sourceHandler: loadOpenAPISubgraph('Wiki', {
+        source: 'https://wikimedia.org/api/rest_v1/?spec',
+        endpoint: 'https://wikimedia.org/api/rest_v1',
+      }),
+    },
+  ],
+  additionalTypeDefs: /* GraphQL */ `
+    extend type pageview_project {
+      banana: String
+      apple: String!
+    }
+  `,
+});
+
+export const serveConfig = defineServeConfig({
+  additionalResolvers: {
+    pageview_project: {
+      banana() {
+        return '🍌';
+      },
+      apple() {
+        return '🍎';
+      }
+    },
+  },
+});
