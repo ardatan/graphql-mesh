d7053e8dd13493f260935ed917491eff148fae47
enisdenjo <denis@denelop.com>
2024-07-16 15:47:53 +0200

    debug docker e2e

--- e2e/docker-image/docker-image.test.ts
+++ e2e/docker-image/docker-image.test.ts
@@ -3,10 +3,10 @@ import { createTenv } from '@e2e/tenv';
 const { container, spawn } = createTenv(__dirname);
 
 it('should build, start and pass healthchecks', async () => {
-  const [, waitForBundle] = await spawn('yarn workspace @graphql-mesh/serve-cli bundle', {
-    pipeLogs: true,
-  });
-  await waitForBundle;
+  // const [, waitForBundle] = await spawn('yarn workspace @graphql-mesh/serve-cli bundle', {
+  //   pipeLogs: true,
+  // });
+  // await waitForBundle;
 
   // SUGGESTION: bake project first so that the tests dont time out during build phase
 
