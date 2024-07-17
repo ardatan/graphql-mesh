769413dffa89e11e727d541156112fb9fe4d95ad
enisdenjo <denis@denelop.com>
2024-07-17 12:04:01 +0200

    test(serve-runtime): separate hive test to avoid jest fake timers

--- packages/serve-runtime/tests/serve-runtime.spec.ts
+++ packages/serve-runtime/tests/serve-runtime.spec.ts
@@ -1,7 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { createServer } from 'http';
-import type { AddressInfo } from 'net';
-import AsyncDisposableStack from 'disposablestack/AsyncDisposableStack';
 import {
   buildClientSchema,
   buildSchema,
@@ -132,54 +128,6 @@ describe('Serve Runtime', () => {
           });
         });
       });
-    });
-  });
-  describe('Hive CDN', () => {
-    afterEach(() => {
-      delete process.env.HIVE_CDN_ENDPOINT;
-      delete process.env.HIVE_CDN_KEY;
-    });
-    it('respects env vars', async () => {
-      jest.useRealTimers();
-      await using disposableStack = new AsyncDisposableStack();
-      const cdnServer = createServer((req, res) => {
-        const supergraph = getUnifiedGraphGracefully([
-          {
-            name: 'upstream',
-            schema: createUpstreamSchema(),
-          },
-        ]);
-        res.end(supergraph);
-      });
-      await new Promise<void>(resolve => {
-        cdnServer.listen(0, resolve);
-      });
-      disposableStack.defer(
-        () =>
-          new Promise<void>((resolve, reject) => {
-            cdnServer.close(err => (err ? reject(err) : resolve()));
-          }),
-      );
-      process.env.HIVE_CDN_ENDPOINT = `http://localhost:${(cdnServer.address() as AddressInfo).port}`;
-      process.env.HIVE_CDN_KEY = 'key';
-      const serveRuntime = createServeRuntime();
-      disposableStack.use(serveRuntime);
-      const res = await serveRuntime.fetch('http://localhost:4000/graphql', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          query: getIntrospectionQuery({
-            descriptions: false,
-          }),
-        }),
-      });
-
-      expect(res.status).toBe(200);
-      const resJson: ExecutionResult<IntrospectionQuery> = await res.json();
-      const clientSchema = buildClientSchema(resJson.data);
-      expect(printSchema(clientSchema)).toMatchSnapshot('hive-cdn');
     });
   });
   describe('Defaults', () => {
