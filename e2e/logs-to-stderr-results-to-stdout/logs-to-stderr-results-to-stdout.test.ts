d0039c5a86c610c63b7f7e7b388b299be9d050ca
enisdenjo <denis@denelop.com>
2024-07-17 17:45:40 +0200

    stderr and stdout dont need to be snapshotted

--- e2e/logs-to-stderr-results-to-stdout/logs-to-stderr-results-to-stdout.test.ts
+++ e2e/logs-to-stderr-results-to-stdout/logs-to-stderr-results-to-stdout.test.ts
@@ -4,14 +4,10 @@ const { serve, compose } = createTenv(__dirname);
 
 it('should write serve logs to stderr', async () => {
   await using serveInstance = await serve();
-
-  expect(serveInstance.getStd('out')).toBeFalsy();
   expect(serveInstance.getStd('err')).toContain('Starting server on');
 });
 
 it('should write compose output to stdout and logs to stderr', async () => {
   const { getStd } = await compose();
-
-  expect(getStd('out')).toMatchSnapshot();
   expect(getStd('err')).toContain('Done!');
 });
