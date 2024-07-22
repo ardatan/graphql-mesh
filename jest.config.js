1164894013c4f8d47afcbaf8278fac6c7832419f
enisdenjo <denis@denelop.com>
2024-07-22 20:22:12 +0200

    more than 5 workers breaks docker

--- jest.config.js
+++ jest.config.js
@@ -44,6 +44,10 @@ if (process.env.E2E_TEST) {
 }
 /** @type {import('jest').Config} */
 module.exports = {
+  maxWorkers:
+    process.env.E2E_SERVE_RUNNER === 'docker'
+      ? 5 // TODO: running with more than 5 workers breaks docker
+      : undefined,
   prettierPath: null, // not supported before Jest v30 https://github.com/jestjs/jest/issues/14305
   testEnvironment: 'node',
   rootDir: ROOT_DIR,
