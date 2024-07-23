0858feffd2bec3e618cbba72130b48cddb36dd8d
enisdenjo <denis@denelop.com>
2024-07-23 19:25:29 +0200

    handle graphql modules

--- packages/serve-cli/rollup.config.js
+++ packages/serve-cli/rollup.config.js
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import path from 'node:path';
 import { defineConfig } from 'rollup';
 import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
@@ -83,21 +84,12 @@ export default defineConfig({
     // system (`/node_modules`)
     chunkFileNames: 'node_modules/.chunk/[name]-[hash].mjs',
   },
-  external: [
-    'uWebSockets.js',
-    'node-libcurl',
-    'tuql',
-    'graphql',
-    'graphql/execution/values.js', // because of https://github.com/n1ru4l/graphql-live-query/blob/beda6eb5a002e9d3b638af185f235951ed8f646d/packages/in-memory-live-query-store/src/extractLiveQueryRootFieldCoordinates.ts#L10
-  ],
+  external: ['uWebSockets.js', 'node-libcurl', 'tuql'],
   plugins: [
     tsConfigPaths(), // use tsconfig paths to resolve modules
     nodeResolve({ preferBuiltins: true }), // resolve node_modules and bundle them too
-    commonjs({
-      esmExternals: [
-        'graphql', // bundled dependency is an esm module
-      ],
-    }), // convert commonjs to esm
+    graphql(), // handle graphql imports
+    commonjs(), // convert commonjs to esm
     json(), // support importing json files to esm (needed for commonjs() plugin)
     sucrase({ transforms: ['typescript'] }), // transpile typescript
     packagejson(), // add package jsons
@@ -125,6 +117,137 @@ function packagejson() {
           source: JSON.stringify(pkg),
         });
       }
+    },
+  };
+}
+
+/**
+ * Marks all "graphql*" module imports as external and fixes the imports to match
+ * the node 16 style (append `.js` and `/index.js` for directories) where necessary.
+ *
+ * Furthermore, it also converts all default imports of the "graphql*" modules to
+ * separate namespace imports, essentially:
+ *
+ * ```ts
+ * import gql, { some, other, imports } from 'graphql*'
+ * ```
+ *
+ * transforms to:
+ *
+ * ```ts
+ * import * as gql from 'graphql'
+ * import { some, other, imports } from 'graphql*'
+ * ```
+ *
+ * @type {import('rollup').PluginImpl}
+ */
+function graphql() {
+  return {
+    name: 'graphql',
+    async resolveId(source) {
+      if (source === 'graphql') {
+        // import 'graphql'
+        return { id: source, external: true };
+      }
+      if (!source.startsWith('graphql/')) {
+        // not import 'graphql/*'
+        return null;
+      }
+      if (source.endsWith('.js')) {
+        // proper node 16 import
+        return { id: source, external: true };
+      }
+
+      const relPath = source.replace('graphql/', '');
+      if (!relPath) {
+        throw new Error(
+          `Importing "${source}" from "${importer}" is not a graphql module relative import`,
+        );
+      }
+
+      // NOTE: cwd must be here
+      // NOTE: the installed graphql must match the graphql in the Dockerfile
+      const graphqlModulePath = path.resolve('..', '..', 'node_modules', 'graphql');
+      try {
+        fs.lstatSync(graphqlModulePath);
+      } catch (e) {
+        console.error(`"graphql" module not found in ${graphqlPath}. Have you run "yarn"?`);
+        throw e;
+      }
+
+      try {
+        if (fs.lstatSync(path.join(graphqlModulePath, relPath)).isDirectory()) {
+          // isdir
+          return {
+            id: source + '/index.js',
+            external: true,
+          };
+        }
+      } catch {
+        // noop
+      }
+
+      // isfile or doesnt exist
+      return {
+        id: source + '.js',
+        external: true,
+      };
+    },
+    renderChunk(code) {
+      if (!code.includes("from 'graphql")) {
+        // code doesnt include a "graphql*" import
+        return null;
+      }
+
+      let augmented = code;
+      for (const line of code.split('\n')) {
+        if (!line.startsWith('import ')) {
+          // not an import line
+          continue;
+        }
+        if (!line.includes("from 'graphql")) {
+          // line doesnt include a "graphql*" import
+          continue;
+        }
+        if (line.startsWith('import {')) {
+          // no default import, ok
+          continue;
+        }
+
+        let defaultImportPart = line.match(/import(.*) {/)?.[1]; // default + named
+        const hasNamedImports = !!defaultImportPart;
+        defaultImportPart ??= line.match(/import(.*) from/)?.[1]; // just default
+        if (!defaultImportPart) {
+          throw new Error(`Unable to match default import on:\n${line}`);
+        }
+
+        const module = line.split(' from ')?.[1];
+        if (!module) {
+          throw new Error(`Unable to detect module on:\n${line}`);
+        }
+
+        const namespaceImportLine = `import * as ${
+          defaultImportPart
+            .trim() // remove spaces
+            .replace(/,$/, '') // remove last comma
+        } from ${module}`;
+        const lineWithoutDefaultImport = line.replace(defaultImportPart, '');
+
+        augmented = augmented.replace(
+          line,
+          // NOTE: we use replacer instead because strings can mess up dollar signs
+          //       see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_the_replacement
+          () => {
+            if (hasNamedImports) {
+              return `${lineWithoutDefaultImport}\n${namespaceImportLine}`;
+            } else {
+              // no named imports, so we just need the namespace import line
+              return namespaceImportLine;
+            }
+          },
+        );
+      }
+      return augmented;
     },
   };
 }
