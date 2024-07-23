26c05809cdbba86aa302007ce78909b6875bea88
YassinEldeeb <yassineldeeb94@gmail.com>
2024-07-23 11:12:53 +0100

    feat: bundle mesh as a binary

---
+++ packages/serve-cli/bundle.sh
@@ -0,0 +1,17 @@
+rm mesh-serve
+rm sea-prep.blobbun
+
+node --experimental-sea-config sea-config.json
+
+cp $(command -v node) mesh-serve
+
+codesign --remove-signature mesh-serve
+
+npx postject mesh-serve NODE_SEA_BLOB sea-prep.blob \
+    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
+    --macho-segment-name NODE_SEA
+
+codesign --sign - mesh-serve
+
+
+./mesh-serve
