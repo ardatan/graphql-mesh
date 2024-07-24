7b8c21ee28bfbca98ac167369d3eafc090e263da
enisdenjo <denis@denelop.com>
2024-07-24 11:25:02 +0200

    cli defaultImportFn gets default

--- packages/legacy/cli/src/defaultImportFn.ts
+++ packages/legacy/cli/src/defaultImportFn.ts
@@ -3,6 +3,20 @@ import type { ImportFn } from '@graphql-mesh/types';
 
 const jiti = createJITI(__filename);
 
-export const defaultImportFn: ImportFn = module => {
-  return jiti.import(module, {}) as Promise<any>;
+export const defaultImportFn: ImportFn = async id => {
+  let module: any = await jiti.import(id, {});
+  if (module.default != null) {
+    module = module.default;
+  }
+  if (typeof module === 'object' && module != null) {
+    const prototypeOfObject = Object.getPrototypeOf(module);
+    if (prototypeOfObject == null || prototypeOfObject === Object.prototype) {
+      const normalizedVal: Record<string, any> = {};
+      for (const key in module) {
+        normalizedVal[key] = module[key];
+      }
+      return normalizedVal;
+    }
+  }
+  return module;
 };
