300d3dc5b66d848f62578482afe36d7e80722b72
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-07-31 16:08:21 +0300

    Request/Response Compression Documentation + New Mesh Plugin to handle upstream requests

---
+++ packages/serve-runtime/src/useContentEncoding.ts
@@ -0,0 +1,60 @@
+import type { FetchAPI } from 'graphql-yoga';
+import type { ExecutionRequest } from '@graphql-tools/utils';
+import { useContentEncoding as useOrigContentEncoding } from '@whatwg-node/server';
+import type { MeshServePlugin } from './types';
+
+export interface UseContentEncodingOpts {
+  subgraphs?: string[];
+}
+
+export function useContentEncoding<TContext>({
+  subgraphs,
+}: UseContentEncodingOpts): MeshServePlugin<TContext> {
+  if (!subgraphs?.length) {
+    return useOrigContentEncoding();
+  }
+  const compressionAlgorithm: CompressionFormat = 'gzip';
+  let fetchAPI: FetchAPI;
+  const execReqWithContentEncoding = new WeakSet<ExecutionRequest>();
+  return {
+    onYogaInit({ yoga }) {
+      fetchAPI = yoga.fetchAPI;
+    },
+    onPluginInit({ addPlugin }) {
+      addPlugin(
+        // @ts-expect-error - Typings are wrong
+        useOrigContentEncoding(),
+      );
+    },
+    onSubgraphExecute({ subgraphName, executionRequest }) {
+      if (subgraphs.includes(subgraphName)) {
+        execReqWithContentEncoding.add(executionRequest);
+      }
+    },
+    onFetch({ executionRequest, options, setOptions }) {
+      if (
+        options.body &&
+        !options.headers?.['Content-Encoding'] &&
+        execReqWithContentEncoding.has(executionRequest)
+      ) {
+        const compressionStream = new fetchAPI.CompressionStream(compressionAlgorithm);
+        let bodyStream: ReadableStream;
+        if (options.body instanceof fetchAPI.ReadableStream) {
+          bodyStream = options.body;
+        } else {
+          // Create a fake Response and use its body to pipe through the compression stream
+          bodyStream = new fetchAPI.Response(options.body).body;
+        }
+        setOptions({
+          ...options,
+          headers: {
+            'Accept-Encoding': 'gzip, deflate',
+            ...options.headers,
+            'Content-Encoding': compressionAlgorithm,
+          },
+          body: bodyStream.pipeThrough(compressionStream),
+        });
+      }
+    },
+  };
+}
