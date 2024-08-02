b3d17a38b8098386b0577ff5270805be63cb76d1
enisdenjo <badurinadenis@gmail.com>
2024-08-02 22:36:28 +0200

    WIP program and proxy command

---
+++ packages/serve-cli/src/program.ts
@@ -0,0 +1,93 @@
+import 'dotenv/config'; // inject dotenv options to process.env
+import 'json-bigint-patch'; // JSON.parse/stringify with bigints support
+
+import { availableParallelism, release } from 'node:os';
+import { Command, InvalidArgumentError, Option } from '@commander-js/extra-typings';
+import type { Logger } from '@graphql-mesh/types';
+import { DefaultLogger } from '@graphql-mesh/utils';
+import { defaultConfigPaths } from './config.js';
+
+export const defaultFork = process.env.NODE_ENV === 'production' ? availableParallelism() : 1;
+
+/** The root program of serve-cli. */
+export const program = new Command()
+  .configureHelp({
+    // will print help of global options for each command
+    showGlobalOptions: true,
+  })
+  .addOption(
+    new Option(
+      '--fork [count]',
+      'count of workers to spawn. defaults to `os.availableParallelism()` when NODE_ENV is "production", otherwise only one (the main) worker',
+    )
+      .env('FORK')
+      .argParser(v => {
+        const count = parseInt(v);
+        if (isNaN(count)) {
+          throw new InvalidArgumentError('not a number.');
+        }
+        return count;
+      })
+      .default(defaultFork),
+  )
+  .addOption(
+    new Option(
+      '-c, --config-path <path>',
+      `path to the configuration file. defaults to the following files respectively in the current working directory: ${defaultConfigPaths.join(', ')}`,
+    ).env('CONFIG_PATH'),
+  )
+  .option(
+    '-h, --host <hostname>',
+    'host to use for serving',
+    release().toLowerCase().includes('microsoft') ? '127.0.0.1' : '0.0.0.0',
+  )
+  .addOption(
+    new Option('-p, --port <number>', 'port to use for serving')
+      .env('PORT')
+      .default(4000)
+      .argParser(v => {
+        const port = parseInt(v);
+        if (isNaN(port)) {
+          throw new InvalidArgumentError('not a number.');
+        }
+        return port;
+      }),
+  )
+  .addOption(
+    new Option('--polling <intervalInMs>', 'schema polling interval in milliseconds')
+      .env('POLLING')
+      .argParser(v => {
+        const interval = parseInt(v);
+        if (isNaN(interval)) {
+          throw new InvalidArgumentError('not a number.');
+        }
+        return interval;
+      }),
+  )
+  .option('--masked-errors', 'mask unexpected errors in responses')
+  .option('--hive-usage-token [token]', 'Hive registry token for usage metrics');
+
+/** The context of the running program. */
+export type Program = {
+  /** @default new DefaultLogger() */
+  log: Logger;
+  /** @default Mesh Serve */
+  productName: string;
+  /** @default serve GraphQL federated architecture for any API service(s) */
+  productDescription: string;
+  /** @default mesh-serve */
+  binName: string;
+  /** @default globalThis.__VERSION__ */
+  version: string;
+};
+
+export const defaultProgram: Program = {
+  log: new DefaultLogger(),
+  productName: 'Mesh',
+  productDescription: 'serve GraphQL federated architecture for any API service(s)',
+  binName: 'mesh-serve',
+  version: globalThis.__VERSION__,
+};
+
+/** Inferred program options from the root command {@link program}. */
+export type ProgramOptions = typeof program extends Command<any, infer O> ? O : never;
