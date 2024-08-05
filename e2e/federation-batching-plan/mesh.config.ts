1ab2a39d9c6c9a773af9f78d76bb271eedabe92b
enisdenjo <badurinadenis@gmail.com>
2024-08-05 11:50:26 +0200

    add plan

--- e2e/federation-batching-plan/mesh.config.ts
+++ e2e/federation-batching-plan/mesh.config.ts
@@ -1,8 +1,40 @@
+import { print } from 'graphql';
 import { defineConfig } from '@graphql-mesh/serve-cli';
 import type { MeshServePlugin } from '@graphql-mesh/serve-runtime';
 
 export function useExplainQueryPlan(): MeshServePlugin {
-  return {};
+  const plans = new WeakMap<Request, unknown[]>();
+  return {
+    onExecute({
+      args: {
+        contextValue: { request },
+      },
+    }) {
+      plans.set(request, []);
+      return {
+        onExecuteDone({ result, setResult }) {
+          setResult({
+            ...result,
+            extensions: {
+              ...result['extensions'],
+              plan: plans.get(request),
+            },
+          });
+        },
+      };
+    },
+    onSubgraphExecute({
+      subgraphName,
+      executionRequest: {
+        context: { request },
+        document,
+        variables,
+      },
+    }) {
+      const plan = plans.get(request)!;
+      plan.push({ subgraphName, query: print(document), variables });
+    },
+  };
 }
 
 export const serveConfig = defineConfig({
