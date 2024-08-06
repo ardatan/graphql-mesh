fbd99ee6d1390372c995dbb63128f81b7f8a2fb3
enisdenjo <badurinadenis@gmail.com>
2024-08-06 15:21:08 +0200

    start command, is also default

--- packages/serve-cli/src/commands/index.ts
+++ packages/serve-cli/src/commands/index.ts
@@ -2,8 +2,10 @@ import type { AddCommand } from '../cli.js';
 import { addCommand as addHiveCommand } from './hive.js';
 import { addCommand as addLocalCommand } from './local.js';
 import { addCommand as addProxyCommand } from './proxy.js';
+import { addCommand as addStartCommand } from './start.js';
 
 export const addCommands: AddCommand = (ctx, cli) => {
+  addStartCommand(ctx, cli);
   addLocalCommand(ctx, cli);
   addHiveCommand(ctx, cli);
   addProxyCommand(ctx, cli);
