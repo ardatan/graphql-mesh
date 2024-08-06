9cbd100d37020974fe5b9c1ed48f04f05c1d4db5
enisdenjo <badurinadenis@gmail.com>
2024-08-06 15:21:08 +0200

    start command, is also default

---
+++ packages/serve-cli/src/commands/start.ts
@@ -0,0 +1,30 @@
+import { createServeRuntime } from '@graphql-mesh/serve-runtime';
+import type { AddCommand, CLIGlobals } from '../cli.js';
+import { loadConfig } from '../config.js';
+import { startServerForRuntime } from '../server.js';
+
+export const addCommand: AddCommand = ({ log }, cli) =>
+  cli
+    .command('start', { isDefault: true })
+    .description('start the Mesh Serve using the config to decide what to do (default)')
+    .action(async function start() {
+      const opts = this.optsWithGlobals<CLIGlobals>();
+
+      const loadedConfig = await loadConfig({ log, configPath: opts.configPath });
+
+      const config = {
+        ...loadedConfig,
+        ...opts,
+      };
+
+      // TODO: decide what to do using the config
+
+      const runtime = createServeRuntime(config);
+
+      await startServerForRuntime(runtime, {
+        ...config,
+        host: config.host!, // defaults are defined in cli.ts
+        port: config.port!, // defaults are defined in cli.ts
+        log,
+      });
+    });
