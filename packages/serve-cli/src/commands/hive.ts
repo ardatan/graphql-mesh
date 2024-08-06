585b156010437e8123c2084d16b509d2b17aabe0
enisdenjo <badurinadenis@gmail.com>
2024-08-06 17:11:07 +0200

    hive command

--- packages/serve-cli/src/commands/hive.ts
+++ packages/serve-cli/src/commands/hive.ts
@@ -1,7 +1,12 @@
+import cluster, { Worker } from 'node:cluster';
 import { Option } from '@commander-js/extra-typings';
-import type { AddCommand } from '../cli.js';
+import { createServeRuntime } from '@graphql-mesh/serve-runtime';
+import { registerTerminateHandler } from '@graphql-mesh/utils';
+import type { AddCommand, CLIGlobals } from '../cli.js';
+import { loadConfig } from '../config.js';
+import { startServerForRuntime } from '../server.js';
 
-export const addCommand: AddCommand = (_ctx, cli) =>
+export const addCommand: AddCommand = ({ log }, cli) =>
   cli
     .command('hive')
     .description(
@@ -16,4 +21,49 @@ export const addCommand: AddCommand = (_ctx, cli) =>
       new Option('--key <key>', 'Hive CDN API key for fetching the supergraph')
         .env('HIVE_CDN_KEY')
         .makeOptionMandatory(),
-    );
+    )
+    .action(async function hive() {
+      const { endpoint, key, ...opts } = this.optsWithGlobals<CLIGlobals>();
+
+      const loadedConfig = await loadConfig({
+        log,
+        configPath: opts.configPath,
+        quiet: !cluster.isPrimary,
+      });
+
+      const config = {
+        logging: log,
+        ...loadedConfig,
+        ...opts,
+        hive: {
+          endpoint,
+          key,
+        },
+      };
+
+      if (cluster.isPrimary && config.fork > 1) {
+        const workers: Worker[] = [];
+        log.info(`Forking ${config.fork} workers`);
+        for (let i = 0; i < config.fork; i++) {
+          workers.push(cluster.fork());
+        }
+        registerTerminateHandler(eventName => {
+          log.info(`Killing workers for ${eventName}`);
+          workers.forEach((w, i) => {
+            w.kill(eventName);
+          });
+        });
+        return;
+      }
+
+      const runtime = createServeRuntime(config);
+
+      log.info(`Serving Hive CDN supergraph from ${endpoint}`);
+
+      await startServerForRuntime(runtime, {
+        ...config,
+        host: config.host!, // defaults are defined in cli.ts
+        port: config.port!, // defaults are defined in cli.ts
+        log,
+      });
+    });
