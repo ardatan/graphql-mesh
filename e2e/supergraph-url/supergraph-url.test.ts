87a25caaaa8a428a4843d981e9d94252202af472
enisdenjo <badurinadenis@gmail.com>
2024-08-12 16:57:44 +0200

    test supergraph from url

---
+++ e2e/supergraph-url/supergraph-url.test.ts
@@ -0,0 +1,34 @@
+import { getIntrospectionQuery } from 'graphql';
+import { createTenv } from '@e2e/tenv';
+
+const { serve, service } = createTenv(__dirname);
+
+it('should serve a schema from a url without pathname', async () => {
+  const cdn = await service('cdn');
+
+  const { execute } = await serve({
+    supergraph: `http://0.0.0.0:${cdn.port}`,
+  });
+
+  await expect(execute({ query: getIntrospectionQuery() })).resolves.toMatchSnapshot();
+});
+
+it('should serve a schema from a url with pathname', async () => {
+  const cdn = await service('cdn');
+
+  const { execute } = await serve({
+    supergraph: `http://0.0.0.0:${cdn.port}/schema`,
+  });
+
+  await expect(execute({ query: getIntrospectionQuery() })).resolves.toMatchSnapshot();
+});
+
+it('should serve a schema from a url with pathname and extension', async () => {
+  const cdn = await service('cdn');
+
+  const { execute } = await serve({
+    supergraph: `http://0.0.0.0:${cdn.port}/schema.graphql`,
+  });
+
+  await expect(execute({ query: getIntrospectionQuery() })).resolves.toMatchSnapshot();
+});
