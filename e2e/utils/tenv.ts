fac8a8298efdfd084ac61cb2f27fca843fb38909
YassinEldeeb <yassineldeeb94@gmail.com>
2024-08-21 16:06:40 +0100

    fix

--- e2e/utils/tenv.ts
+++ e2e/utils/tenv.ts
@@ -345,6 +345,8 @@ export function createTenv(cwd: string): Tenv {
         [proc, waitForExit] = await spawn(
           { env, cwd, pipeLogs },
           path.resolve(__project, 'packages', 'serve-cli', 'mesh-serve'),
+          'supergraph',
+          supergraph,
           createPortOpt(port),
           supergraph && `supergraph ${supergraph}`,
         );
