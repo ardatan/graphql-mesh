42059a232b30d3670fda7e085c49707740e23a50
enisdenjo <denis@denelop.com>
2024-08-26 18:07:54 +0200

    win build to dest

--- packages/serve-cli/scripts/package-binary.ts
+++ packages/serve-cli/scripts/package-binary.ts
@@ -26,7 +26,7 @@ await fs.copyFile(process.execPath, dest);
 
 console.log('Injecting blob');
 if (isWindows) {
-  await $`npx postject hello.exe NODE_SEA_BLOB sea-prep.blob ^
+  await $`npx postject ${dest} NODE_SEA_BLOB sea-prep.blob ^
     --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2`;
 } else if (isDarwin) {
   await $`npx postject ${dest} NODE_SEA_BLOB sea-prep.blob \
