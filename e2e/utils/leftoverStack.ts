e3bf1fce3dfa034dc09b266d6280b631c8380467
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-08-30 18:42:03 +0300

    Start Docker on macOS

--- e2e/utils/leftoverStack.ts
+++ e2e/utils/leftoverStack.ts
@@ -2,8 +2,12 @@ import { AsyncDisposableStack } from '@whatwg-node/disposablestack';
 
 export let leftoverStack = new AsyncDisposableStack();
 
-afterAll(() =>
-  leftoverStack.disposeAsync().finally(() => {
-    leftoverStack = new AsyncDisposableStack();
-  }),
-);
+afterAll(() => {
+  const disposeRes$ = leftoverStack.disposeAsync();
+  if (disposeRes$.finally) {
+    return disposeRes$.finally(() => {
+      leftoverStack = new AsyncDisposableStack();
+    });
+  }
+  leftoverStack = new AsyncDisposableStack();
+});
