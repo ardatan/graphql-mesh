e2ce656cd1556336809cc11d29080196bfda1f63
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-08-30 17:14:31 +0300

    Fix lockfile

--- packages/serve-cli/scripts/install-sea-packed-deps.cjs
+++ packages/serve-cli/scripts/install-sea-packed-deps.cjs
@@ -5,39 +5,41 @@
 
 // @ts-check
 
-const isDebug = ['1', 'y', 'yes', 't', 'true'].includes(String(process.env.DEBUG));
-const shouldCleanPackedDeps = ['1', 'y', 'yes', 't', 'true'].includes(
-  String(process.env.SEA_CLEAN_PACKED_DEPS),
-);
 
-/**
- * Will log only when DEBUG env is set to a truthy value.
- *
- * @param {string} msg
- * @param  {...unknown} args
- */
-function debug(msg, ...args) {
-  if (isDebug) {
-    console.debug(`[${new Date().toISOString()}] SEA ${msg}`, ...args);
-  }
-}
-
-// NOTE that the path is stable for modules hash and system,
-// we should NEVER install modules in multiple places to avoid
-// spamming user's devices
-globalThis.__PACKED_DEPS_PATH__ = `${require('node:os').tmpdir()}${require('node:path').sep}mesh-serve_${
-  // @ts-expect-error INJECTED DURING BUNDLE (check rollup.binary.config.js)
-  __MODULES_HASH__
-}_node_modules`;
 
 /** Intentionally IIFE, should run immediately on CLI boot. */
 (function installSeaPackedDeps() {
+  const shouldCleanPackedDeps = ['1', 'y', 'yes', 't', 'true'].includes(
+    String(process.env.SEA_CLEAN_PACKED_DEPS),
+  );
+  const isDebug = ['1', 'y', 'yes', 't', 'true'].includes(String(process.env.DEBUG));
+  /**
+   * Will log only when DEBUG env is set to a truthy value.
+   *
+   * @param {string} msg
+   * @param  {...unknown} args
+   */
+  function debug(msg, ...args) {
+    if (isDebug) {
+      console.debug(`[${new Date().toISOString()}] SEA ${msg}`, ...args);
+    }
+  }
+
   const ADMZip = require('adm-zip'); // THIS IS BUNDLED AND INJECTED
 
   const fs = require('node:fs');
   const Module = require('node:module');
   const path = require('node:path');
   const sea = require('node:sea');
+  const os = require('node:os');
+
+  // NOTE that the path is stable for modules hash and system,
+  // we should NEVER install modules in multiple places to avoid
+  // spamming user's devices
+  globalThis.__PACKED_DEPS_PATH__ = `${os.tmpdir()}${path.sep}mesh-serve_${
+    // @ts-expect-error INJECTED DURING BUNDLE (check rollup.binary.config.js)
+    __MODULES_HASH__
+  }_node_modules`;
 
   const modulesPath = globalThis.__PACKED_DEPS_PATH__;
   let packedDepsInstalled = fs.existsSync(modulesPath);
@@ -68,7 +70,10 @@ globalThis.__PACKED_DEPS_PATH__ = `${require('node:os').tmpdir()}${require('node
       // always try to import from necessary modules first
       return originalResolveFilename(path.join(modulesPath, id), ...rest);
     } catch (e) {
-      debug(`Failed to resolve packed dependency "${id}"; Falling back to the original resolver...`, e);
+      debug(
+        `Failed to resolve packed dependency "${id}"; Falling back to the original resolver...`,
+        e,
+      );
       // fall back to the original resolver
       return originalResolveFilename(...args);
     }
