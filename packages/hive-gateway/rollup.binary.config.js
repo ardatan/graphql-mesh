a612e35b747c4a562024ddf9423934f78d526423
enisdenjo <denis@denelop.com>
2024-09-03 17:10:30 +0200

    use .test with regex

--- packages/hive-gateway/rollup.binary.config.js
+++ packages/hive-gateway/rollup.binary.config.js
@@ -143,7 +143,7 @@ return module.exports;
 
       // replace the @graphql-mesh/include/hooks register to use the absolute path of the packed deps
       const includeHooksRegisterDest = /register\(\s*'@graphql-mesh\/include\/hooks'/; // intentionally no closing bracked because there's more arguments
-      if (code.includes(includeHooksRegisterDest)) {
+      if (includeHooksRegisterDest.test(code)) {
         code = code.replaceAll(
           includeHooksRegisterDest,
           `register(require('node:path').join(globalThis.__PACKED_DEPS_PATH__, ${JSON.stringify(
