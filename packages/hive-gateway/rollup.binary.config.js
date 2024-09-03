8d5298cf0c88cb175aa5321c8951f53e8d65e8ec
Denis Badurina <badurinadenis@gmail.com>
2024-09-03 17:51:20 +0200

    Bundle custom import hooks and use path instead of injecting code (#7622)

--- packages/hive-gateway/rollup.binary.config.js
+++ packages/hive-gateway/rollup.binary.config.js
@@ -141,6 +141,19 @@ return module.exports;
         throw new Error('Bundle does not have any dynamic commonjs requires');
       }
 
+      // replace the @graphql-mesh/include/hooks register to use the absolute path of the packed deps
+      const includeHooksRegisterDest = /register\(\s*'@graphql-mesh\/include\/hooks'/g; // intentionally no closing bracked because there's more arguments
+      if (includeHooksRegisterDest.test(code)) {
+        code = code.replaceAll(
+          includeHooksRegisterDest,
+          `register(require('node:path').join(globalThis.__PACKED_DEPS_PATH__, '@graphql-mesh', 'include', 'hooks.mjs')`,
+        );
+      } else {
+        throw new Error(
+          `Include hooks path cannot be fixed, does "${includeHooksRegisterDest}" exist in the source code?`,
+        );
+      }
+
       return code;
     },
     async generateBundle() {
