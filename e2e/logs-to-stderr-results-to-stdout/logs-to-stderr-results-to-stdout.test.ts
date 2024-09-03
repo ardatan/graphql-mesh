ef6d0c8405764b73fd8f0c6db020d967ce1cddd7
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-09-03 16:37:00 +0300

    Fix E2E tests

--- e2e/logs-to-stderr-results-to-stdout/logs-to-stderr-results-to-stdout.test.ts
+++ e2e/logs-to-stderr-results-to-stdout/logs-to-stderr-results-to-stdout.test.ts
@@ -6,7 +6,7 @@ it('should write serve logs to stderr', async () => {
   await using serveInstance = await serve({
     supergraph: await fs.tempfile('supergraph.graphql', 'type Query { hello: String }'),
   });
-  expect(serveInstance.getStd('err')).toContain('Starting server on');
+  expect(serveInstance.getStd('err')).toContain('Listening on');
 });
 
 it('should write compose output to stdout and logs to stderr', async () => {
