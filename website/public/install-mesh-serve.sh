762a655170569b1239a3f4770f436767fb1d7847
Yassin Eldeeb <yassineldeeb94@gmail.com>
2024-09-09 21:37:14 -0700

    fix: install.sh script (#7649)
    * fix: install.sh script
    
    * chore: format

--- website/public/install-mesh-serve.sh
+++ website/public/install-mesh-serve.sh
@@ -25,52 +25,31 @@ fetch_and_prepare_binary() {
   check_non_empty "$architecture" "architecture"
 
   RELEASE_TAG="v$TARGET_VERSION"
-  RELEASE_API_URL="https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases/tags/$RELEASE_TAG"
 
-  echo "Fetching release details from: $RELEASE_API_URL"
+  DOWNLOAD_URL="https://github.com/$GITHUB_OWNER/$GITHUB_REPO/releases/download/$RELEASE_TAG/$BINARY_NAME-${architecture}.gz"
 
-  API_RESPONSE=$(curl -s "$RELEASE_API_URL")
+  destination_file="./$BINARY_NAME-${architecture}.gz"
+  echo "Downloading $BINARY_NAME from $DOWNLOAD_URL ..."
+  curl -sSfL "$DOWNLOAD_URL" -o "$destination_file"
 
-  # Validate JSON response
-  if ! echo "$API_RESPONSE" | jq . > /dev/null 2>&1; then
-    echo "Error parsing release information from GitHub API."
+  if [ $? -ne 0 ]; then
+    echo "Download failed: $DOWNLOAD_URL"
     exit 1
   fi
 
-  # Ensure assets are available
-  TOTAL_ASSETS=$(echo "$API_RESPONSE" | jq '.assets | length')
-  if [ "$TOTAL_ASSETS" -eq 0 ]; then
-    echo "No assets found for release $RELEASE_TAG. Exiting."
+  echo "Unzipping $destination_file..."
+  gunzip "$destination_file"
+
+  if [ $? -ne 0 ]; then
+    echo "Unzipping failed: $destination_file"
     exit 1
   fi
 
-  DOWNLOAD_URL=$(echo "$API_RESPONSE" | jq -r ".assets[] | select(.name == \"$BINARY_NAME-${architecture}\") | .browser_download_url")
+  binary_path="./$BINARY_NAME"
 
-  if [ -n "$DOWNLOAD_URL" ]; then
-    destination_file="./$BINARY_NAME-${architecture}.gz"
-    echo "Downloading $BINARY_NAME from $DOWNLOAD_URL ..."
-    curl -sSfL "$DOWNLOAD_URL" -o "$destination_file"
-
-    if [ $? -ne 0 ]; then
-      echo "Download failed: $DOWNLOAD_URL"
-      exit 1
-    fi
-
-    echo "Unzipping $destination_file..."
-    unzip "$destination_file"
-
-    if [ $? -ne 0 ]; then
-      echo "Unzipping failed: $destination_file"
-      exit 1
-    fi
-
-    binary_path="./$BINARY_NAME"
-    chmod +x "$binary_path"
-    echo "Binary downloaded and ready to use at $binary_path."
-  else
-    echo "No suitable binary found for architecture $architecture in release $RELEASE_TAG."
-    exit 1
-  fi
+  mv "$BINARY_NAME-${architecture}" "$BINARY_NAME"
+  chmod +x "$BINARY_NAME"
+  echo "Binary downloaded and ready to use at $binary_path."
 }
 
 identify_architecture() {
