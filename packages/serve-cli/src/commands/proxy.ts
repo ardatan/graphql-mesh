31a34ea7293db99dd5b83c83bfdc5b4deb7b9cb3
Mathias Klippinge <mathias.klippinge@gmail.com>
2024-10-16 09:52:38 +0200

    Provide logger to fix typecheck issues

--- packages/serve-cli/src/commands/proxy.ts
+++ packages/serve-cli/src/commands/proxy.ts
@@ -90,7 +90,10 @@ export const addCommand: AddCommand = (ctx, cli) =>
         pubsub,
         logger: ctx.log,
       });
-      const builtinPlugins = await getBuiltinPluginsFromConfig(loadedConfig, { cache });
+      const builtinPlugins = await getBuiltinPluginsFromConfig(loadedConfig, {
+        cache,
+        logger: ctx.log,
+      });
 
       const config: ProxyConfig = {
         ...defaultOptions,
