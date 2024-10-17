148850e9f3a3ce12431e35111d6d2512dd25c5ff
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-10-17 16:32:15 +0300

    Relax CPU

--- packages/serve-cli/rollup.config.js
+++ packages/serve-cli/rollup.config.js
@@ -44,6 +44,9 @@ const deps = {
   'node_modules/@graphql-mesh/transport-common/index': '../transports/common/src/index.ts',
   'node_modules/@graphql-mesh/transport-http/index': '../transports/http/src/index.ts',
   // extras for docker only
+  'node_modules/@graphql-mesh/transport-ws/index': '../transports/ws/src/index.ts',
+  'node_modules/@graphql-mesh/transport-http-callback/index':
+    '../transports/http-callback/src/index.ts',
   'node_modules/@graphql-mesh/plugin-http-cache/index': '../plugins/http-cache/src/index.ts',
   'node_modules/@graphql-mesh/hmac-upstream-signature/index':
     '../plugins/hmac-upstream-signature/src/index.ts',
@@ -56,9 +59,6 @@ if (process.env.E2E_SERVE_RUNNER === 'docker') {
   deps['node_modules/@e2e/opts/index'] = '../../e2e/utils/opts.ts';
   deps['node_modules/@graphql-mesh/utils/index'] = '../legacy/utils/src/index.ts';
   deps['node_modules/@omnigraph/openapi/index'] = '../loaders/openapi/src/index.ts';
-  deps['node_modules/@graphql-mesh/transport-ws/index'] = '../transports/ws/src/index.ts';
-  deps['node_modules/@graphql-mesh/transport-http-callback/index'] =
-    '../transports/http-callback/src/index.ts';
   deps['node_modules/@graphql-mesh/transport-rest/index'] = '../transports/rest/src/index.ts';
   deps['node_modules/@graphql-mesh/transport-soap/index'] = '../transports/soap/src/index.ts';
   deps['node_modules/@graphql-mesh/transport-mysql/index'] = '../transports/mysql/src/index.ts';
--- packages/serve-cli/src/cli.ts
+++ packages/serve-cli/src/cli.ts
@@ -172,8 +172,9 @@ export type AddCommand = (ctx: CLIContext, cli: CLI) => void;
 
 // we dont use `Option.default()` in the command definitions because we want the CLI options to
 // override the config file (with option defaults, config file will always be overwritten)
+const maxAvailableFork = Math.max(availableParallelism() - 1, 1);
 export const defaultOptions = {
-  fork: process.env.NODE_ENV === 'production' ? availableParallelism() : 1,
+  fork: process.env.NODE_ENV === 'production' ? maxAvailableFork : 1,
   host:
     platform().toLowerCase() === 'win32' ||
     // is WSL?
@@ -200,6 +201,9 @@ let cli = new Command()
         const count = parseInt(v);
         if (isNaN(count)) {
           throw new InvalidArgumentError('not a number.');
+        }
+        if (count > maxAvailableFork) {
+          return maxAvailableFork;
         }
         return count;
       }),
