ed062780f70e49c94981f52776be922de0430be0
enisdenjo <denis@denelop.com>
2024-11-20 18:18:25 +0100

    logs to stderr only relevent to compose

--- e2e/logs-to-stderr-results-to-stdout/logs-to-stderr-results-to-stdout.test.ts
+++ e2e/logs-to-stderr-results-to-stdout/logs-to-stderr-results-to-stdout.test.ts
@@ -1,16 +1,9 @@
 import { createTenv } from '@e2e/tenv';
 
-const { serve, compose, fs } = createTenv(__dirname);
-
-it('should write serve logs to stderr', async () => {
-  await using serveInstance = await serve({
-    supergraph: await fs.tempfile('supergraph.graphql', 'type Query { hello: String }'),
-  });
-  // stdout from serve because it uses the default logger that uses console.log
-  expect(serveInstance.getStd('out')).toContain('Serving local supergraph from');
-});
+const { compose } = createTenv(__dirname);
 
 it('should write compose output to stdout and logs to stderr', async () => {
   const { getStd } = await compose();
+  expect(getStd('out')).toContain('type Query @join__type(graph: HELLOWORLD)');
   expect(getStd('err')).toContain('Done!');
 });
