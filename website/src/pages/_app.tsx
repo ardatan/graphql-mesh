72706dcca79d8633a12a380117d1b512e8ac44e7
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-11-25 14:17:42 +0100

    hotfix: specify the --font-sans in a .css file (#7983)

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react';
 import { AppProps } from 'next/app';
 import '@theguild/components/style.css';
+import '../hotfix.css';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
