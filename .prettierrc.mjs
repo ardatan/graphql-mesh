033a5989658c89ba8ed9aca8e37569fa82d7591d
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-09 18:28:34 +0100

    Update dependency versions in package.json and yarn.lock

--- .prettierrc.mjs
+++
@@ -1,7 +0,0 @@
-import guildConfig from '@theguild/prettier-config';
-
-export default {
-  ...guildConfig,
-  overrides: [{ files: '*.json', options: { trailingComma: 'none' } }, ...guildConfig.overrides],
-  importOrderParserPlugins: ['explicitResourceManagement', ...guildConfig.importOrderParserPlugins],
-};
