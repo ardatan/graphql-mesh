9537a2347f423e49ecb16e3764831c1b5f054ee5
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-19 14:22:50 +0100

    Add InfoCardsSection

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -1,6 +1,18 @@
+import type { Config } from 'tailwindcss';
 import guildConfig from '@theguild/tailwind-config';
 
-export default {
+const config: Config = {
   ...guildConfig,
   content: [...guildConfig.content, 'theme.config.tsx'],
+  theme: {
+    ...guildConfig.theme,
+    extend: {
+      colors: {
+        ...guildConfig.theme.extend.colors,
+        primary: guildConfig.theme.extend.colors['hive-yellow'],
+      },
+    },
+  },
 };
+
+export default config;
