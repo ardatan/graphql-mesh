758429c6cb99be0d9df2463ca1716da32365c3cc
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-12-23 18:13:49 +0100

    Update website/src/components/docs-mdx-components.tsx
    Co-authored-by: Dimitri POSTOLOV <dmytropostolov@gmail.com>

--- website/src/components/docs-mdx-components.tsx
+++ website/src/components/docs-mdx-components.tsx
@@ -7,7 +7,7 @@ export const Link: typeof Anchor = ({ className, ...props }) => {
     <Anchor
       // we remove `text-underline-position` from default Nextra link styles
       className={cn(
-        'hive-focus dark:text-primary/90 dark:focus-visible:ring-primary/50 -mx-1 -my-0.5 rounded px-1 py-0.5 font-medium text-blue-700 underline underline-offset-[2px] hover:no-underline focus-visible:no-underline focus-visible:ring-current focus-visible:ring-offset-blue-200',
+        'hive-focus dark:text-primary/90 dark:focus-visible:ring-primary/50 -mx-1 -my-0.5 rounded px-1 py-0.5 font-medium text-blue-700 underline underline-offset-2 hover:no-underline focus-visible:no-underline focus-visible:ring-current focus-visible:ring-offset-blue-200',
         className,
       )}
       {...props}
