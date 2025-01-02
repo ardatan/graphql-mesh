0839e41deb54e95aa6c7ef4355a2ed411ff42390
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-01-02 14:09:02 +0200

    Use whole width of the screen in Comparison Table on mobile

--- website/src/components/comparison-table/index.tsx
+++ website/src/components/comparison-table/index.tsx
@@ -94,16 +94,16 @@ export function ComparisonTable({ children, className, ...rest }: ComparisonTabl
       className={cn('py-6 sm:py-12 md:px-6 xl:px-[120px] lg:py-[120px]', className)}
       {...rest}
     >
-      <header className="text-center text-balance">
+      <header className="sm:text-center sm:text-balance">
         <Heading as="h3" size="md">
           Consider switching from other tools?
         </Heading>
-        <p className="mt-4 mb-8 sm:mb-16 text-green-800 text-balance">
+        <p className="mt-4 mb-8 sm:mb-16 text-green-800">
           Learn the differences between Mesh, Apollo Federation, Hasura and GraphQL Tools
         </p>
       </header>
-      <div className="overflow-x-scroll max-sm:p-4 max-sm:-my-4">
-        <Table className="max-sm:text-sm sm:table-fixed">
+      <div className="overflow-x-scroll max-sm:p-4 max-sm:-my-4 max-sm:-mx-8">
+        <Table className="max-sm:text-sm sm:table-fixed max-sm:rounded-none">
           <thead>
             <TableRow>
               {headers.map(header => (
