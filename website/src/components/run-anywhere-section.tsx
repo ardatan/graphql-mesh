6a2bb117ea87524edc4afb9298033d20e2f09d9f
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-01-10 23:13:26 +0100

    Highlight some sections in Run Anywhere

--- website/src/components/run-anywhere-section.tsx
+++ website/src/components/run-anywhere-section.tsx
@@ -2,12 +2,13 @@ import { Anchor, ArrowIcon, cn, Heading } from '@theguild/components';
 
 export interface RunAnywhereSectionProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export function RunAnywhereSection({ className, ...props }: RunAnywhereSectionProps) {
+export function RunAnywhereSection(props: RunAnywhereSectionProps) {
   return (
     <section
+      {...props}
       className={cn(
         'relative rounded-3xl bg-blue-400 text-green-1000 p-4 pt-6 sm:py-24 md:px-6 lg:px-12 2xl:px-24 md:py-[120px]',
-        className,
+        props.className,
       )}
     >
       <Heading as="h3" size="md" className="text-center">
@@ -20,25 +21,43 @@ export function RunAnywhereSection({ className, ...props }: RunAnywhereSectionPr
       </p>
 
       <ul className="flex mt-4 sm:mt-8 lg:mt-16 flex-wrap gap-2 sm:gap-4 max-sm:grid max-sm:grid-cols-3 justify-center items-center max-w-[1200px] mx-auto">
-        <ListItemAnchor href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/runtimes/nodejs">
+        <ListItemAnchor
+          href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/runtimes/nodejs"
+          highlighted
+        >
           Node.js
         </ListItemAnchor>
         <ListItemAnchor href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/runtimes/bun">
           Bun
         </ListItemAnchor>
-        <ListItemAnchor href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/docker">
+        <ListItemAnchor
+          href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/docker"
+          highlighted
+        >
           Docker
         </ListItemAnchor>
-        <ListItemAnchor href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/serverless/cloudflare-workers">
+        <ListItemAnchor
+          href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/serverless/cloudflare-workers"
+          highlighted
+        >
           Cloudflare Workers
         </ListItemAnchor>
-        <ListItemAnchor href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/serverless/aws-lambda">
+        <ListItemAnchor
+          href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/serverless/aws-lambda"
+          highlighted
+        >
           AWS Lambda
         </ListItemAnchor>
-        <ListItemAnchor href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/serverless/google-cloud-platform">
+        <ListItemAnchor
+          href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/serverless/google-cloud-platform"
+          highlighted
+        >
           Google Cloud Platform
         </ListItemAnchor>
-        <ListItemAnchor href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/serverless/azure-functions">
+        <ListItemAnchor
+          href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/serverless/azure-functions"
+          highlighted
+        >
           Azure Functions
         </ListItemAnchor>
         <ListItemAnchor href="https://the-guild.dev/graphql/hive/docs/gateway/deployment/node-frameworks/express">
@@ -74,15 +93,20 @@ function ListItemAnchor({
   children,
   href,
   className,
+  highlighted,
 }: {
   children: React.ReactNode;
   href: string;
   className?: string;
+  highlighted?: boolean;
 }) {
   return (
     <li className={className}>
       <Anchor
-        className="hive-focus flex items-center gap-4 bg-blue-200 hover:bg-blue-100 rounded-2xl px-4 sm:px-6 md:px-8 h-[102px]"
+        className={cn(
+          'hive-focus flex items-center gap-4 bg-blue-200 hover:bg-white rounded-2xl px-4 sm:px-6 md:px-8 h-[102px] hover:[&:not(:focus)]:ring-blue-300 hover:ring',
+          highlighted && 'bg-blue-100',
+        )}
         href={href}
       >
         {children}
