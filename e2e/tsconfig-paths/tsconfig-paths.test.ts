33701a1b3bda167e15d1d997926611ac1b86ae3a
Denis Badurina <denis@denelop.com>
2025-01-16 15:27:26 +0100

    use HIVE_IMPORTER_TSCONFIG_SEARCH_PATH

--- e2e/tsconfig-paths/tsconfig-paths.test.ts
+++ e2e/tsconfig-paths/tsconfig-paths.test.ts
@@ -5,7 +5,7 @@ const { compose } = createTenv(__dirname);
 it('should compose', async () => {
   const proc = await compose({
     env: {
-      MESH_INCLUDE_TSCONFIG_SEARCH_PATH: 'tsconfig-paths.tsconfig.json',
+      HIVE_IMPORTER_TSCONFIG_SEARCH_PATH: 'tsconfig-paths.tsconfig.json',
     },
   });
   expect(proc.result).toMatchSnapshot();
