0c18fe12b1d92af006e945b19e9207cb16a1ee7e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-08-29 14:50:54 +0300

    Support Type Merging within additional type defs with subscriptions (#8733)

---
+++ e2e/subscriptions-type-merging/gateway.config.ts
@@ -0,0 +1,31 @@
+import { Redis } from 'ioredis';
+import { defineConfig as defineGatewayConfig } from '@graphql-hive/gateway';
+import { RedisPubSub } from '@graphql-hive/pubsub/redis';
+import { toMeshPubSub } from '@graphql-mesh/types';
+
+/**
+ * When a Redis connection enters "subscriber mode" (after calling SUBSCRIBE), it can only execute
+ * subscriber commands (SUBSCRIBE, UNSUBSCRIBE, etc.). Meaning, it cannot execute other commands like PUBLISH.
+ * To avoid this, we use two separate Redis clients: one for publishing and one for subscribing.
+ */
+const pub = new Redis({
+  host: process.env['REDIS_HOST'],
+  port: parseInt(process.env['REDIS_PORT']!),
+  autoResubscribe: true,
+});
+const sub = new Redis({
+  host: process.env['REDIS_HOST'],
+  port: parseInt(process.env['REDIS_PORT']!),
+  autoResubscribe: true,
+});
+
+export const gatewayConfig = defineGatewayConfig({
+  pubsub: toMeshPubSub(
+    new RedisPubSub(
+      { pub, sub },
+      {
+        channelPrefix: 'gw',
+      },
+    ),
+  ),
+});
---
+++ e2e/subscriptions-type-merging/subscriptions-type-merging.test.ts
@@ -0,0 +1,86 @@
+import { setTimeout } from 'node:timers/promises';
+import { createClient } from 'graphql-sse';
+import Redis from 'ioredis';
+import { createTenv } from '@e2e/tenv';
+import { createDeferred } from '@graphql-tools/utils';
+import { AsyncDisposableStack } from '@whatwg-node/disposablestack';
+import { fetch } from '@whatwg-node/fetch';
+
+const { container, compose, service, serve } = createTenv(__dirname);
+
+const redisEnv = {
+  REDIS_HOST: '',
+  REDIS_PORT: 0,
+};
+beforeAll(async () => {
+  const redis = await container({
+    name: 'redis',
+    image: 'redis:8',
+    containerPort: 6379,
+    healthcheck: ['CMD-SHELL', 'redis-cli ping'],
+    env: {
+      LANG: '', // fixes "Failed to configure LOCALE for invalid locale name."
+    },
+  });
+  redisEnv.REDIS_HOST = '0.0.0.0';
+  redisEnv.REDIS_PORT = redis.port;
+});
+
+const leftoverStack = new AsyncDisposableStack();
+afterAll(async () => {
+  try {
+    await leftoverStack.disposeAsync();
+  } catch (e) {
+    console.error('Failed to dispose leftover stack', e);
+  }
+});
+
+it('consumes the pubsub topics and resolves the types correctly', async () => {
+  await using products = await service('products');
+  await using composition = await compose({ output: 'graphql', services: [products] });
+  await using gw = await serve({ supergraph: composition.output, env: redisEnv });
+  const sseClient = createClient({
+    retryAttempts: 0,
+    url: `http://0.0.0.0:${gw.port}/graphql`,
+    fetchFn: fetch,
+  });
+  const iterator = sseClient.iterate({
+    query: /* GraphQL */ `
+      subscription {
+        newProduct {
+          id
+          name
+          price
+        }
+      }
+    `,
+  });
+  leftoverStack.defer(() => iterator.return?.() as any);
+  const pub = new Redis({
+    host: redisEnv.REDIS_HOST,
+    port: redisEnv.REDIS_PORT,
+  });
+  leftoverStack.defer(() => pub.disconnect());
+  for (let i = 0; i < 3; i++) {
+    const id = i + '';
+    const publishing = (async () => {
+      // Publish messages after making sure the user's subscribed
+      await setTimeout(500);
+      await pub.publish('gw:new_product', JSON.stringify({ id }));
+    })();
+    await expect(iterator.next()).resolves.toMatchObject({
+      value: {
+        data: {
+          newProduct: {
+            id,
+            name: 'Roomba X' + id,
+            price: 100,
+          },
+        },
+      },
+      done: false,
+    });
+    // Avait publishing to ensure no unhandled rejections
+    await publishing;
+  }
+});
