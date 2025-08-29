87360c6d2d712b28d2f7882ba9954fcab46e8a60
Denis Badurina <denis@denelop.com>
2025-08-29 20:17:28 +0200

    skip test

--- packages/legacy/utils/test/resolve-additional-resolvers.spec.ts
+++ packages/legacy/utils/test/resolve-additional-resolvers.spec.ts
@@ -6,7 +6,7 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { stitchSchemas, ValidationLevel } from '@graphql-tools/stitch';
 import { assertAsyncIterable } from '../../testing/utils';
 
-it('should resolve missing fields from subgraphs on subscription event', async () => {
+it('should resolve fields from subgraphs on subscription event', async () => {
   const products = makeExecutableSchema({
     typeDefs: parse(/* GraphQL */ `
       type Query {
@@ -98,4 +98,104 @@ it('should resolve missing fields from subgraphs on subscription event', async (
   },
 }
 `);
+});
+
+// TODO: we skip because this test will fail, we need to optimize the resolver to account for available fields
+it.skip('should resolve only missing fields from subgraphs on subscription event', async () => {
+  const productPriceResolver = jest.fn(() => 100);
+  const products = makeExecutableSchema({
+    typeDefs: parse(/* GraphQL */ `
+      type Query {
+        productById(id: ID!): Product
+      }
+      type Product {
+        id: ID!
+        name: String!
+        price: Float!
+      }
+    `),
+    resolvers: {
+      Query: {
+        productById: (_, { id }) => ({
+          id,
+          name: `Roomba X${id}`,
+        }),
+      },
+      Product: {
+        price: productPriceResolver,
+      },
+    },
+  });
+
+  await using pubsub = new MemPubSub();
+  const additionalTypeDefs = parse(/* GraphQL */ `
+    extend schema {
+      subscription: Subscription
+    }
+    type Subscription {
+      newProduct: Product!
+    }
+  `);
+  const additionalResolvers = resolveAdditionalResolversWithoutImport(
+    {
+      targetTypeName: 'Subscription',
+      targetFieldName: 'newProduct',
+      pubsubTopic: 'new_product',
+    },
+    pubsub,
+  );
+
+  const stitched = stitchSchemas({
+    subschemas: [
+      {
+        schema: products,
+        merge: {
+          Product: {
+            selectionSet: '{ id }',
+            fieldName: 'productById',
+            args: ({ id }) => ({ id }),
+          },
+        },
+      },
+    ],
+    typeDefs: additionalTypeDefs,
+    resolvers: additionalResolvers,
+  });
+
+  const result = await subscribe({
+    schema: stitched,
+    document: parse(/* GraphQL */ `
+      subscription {
+        newProduct {
+          name
+          ...P
+        }
+      }
+      fragment P on Product {
+        price
+      }
+    `),
+  });
+  assertAsyncIterable(result);
+  const iter = result[Symbol.asyncIterator]();
+
+  setTimeout(() => {
+    pubsub.publish('new_product', { id: '60', price: 999 });
+  }, 0);
+
+  await expect(iter.next()).resolves.toMatchInlineSnapshot(`
+{
+  "done": false,
+  "value": {
+    "data": {
+      "newProduct": {
+        "name": "Roomba X60",
+        "price": 999,
+      },
+    },
+  },
+}
+`);
+
+  expect(productPriceResolver).not.toHaveBeenCalled();
 });
