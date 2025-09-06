ffec6e7ae007b7019a895d4dbee79177a4a0a13e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-06 08:51:23 -0400

    Export PubSub operation factory (#8747)
    * Export PubSub operation factory
    
    * uPDATE SNAPSHOT

---
+++ .changeset/tangy-otters-search.md
@@ -0,0 +1,6 @@
+---
+'@graphql-mesh/transport-rest': patch
+'@graphql-mesh/utils': patch
+---
+
+Export PubSub resolver factory
--- packages/transports/rest/src/directives/pubsubOperation.ts
+++ packages/transports/rest/src/directives/pubsubOperation.ts
@@ -1,7 +1,7 @@
 import type { GraphQLField } from 'graphql';
 import { stringInterpolator } from '@graphql-mesh/string-interpolation';
 import { toMeshPubSub, type HivePubSub, type Logger, type MeshPubSub } from '@graphql-mesh/types';
-import { createGraphQLError } from '@graphql-tools/utils';
+import { getResolverForPubSubOperation } from '@graphql-mesh/utils';
 
 interface ProcessPubSubOperationAnnotationsOpts {
   field: GraphQLField<any, any>;
@@ -16,30 +16,11 @@ export function processPubSubOperationAnnotations({
   pubsubTopic,
   logger: globalLogger,
 }: ProcessPubSubOperationAnnotationsOpts) {
-  field.subscribe = function pubSubSubscribeFn(root, args, context, info) {
-    const logger = context?.logger || globalLogger;
-    const operationLogger = logger.child({ operation: `${info.parentType.name}.${field.name}` });
-    const meshOrHivePubsub: MeshPubSub | HivePubSub = context?.pubsub || globalPubsub;
-    if (!meshOrHivePubsub) {
-      return new TypeError(`You should have PubSub defined in either the config or the context!`);
-    }
-    const pubsub = toMeshPubSub(meshOrHivePubsub);
-    const interpolationData = { root, args, context, info, env: process.env };
-    let interpolatedPubSubTopic: string = stringInterpolator.parse(pubsubTopic, interpolationData);
-    if (interpolatedPubSubTopic.startsWith('webhook:')) {
-      const [, expectedMethod, expectedUrl] = interpolatedPubSubTopic.split(':');
-      const expectedPath = new URL(expectedUrl, 'http://localhost').pathname;
-      interpolatedPubSubTopic = `webhook:${expectedMethod}:${expectedPath}`;
-    }
-    operationLogger.debug(
-      `${info.parentType.name}.${field.name} => Subscribing to pubSubTopic: ${interpolatedPubSubTopic}`,
-    );
-    return pubsub.asyncIterator(interpolatedPubSubTopic);
-  };
-  field.resolve = function pubSubResolver(root, args, context, info) {
-    const logger = context?.logger || globalLogger;
-    const operationLogger = logger.child({ operation: `${info.parentType.name}.${field.name}` });
-    operationLogger.debug('received', { root, pubsubTopic });
-    return root;
-  };
+  const { subscribe, resolve } = getResolverForPubSubOperation({
+    pubsubTopic,
+    pubsub: globalPubsub,
+  });
+  field.subscribe = subscribe;
+  field.resolve = resolve;
+  return field;
 }
