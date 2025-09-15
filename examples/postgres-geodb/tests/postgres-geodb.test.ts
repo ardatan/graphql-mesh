9513c2a64f45c761299bccc869b6e36567cf297e
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-09-15 14:41:38 +0300

    Lets go

--- examples/postgres-geodb/tests/postgres-geodb.test.ts
+++ examples/postgres-geodb/tests/postgres-geodb.test.ts
@@ -8,21 +8,32 @@ jest.setTimeout(30000);
 describe('PostgresGeoDB', () => {
   let config: ProcessedConfig;
   let mesh: MeshInstance;
-  const debugEnvFlag = '1';
   beforeAll(async () => {
     config = await findAndParseConfig({
       dir: join(__dirname, '..'),
     });
     mesh = await getMesh(config);
-    process.env.DEBUG = debugEnvFlag;
   });
   it('should give correct response for example queries', async () => {
     const result = await mesh.execute(config.documents[0].document!);
-    expect(result?.data?.allCities?.nodes?.[0]?.countrycode).toBeTruthy();
-    expect(result?.data?.allCities?.nodes?.[0]?.developers?.[0]?.login).toBeTruthy();
+    expect(result).toEqual({
+      data: {
+        allCities: {
+          nodes: expect.arrayContaining([
+            expect.objectContaining({
+              countrycode: expect.any(String),
+              developers: expect.arrayContaining([
+                expect.objectContaining({
+                  login: expect.any(String),
+                }),
+              ]),
+            }),
+          ]),
+        },
+      },
+    });
   });
   afterAll(() => {
-    process.env.DEBUG = debugEnvFlag;
     mesh?.destroy();
   });
 });
