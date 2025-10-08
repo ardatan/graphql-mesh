9e41519d8108df086da6d60b6b6e9461e2378cb4
Valentin Cocaud <v.cocaud@gmail.com>
2025-10-08 11:37:29 +0200

    fix

--- packages/cache/redis/src/index.ts
+++ packages/cache/redis/src/index.ts
@@ -27,113 +27,116 @@ export default class RedisCache<V = string> implements KeyValueCache<V>, Disposa
   ) {
     this.tracer = trace.getTracer('hive.cache.redis');
     this.tracer.startActiveSpan('hive.cache.redis.init', span => {
-      const lazyConnect = options.lazyConnect !== false;
-      if ('startupNodes' in options) {
-        const parsedUsername =
-          interpolateStrWithEnv(options.username?.toString()) || process.env.REDIS_USERNAME;
-        const parsedPassword =
-          interpolateStrWithEnv(options.password?.toString()) || process.env.REDIS_PASSWORD;
-        const parsedDb = interpolateStrWithEnv(options.db?.toString()) || process.env.REDIS_DB;
-        const numDb = parseInt(parsedDb);
-        this.client = new Redis.Cluster(
-          options.startupNodes.map(s => ({
-            host: s.host && interpolateStrWithEnv(s.host),
-            port: s.port && parseInt(interpolateStrWithEnv(s.port)),
-            family: s.family && parseInt(interpolateStrWithEnv(s.family)),
-          })),
-          {
-            dnsLookup: options.dnsLookupAsIs
-              ? (address, callback) => callback(null, address)
-              : undefined,
-            redisOptions: {
+      try {
+        const lazyConnect = options.lazyConnect !== false;
+        if ('startupNodes' in options) {
+          const parsedUsername =
+            interpolateStrWithEnv(options.username?.toString()) || process.env.REDIS_USERNAME;
+          const parsedPassword =
+            interpolateStrWithEnv(options.password?.toString()) || process.env.REDIS_PASSWORD;
+          const parsedDb = interpolateStrWithEnv(options.db?.toString()) || process.env.REDIS_DB;
+          const numDb = parseInt(parsedDb);
+          this.client = new Redis.Cluster(
+            options.startupNodes.map(s => ({
+              host: s.host && interpolateStrWithEnv(s.host),
+              port: s.port && parseInt(interpolateStrWithEnv(s.port)),
+              family: s.family && parseInt(interpolateStrWithEnv(s.family)),
+            })),
+            {
+              dnsLookup: options.dnsLookupAsIs
+                ? (address, callback) => callback(null, address)
+                : undefined,
+              redisOptions: {
+                username: parsedUsername,
+                password: parsedPassword,
+                db: isNaN(numDb) ? undefined : numDb,
+                enableAutoPipelining: true,
+                ...(lazyConnect ? { lazyConnect: true } : {}),
+                tls: options.tls ? {} : undefined,
+              },
+              enableAutoPipelining: true,
+              enableOfflineQueue: true,
+              ...(lazyConnect ? { lazyConnect: true } : {}),
+            },
+          );
+        } else if ('sentinels' in options) {
+          this.client = new Redis({
+            name: options.name,
+            sentinelPassword:
+              options.sentinelPassword && interpolateStrWithEnv(options.sentinelPassword),
+            sentinels: options.sentinels.map(s => ({
+              host: s.host && interpolateStrWithEnv(s.host),
+              port: s.port && parseInt(interpolateStrWithEnv(s.port)),
+              family: s.family && parseInt(interpolateStrWithEnv(s.family)),
+            })),
+            role: options.role,
+            enableTLSForSentinelMode: options.enableTLSForSentinelMode,
+            enableAutoPipelining: true,
+            enableOfflineQueue: true,
+            lazyConnect,
+          });
+        } else if (options.url) {
+          const redisUrl = new URL(interpolateStrWithEnv(options.url));
+
+          if (!['redis:', 'rediss:'].includes(redisUrl.protocol)) {
+            throw new Error('Redis URL must use either redis:// or rediss://');
+          }
+
+          if (lazyConnect) {
+            redisUrl.searchParams.set('lazyConnect', 'true');
+          }
+
+          redisUrl.searchParams.set('enableAutoPipelining', 'true');
+          redisUrl.searchParams.set('enableOfflineQueue', 'true');
+          const IPV6_REGEX =
+            /^(?:(?:[a-fA-F\d]{1,4}:){7}(?:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){6}(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){5}(?::(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,2}|:)|(?:[a-fA-F\d]{1,4}:){4}(?:(?::[a-fA-F\d]{1,4}){0,1}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,3}|:)|(?:[a-fA-F\d]{1,4}:){3}(?:(?::[a-fA-F\d]{1,4}){0,2}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,4}|:)|(?:[a-fA-F\d]{1,4}:){2}(?:(?::[a-fA-F\d]{1,4}){0,3}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,5}|:)|(?:[a-fA-F\d]{1,4}:){1}(?:(?::[a-fA-F\d]{1,4}){0,4}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,6}|:)|(?::(?:(?::[a-fA-F\d]{1,4}){0,5}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,7}|:)))(?:%[0-9a-zA-Z]{1,})?$/gm;
+          if (IPV6_REGEX.test(redisUrl.hostname)) {
+            redisUrl.searchParams.set('family', '6');
+          }
+          const urlStr = redisUrl.toString();
+          options.logger.debug(`Connecting to Redis at ${urlStr}`);
+          this.client = new Redis(urlStr);
+        } else {
+          const parsedHost =
+            interpolateStrWithEnv(options.host?.toString()) || process.env.REDIS_HOST;
+          const parsedPort =
+            interpolateStrWithEnv(options.port?.toString()) || process.env.REDIS_PORT;
+          const parsedUsername =
+            interpolateStrWithEnv(options.username?.toString()) || process.env.REDIS_USERNAME;
+          const parsedPassword =
+            interpolateStrWithEnv(options.password?.toString()) || process.env.REDIS_PASSWORD;
+          const parsedDb = interpolateStrWithEnv(options.db?.toString()) || process.env.REDIS_DB;
+          const parsedFamily =
+            interpolateStrWithEnv(options.family?.toString()) || process.env.REDIS_FAMILY;
+          const numPort = parseInt(parsedPort);
+          const numDb = parseInt(parsedDb);
+          if (parsedHost) {
+            options.logger.debug(`Connecting to Redis at ${parsedHost}:${parsedPort}`);
+            this.client = new Redis({
+              host: parsedHost,
+              port: isNaN(numPort) ? undefined : numPort,
               username: parsedUsername,
               password: parsedPassword,
               db: isNaN(numDb) ? undefined : numDb,
-              enableAutoPipelining: true,
+              family: parsedFamily === '6' ? 6 : undefined,
               ...(lazyConnect ? { lazyConnect: true } : {}),
-              tls: options.tls ? {} : undefined,
-            },
-            enableAutoPipelining: true,
-            enableOfflineQueue: true,
-            ...(lazyConnect ? { lazyConnect: true } : {}),
-          },
-        );
-      } else if ('sentinels' in options) {
-        this.client = new Redis({
-          name: options.name,
-          sentinelPassword:
-            options.sentinelPassword && interpolateStrWithEnv(options.sentinelPassword),
-          sentinels: options.sentinels.map(s => ({
-            host: s.host && interpolateStrWithEnv(s.host),
-            port: s.port && parseInt(interpolateStrWithEnv(s.port)),
-            family: s.family && parseInt(interpolateStrWithEnv(s.family)),
-          })),
-          role: options.role,
-          enableTLSForSentinelMode: options.enableTLSForSentinelMode,
-          enableAutoPipelining: true,
-          enableOfflineQueue: true,
-          lazyConnect,
+              enableAutoPipelining: true,
+              enableOfflineQueue: true,
+            });
+          } else {
+            options.logger.debug(`Connecting to Redis mock`);
+            this.client = new RedisMock();
+          }
+        }
+        const pubsub = toMeshPubSub(options.pubsub);
+        // TODO: PubSub.destroy will no longer be needed after v0
+        const id = pubsub?.subscribe('destroy', () => {
+          this.client.disconnect(false);
+          pubsub.unsubscribe(id);
         });
-      } else if (options.url) {
-        const redisUrl = new URL(interpolateStrWithEnv(options.url));
-
-        if (!['redis:', 'rediss:'].includes(redisUrl.protocol)) {
-          throw new Error('Redis URL must use either redis:// or rediss://');
-        }
-
-        if (lazyConnect) {
-          redisUrl.searchParams.set('lazyConnect', 'true');
-        }
-
-        redisUrl.searchParams.set('enableAutoPipelining', 'true');
-        redisUrl.searchParams.set('enableOfflineQueue', 'true');
-        const IPV6_REGEX =
-          /^(?:(?:[a-fA-F\d]{1,4}:){7}(?:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){6}(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|:[a-fA-F\d]{1,4}|:)|(?:[a-fA-F\d]{1,4}:){5}(?::(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,2}|:)|(?:[a-fA-F\d]{1,4}:){4}(?:(?::[a-fA-F\d]{1,4}){0,1}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,3}|:)|(?:[a-fA-F\d]{1,4}:){3}(?:(?::[a-fA-F\d]{1,4}){0,2}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,4}|:)|(?:[a-fA-F\d]{1,4}:){2}(?:(?::[a-fA-F\d]{1,4}){0,3}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,5}|:)|(?:[a-fA-F\d]{1,4}:){1}(?:(?::[a-fA-F\d]{1,4}){0,4}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,6}|:)|(?::(?:(?::[a-fA-F\d]{1,4}){0,5}:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)(?:\\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)){3}|(?::[a-fA-F\d]{1,4}){1,7}|:)))(?:%[0-9a-zA-Z]{1,})?$/gm;
-        if (IPV6_REGEX.test(redisUrl.hostname)) {
-          redisUrl.searchParams.set('family', '6');
-        }
-        const urlStr = redisUrl.toString();
-        options.logger.debug(`Connecting to Redis at ${urlStr}`);
-        this.client = new Redis(urlStr);
-      } else {
-        const parsedHost =
-          interpolateStrWithEnv(options.host?.toString()) || process.env.REDIS_HOST;
-        const parsedPort =
-          interpolateStrWithEnv(options.port?.toString()) || process.env.REDIS_PORT;
-        const parsedUsername =
-          interpolateStrWithEnv(options.username?.toString()) || process.env.REDIS_USERNAME;
-        const parsedPassword =
-          interpolateStrWithEnv(options.password?.toString()) || process.env.REDIS_PASSWORD;
-        const parsedDb = interpolateStrWithEnv(options.db?.toString()) || process.env.REDIS_DB;
-        const parsedFamily =
-          interpolateStrWithEnv(options.family?.toString()) || process.env.REDIS_FAMILY;
-        const numPort = parseInt(parsedPort);
-        const numDb = parseInt(parsedDb);
-        if (parsedHost) {
-          options.logger.debug(`Connecting to Redis at ${parsedHost}:${parsedPort}`);
-          this.client = new Redis({
-            host: parsedHost,
-            port: isNaN(numPort) ? undefined : numPort,
-            username: parsedUsername,
-            password: parsedPassword,
-            db: isNaN(numDb) ? undefined : numDb,
-            family: parsedFamily === '6' ? 6 : undefined,
-            ...(lazyConnect ? { lazyConnect: true } : {}),
-            enableAutoPipelining: true,
-            enableOfflineQueue: true,
-          });
-        } else {
-          options.logger.debug(`Connecting to Redis mock`);
-          this.client = new RedisMock();
-        }
+      } finally {
+        span.end();
       }
-      const pubsub = toMeshPubSub(options.pubsub);
-      // TODO: PubSub.destroy will no longer be needed after v0
-      const id = pubsub?.subscribe('destroy', () => {
-        this.client.disconnect(false);
-        pubsub.unsubscribe(id);
-      });
-      span.end();
     });
   }
 
@@ -158,7 +161,8 @@ export default class RedisCache<V = string> implements KeyValueCache<V>, Disposa
     return this.tracer.startActiveSpan('hive.cache.get', span =>
       this.client
         .get(key)
-        .then(value => (value != null ? JSON.parse(value) : undefined).finally(() => span.end())),
+        .then(value => (value != null ? JSON.parse(value) : undefined))
+        .finally(() => span.end()),
     );
   }
 
