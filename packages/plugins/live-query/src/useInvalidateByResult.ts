6eaea5b493e409c2268f0ebfca908839bd7c177a
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-11-03 02:55:08 +0300

    Better checks

--- packages/plugins/live-query/src/useInvalidateByResult.ts
+++ packages/plugins/live-query/src/useInvalidateByResult.ts
@@ -21,7 +21,18 @@ export function useInvalidateByResult(params: InvalidateByResultParams): Plugin 
   const liveQueryInvalidationFactoryMap = new Map<string, ResolverDataBasedFactory<string>[]>();
   const timers = new Set<ReturnType<typeof setInterval>>();
   const pubsub = toMeshPubSub(params.pubsub);
-  params.invalidations.forEach(liveQueryInvalidation => {
+  if (!pubsub) {
+    throw new Error(`Live Query plugin requires a pubsub instance.`);
+  }
+  for (const liveQueryInvalidation of params.invalidations) {
+    if (liveQueryInvalidation.invalidate.length === 0) {
+      params.logger.warn(
+        `Invalidation paths are empty for live query invalidation config: ${JSON.stringify(
+          liveQueryInvalidation,
+        )}. Skipping this invalidation config.`,
+      );
+      continue;
+    }
     const rawInvalidationPaths = liveQueryInvalidation.invalidate;
     const factories = rawInvalidationPaths.map(rawInvalidationPath =>
       getInterpolatedStringFactory(rawInvalidationPath),
@@ -29,6 +40,11 @@ export function useInvalidateByResult(params: InvalidateByResultParams): Plugin 
 
     // Set up polling-based invalidation if pollingInterval is provided
     if (liveQueryInvalidation.pollingInterval != null) {
+      if (liveQueryInvalidation.pollingInterval <= 0) {
+        throw new Error(
+          `Polling interval must be a positive integer. Received: ${liveQueryInvalidation.pollingInterval}`,
+        );
+      }
       timers.add(
         setInterval(() => {
           pubsub.publish('live-query:invalidate', liveQueryInvalidation.invalidate);
@@ -38,9 +54,14 @@ export function useInvalidateByResult(params: InvalidateByResultParams): Plugin 
 
     // Set up mutation-based invalidation if field is provided
     if (liveQueryInvalidation.field != null) {
-      liveQueryInvalidationFactoryMap.set(liveQueryInvalidation.field, factories);
+      let existingFactories = liveQueryInvalidationFactoryMap.get(liveQueryInvalidation.field);
+      if (!existingFactories) {
+        existingFactories = [];
+        liveQueryInvalidationFactoryMap.set(liveQueryInvalidation.field, existingFactories);
+      }
+      existingFactories.push(...factories);
     }
-  });
+  }
   const id = pubsub.subscribe('destroy', () => {
     for (const timer of timers) {
       clearInterval(timer);
