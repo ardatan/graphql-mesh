fc47dc90d34d9f9f03acfc11b142f6fdb50367b6
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-11-20 22:28:53 +0300

    Call onDispose

--- packages/plugins/hive/tests/hive.test.ts
+++ packages/plugins/hive/tests/hive.test.ts
@@ -3,15 +3,18 @@ import useMeshHive from '../src';
 describe('Hive', () => {
   it('does not hook into Node.js process', () => {
     const spy = jest.spyOn(process, 'once');
-    useMeshHive({
+    const plugin = useMeshHive({
       enabled: true,
       token: 'FAKE_TOKEN',
-    }).onPluginInit?.({
+    });
+    plugin.onPluginInit?.({
       addPlugin: jest.fn(),
       plugins: [],
       setSchema: jest.fn(),
       registerContextErrorHandler: jest.fn(),
     });
     expect(spy).not.toHaveBeenCalled();
+    // @ts-expect-error - ignore
+    return plugin.onDispose();
   });
 });
